da4d6261083c2760fdf4a4733d7f987fe2eefc13
shovington <samuel.hovington@usherbrooke.ca>
2019-10-08 17:45:36 -0400

    Change control buttons

--- rover_gui/src/rover_gui/rover_gui_widget.py
+++ rover_gui/src/rover_gui/rover_gui_widget.py
@@ -20,100 +20,99 @@ class RoverGuiWidget(QtWidgets.QWidget):
         loadUi(ui_file, self)
         self.setObjectName('RoverGuiWidget')
 
-        self.left_foward_flag = False
-        self.right_foward_flag = False
-        self.left_backward_flag = False
-        self.right_backward_flag = False
+        self.foward_flag = False
+        self.right_flag = False
+        self.left_flag = False
+        self.backward_flag = False
 
-        self.left = 0.0
-        self.right = 0.0
+        self.linear = 0.0
+        self.angular= 0.0
         self.is_active = False
 
-        self.left_foward_btn.pressed.connect(self.left_foward_btn_callback)
-        self.right_foward_btn.pressed.connect(self.right_foward_btn_callback)
-        self.left_backward_btn.pressed.connect(self.left_backward_btn_callback)
-        self.right_backward_btn.pressed.connect(self.right_backward_btn_callback)
+        self.foward_btn.pressed.connect(self.foward_btn_callback)
+        self.left_btn.pressed.connect(self.left_btn_callback)
+        self.right_btn.pressed.connect(self.right_btn_callback)
+        self.backward_btn.pressed.connect(self.backward_btn_callback)
 
-        self.left_foward_btn.released.connect(self.left_foward_btn_released_callback)
-        self.right_foward_btn.released.connect(self.right_foward_btn_released_callback)
-        self.left_backward_btn.released.connect(self.left_backward_btn_released_callback)
-        self.right_backward_btn.released.connect(self.right_backward_btn_released_callback)
+        self.foward_btn.released.connect(self.foward_btn_released_callback)
+        self.right_btn.released.connect(self.right_btn_released_callback)
+        self.left_btn.released.connect(self.left_btn_released_callback)
+        self.backward_btn.released.connect(self.backward_btn_released_callback)
 
         self.keyboard_check_box.clicked[bool].connect(self.keyboard_check_box_callback)
         
-        self.left_foward_shortcut = QShortcut(QKeySequence(self.tr("W", "File|Open")),self)
-        self.right_foward_shortcut = QShortcut(QKeySequence(self.tr("O", "File|Open")),self)
-        self.left_backward_shortcut = QShortcut(QKeySequence(self.tr("S", "File|Open")),self)
-        self.right_backward_shortcut = QShortcut(QKeySequence(self.tr("K", "File|Open")),self)
+        self.foward_shortcut = QShortcut(QKeySequence(self.tr("W", "File|Open")),self)
+        self.right_shortcut = QShortcut(QKeySequence(self.tr("D", "File|Open")),self)
+        self.left_shortcut = QShortcut(QKeySequence(self.tr("A", "File|Open")),self)
+        self.backward_shortcut = QShortcut(QKeySequence(self.tr("S", "File|Open")),self)
 
-        self.left_foward_shortcut.activated.connect(self.left_foward_btn.animateClick)
-        self.right_foward_shortcut.activated.connect(self.right_foward_btn.animateClick)
-        self.left_backward_shortcut.activated.connect(self.left_backward_btn.animateClick)
-        self.right_backward_shortcut.activated.connect(self.right_backward_btn.animateClick)
+        self.foward_shortcut.activated.connect(self.foward_btn.animateClick)
+        self.right_shortcut.activated.connect(self.right_btn.animateClick)
+        self.left_shortcut.activated.connect(self.left_btn.animateClick)
+        self.backward_shortcut.activated.connect(self.backward_btn.animateClick)
 
         self.gui_cmd_pub = rospy.Publisher('gui_cmd', Command, queue_size=10)
         rospy.Timer(rospy.Duration(1.0/10.0),self.publish_command)
         
 
-    def left_foward_btn_callback(self):
-        self.left_foward_flag = True
+    def foward_btn_callback(self):
+        self.foward_flag = True
         self.update_command()
 
-    def right_foward_btn_callback(self):
-        self.right_foward_flag = True
+    def right_btn_callback(self):
+        self.right_flag = True
         self.update_command()
 
-    def left_backward_btn_callback(self):
-        self.left_backward_flag = True
+    def left_btn_callback(self):
+        self.left_flag = True
         self.update_command()
 
-    def right_backward_btn_callback(self):
-        self.right_backward_flag = True
+    def backward_btn_callback(self):
+        self.backward_flag = True
         self.update_command()
 
-    def left_foward_btn_released_callback(self):
-        self.left_foward_flag = False
+    def foward_btn_released_callback(self):
+        self.foward_flag = False
         self.update_command()
 
-    def right_foward_btn_released_callback(self):
-        self.right_foward_flag = False
+    def right_btn_released_callback(self):
+        self.right_flag = False
         self.update_command()
 
-    def left_backward_btn_released_callback(self):
-        self.left_backward_flag = False
+    def left_btn_released_callback(self):
+        self.left_flag = False
         self.update_command()
 
-    def right_backward_btn_released_callback(self):
-        self.right_backward_flag = False
+    def backward_btn_released_callback(self):
+        self.backward_flag = False
         self.update_command()
     
     def keyboard_check_box_callback(self):
         self.update_command()
 
     def update_command(self):
-        if self.left_foward_flag:
-            self.left = self.speed_slider.value()/100.0
-        elif self.left_backward_flag:
-            self.left = -(self.speed_slider.value()/100.0)
+        if self.foward_flag:
+            self.linear = self.speed_slider.value()/100.0
+        elif self.backward_flag:
+            self.linear = -(self.speed_slider.value()/100.0)
         else:
-            self.left = 0
+            self.linear = 0
 
-        if self.right_foward_flag:
-            self.right = self.speed_slider.value()/100.0
-        elif self.right_backward_flag:
-            self.right = -(self.speed_slider.value()/100.0)
+        if self.right_flag:
+            self.angular = self.speed_slider.value()/100.0
+        elif self.left_flag:
+            self.angular = -(self.speed_slider.value()/100.0)
         else:
-            self.right = 0
+            self.angular = 0
             
         self.is_active = self.keyboard_check_box.isChecked()
 
     def publish_command(self, event):
         command = Command()
-        command.left = self.left
-        command.right = self.right
+        command.linear = self.linear
+        command.angular = self.angular
         command.is_active = self.is_active
         self.gui_cmd_pub.publish(command)
-
 
 
 
