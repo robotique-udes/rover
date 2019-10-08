cf1be681d4a096558fa79b90ebaafb0dd25a1c14
Philippe Warren <philippewarren31@gmail.com>
2019-10-08 14:56:38 -0400

    Linked all buttons and fields but checkbox and update btn

--- rover_cam_control/src/rover_cam_control/rover_cam_control_widget.py
+++ rover_cam_control/src/rover_cam_control/rover_cam_control_widget.py
@@ -20,132 +20,108 @@ class RoverCamControlWidget(QtWidgets.QWidget):
         loadUi(ui_file, self)
         self.setObjectName('RoverCamControlWidget')
 
-        self.up_cam_flag = False
-        self.down_cam_flag = False
-        self.left_cam_flag = False
-        self.right_cam_flag = False
+        self.cam_horizontal_pos = 0
+        self.cam_vertical_pos = 0
 
-        self.cam_horizontal_slider
-        self.cam_vertical_slider
-
-        self.cam_horizontal_field.setValue(self.cam_horizontal_slider.value())
-        self.cam_vertical_field.setValue(self.cam_vertical_slider.value())
-
-        self.left = 0.0
-        self.right = 0.0
-        self.is_active = False
+        self.cam_horizontal_pos_signal = 0
+        self.cam_vertical_pos_signal = 0
 
         self.cam_left_btn.clicked.connect(self.cam_left_btn_callback)
         self.cam_right_btn.clicked.connect(self.cam_right_btn_callback)
         self.cam_up_btn.clicked.connect(self.cam_up_btn_callback)
         self.cam_down_btn.clicked.connect(self.cam_down_btn_callback)
+        self.cam_update_btn.clicked.connect(self.update_command)
 
-        # self.cam_left_btn.released.connect(self.cam_left_btn_released_callback)
-        # self.cam_right_btn.released.connect(self.cam_right_btn_released_callback)
-        # self.cam_up_btn.released.connect(self.cam_up_btn_released_callback)
-        # self.cam_down_btn.released.connect(self.cam_down_btn_released_callback)
-
-        self.keyboard_check_box.clicked[bool].connect(self.keyboard_check_box_callback)
+        # self.cam_keyboard_check_box.clicked[bool].connect(self.cam_keyboard_check_box_callback)
         
         self.cam_left_shortcut = QShortcut(QKeySequence(self.tr("G", "File|Open")), self)
         self.cam_right_shortcut = QShortcut(QKeySequence(self.tr("J", "File|Open")), self)
         self.cam_up_shortcut = QShortcut(QKeySequence(self.tr("Y", "File|Open")), self)
         self.cam_down_shortcut = QShortcut(QKeySequence(self.tr("H", "File|Open")), self)
+        self.cam_update_shortcut = QShortcut(QKeySequence(self.tr("B", "File|Open")), self)
 
-        self.cam_left_shortcut.activated.connect(self.cam_left_btn.animateClick)
-        self.cam_right_shortcut.activated.connect(self.cam_right_btn.animateClick)
-        self.cam_up_shortcut.activated.connect(self.cam_up_btn.animateClick)
-        self.cam_down_shortcut.activated.connect(self.cam_down_btn.animateClick)
+        if self.cam_keyboard_check_box.isChecked():
+            self.cam_left_shortcut.activated.connect(self.cam_left_btn.animateClick)
+            self.cam_right_shortcut.activated.connect(self.cam_right_btn.animateClick)
+            self.cam_up_shortcut.activated.connect(self.cam_up_btn.animateClick)
+            self.cam_down_shortcut.activated.connect(self.cam_down_btn.animateClick)
+            self.cam_update_btn.activated.connect(self.cam_update_btn.animateClick)
+
+        self.cam_horizontal_slider.valueChanged.connect(self.update_fields)
+        self.cam_vertical_slider.valueChanged.connect(self.update_fields)
+        self.cam_horizontal_field.valueChanged.connect(self.update_sliders)
+        self.cam_vertical_field.valueChanged.connect(self.update_sliders)
 
         # self.gui_cmd_pub = rospy.Publisher('gui_cmd', Command, queue_size=10)
         # rospy.Timer(rospy.Duration(1.0/10.0), self.publish_command)
-        
+
+    def init_fields(self):
+        self.cam_horizontal_field.setValue(0)
+        self.cam_horizontal_slider.setValue(0)
+        self.cam_horizontal_pos_info.setValue(0)
+        self.cam_vertical_field.setValue(0)
+        self.cam_vertical_slider.setValue(0)
+        self.cam_vertical_pos_info.setValue(0)
+        self.cam_keyboard_check_box.setChecked(False)
+
     def cam_left_btn_callback(self):
         self.cam_horizontal_slider.triggerAction(2)
-        # self.update_command()
         self.update_fields()
 
     def cam_right_btn_callback(self):
         self.cam_horizontal_slider.triggerAction(1)
-        # self.update_command()
         self.update_fields()
 
     def cam_up_btn_callback(self):
         self.cam_vertical_slider.triggerAction(1)
-        # self.update_command()
         self.update_fields()
 
     def cam_down_btn_callback(self):
         self.cam_vertical_slider.triggerAction(2)
-        # self.update_command()
         self.update_fields()
 
-    def cam_left_btn_released_callback(self):
-        self.left_cam_flag = False
-       
-        # self.cam_vertical_field.valuechage.connect(self.fix_slider)
-       
        # self.gui_cmd_pub = rospy.Publisher('gui_cmd', Command, queue_size=10)
        #  rospy.Timer(rospy.Duration(1.0/10.0), self.publish_command)
-        
-#     def fix_slider(self):
-#         self.vertical_slider.setvalue(cam_vertical_field.value())
-#
-#     def left_foward_btn_callback(self):
-#         self.left_foward_flag = True
-#         self.update_command()
-#
-#     def right_foward_btn_callback(self):
-#         self.right_foward_flag = True
-#         self.update_command()
-#
-#     def left_backward_btn_callback(self):
-#         self.left_backward_flag = True
-#         self.update_command()
-#
-#     def right_backward_btn_callback(self):
-#         self.right_backward_flag = True
-#         self.update_command()
-#
-#     def left_foward_btn_released_callback(self):
-#         self.left_foward_flag = False
-#         self.update_command()
 
-    def cam_right_btn_released_callback(self):
-        self.right_cam_flag = False
-        self.update_command()
-
-    def cam_up_btn_released_callback(self):
-        self.up_cam_flag = False
-        self.update_command()
-
-    def cam_down_btn_released_callback(self):
-        self.down_cam_flag = False
-        self.update_command()
+    def update_sliders(self):
+        self.cam_horizontal_slider.setValue(self.cam_horizontal_field.value())
+        self.cam_vertical_slider.setValue(self.cam_vertical_field.value())
     
-    def keyboard_check_box_callback(self):
+    def cam_keyboard_check_box_callback(self):
         self.update_command()
 
     def update_fields(self):
-        self.cam_horizontal_field.setValue(self.cam_horizontal_slider.value())
-        self.cam_vertical_field.setValue(self.cam_vertical_slider.value())
+        self.cam_horizontal_pos = self.cam_horizontal_slider.value()
+        self.cam_vertical_pos = self.cam_vertical_slider.value()
+        self.cam_horizontal_field.setValue(self.cam_horizontal_pos)
+        self.cam_vertical_field.setValue(self.cam_vertical_pos)
 
-    # def update_command(self):
-    #     if self.left_foward_flag:
-    #         self.left = self.speed_slider.value()/100.0
-    #     elif self.left_backward_flag:
-    #         self.left = -(self.speed_slider.value()/100.0)
-    #     else:
-    #         self.left = 0
-    #
-    #     if self.right_foward_flag:
-    #         self.right = self.speed_slider.value()/100.0
-    #     elif self.right_backward_flag:
-    #         self.right = -(self.speed_slider.value()/100.0)
-    #     else:
-    #         self.right = 0
-            
-        self.is_active = self.keyboard_check_box.isChecked()
+    def update_position(self):
+        self.cam_horizontal_pos_info.setValue(self.cam_horizontal_slider.value())
+        self.cam_vertical_pos_info.setValue(self.cam_vertical_slider.value())
+    # CHANGEMENT DU SIGNAL DE POSITION ENVOYE AU ROBOT
+
+    def update_command(self):
+        self.cam_horizontal_pos_signal = self.cam_horizontal_pos
+        self.cam_vertical_pos_signal = self.cam_vertical_pos
+        self.cam_horizontal_pos_info.setValue(self.cam_horizontal_pos_signal)
+        self.cam_vertical_pos_info.setValue(self.cam_vertical_pos_signal)
+
+
+        # if self.left_foward_flag:
+        #     self.left = self.speed_slider.value()/100.0
+        # elif self.left_backward_flag:
+        #     self.left = -(self.speed_slider.value()/100.0)
+        # else:
+        #     self.left = 0
+        #
+        # if self.right_foward_flag:
+        #     self.right = self.speed_slider.value()/100.0
+        # elif self.right_backward_flag:
+        #     self.right = -(self.speed_slider.value()/100.0)
+        # else:
+        #     self.right = 0
+        # self.is_active = self.keyboard_check_box.isChecked()
 
     # def publish_command(self, event):
     #     command = Command()
