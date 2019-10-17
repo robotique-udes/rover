92d03d0f086df10d103e6ba91efc35a9bb82f6fd
shovington <samuel.hovington@usherbrooke.ca>
2019-10-17 17:47:29 -0400

    Add rover_nav package

---
+++ rover_nav/scripts/sim_gps.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+from gps_common.msg import GPSFix
+
+def simGPS():
+	cur_lon = -73.5
+	cur_lat = 45.5
+	pub = rospy.Publisher('pos', GPSFix, queue_size=10)
+	rospy.init_node('simGPS', anonymous=False)
+	rate = rospy.Rate(1) # 10hz
+	msg = GPSFix()
+	while not rospy.is_shutdown():
+		cur_lon += 0.001
+		cur_lat += 0.001
+		msg.longitude = cur_lon
+		msg.latitude = cur_lat
+		rospy.loginfo("Longitude = %f   Latitude = %f" % (msg.longitude, msg.latitude))
+		pub.publish(msg)
+		rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        simGPS()
+    except rospy.ROSInterruptException:
+        pass
