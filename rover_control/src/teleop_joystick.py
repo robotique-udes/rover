00dfa7be48c8c1624745f372dd2afb40b61e03f1
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2020-11-03 19:57:23 -0500

    Squashed commit of the following:
    commit 553fd6f1753560b06abfc5ad981d0a755dbd1f16
    Author: Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
    Date:   Tue Nov 3 19:52:09 2020 -0500
    
        changed axis x to y and changed variable names to fit the coding convention
    
    commit fb6190f01d4f40328d4c69dcf7d450644357c9e3
    Author: Vincent Pelletier <vincentpelletier97@gmail.com>
    Date:   Tue Nov 3 19:42:30 2020 -0500
    
        Pan tilt unit (PTU) added to joystick
    
        Pan tilt unit was introduced to the code. With the right joystick, the up and down can now publish X angular commands and the left to right publishes Z angular commands
    
    commit a951943f62c212e667aa272508e0c7c7a60c288d
    Author: Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
    Date:   Thu Oct 29 16:18:09 2020 -0400
    
        Set default twist to 0 instead of 0.0 to avoid float precision problem
    
    commit b7d4d47b52f3ef23cee707afdcab20a890e1f1ec
    Author: Vincent Pelletier <vincentpelletier97@gmail.com>
    Date:   Sun Oct 25 15:23:49 2020 -0400
    
        removed the set_params from the teleop python file and put back the initial values on the launch file
    
    commit fc4f8c4e1c67bc1358acf3b7bc2f820f8d26527c
    Author: Vincent Pelletier <vincentpelletier97@gmail.com>
    Date:   Sun Oct 25 15:07:27 2020 -0400
    
        Every scaling factor are beginning at 0. If the deadman is activated, then their value may change. Furthermore, when deadman is untoggled, parameter values are reset to 0.
    
    commit d8b33ad849e624378be6150cca91c06ae62c6d0f
    Author: Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
    Date:   Sun Oct 25 13:26:03 2020 -0400
    
        removed namespace for cmd_vel
    
    commit 7c75f8ae0c6a98685370442d83a53d2043d99e83
    Author: Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
    Date:   Sun Oct 25 13:13:24 2020 -0400
    
        fixed parameter errors
    
    commit eec85c5db33e3fcbb59547d16691c1c64f9a7961
    Author: Vincent Pelletier <vincentpelletier97@gmail.com>
    Date:   Sun Oct 25 12:13:08 2020 -0400
    
        Added every joystick parameter to the launch file with base values. They are called privately. The teleop_joystick.py now receives parameters and process the output to cmd_vel depending on joystick inputs.
    
    commit c3e174ff4c03312c5b85ce86c3895020a2c7ea80
    Author: Plopao <42898536+Plopao@users.noreply.github.com>
    Date:   Sun Oct 25 10:18:45 2020 -0400
    
        Update teleop_joystick
    
        Changement des variables
    
    commit 5a66915257988343d6d41af69c1ae6ade4e6d0e0
    Author: Vincent Pelletier <vincentpelletier97@gmail.com>
    Date:   Sat Oct 24 13:20:59 2020 -0400
    
        Joystick callback function was made. Can receive from /joy and publish a twist
    
    commit 969f2039545c4095ba32306a408ceaa4d22bed58
    Author: Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
    Date:   Mon Oct 19 12:04:56 2020 -0400
    
        starter code

--- rover_control/src/teleop_joystick.py
+++ rover_control/src/teleop_joystick.py
@@ -5,10 +5,11 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
 class TeleopJoystick():
-   def __init__(self):
+    def __init__(self):
         rospy.init_node("teleop_joystick", anonymous = True)
         self.joy_sub = rospy.Subscriber('/joy', Joy, self.joyCB)
         self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
+        self.cmd_ptu_pub = rospy.Publisher('/cmd_ptu', Twist, queue_size=2)
 
         #Getting parameters initialized at the launch file
         self.enable_button = rospy.get_param("~enable_button", 4)
@@ -18,8 +19,14 @@ class TeleopJoystick():
         self.angular_scaling = rospy.get_param("~scale_angular", 1.0)
         self.axis_linear = rospy.get_param("~axis_linear", 1)
         self.axis_angular = rospy.get_param("~axis_angular", 0)
+        self.ptu_y_axis = rospy.get_param("~ptu_y_axis", 4)
+        self.ptu_z_axis = rospy.get_param("~ptu_z_axis", 3)
 
-   def joyCB(self, data):
+    def joyCB(self, data): #calls both commands functions (for PTU and vehicle movements)
+        self.vehicleCMD(data)
+        self.ptuCMD(data)
+
+    def vehicleCMD(self,data): #Publishes the vehicle movements commands to the cmd_vel topic
         twist = Twist()
         twist.linear.x = data.axes[self.axis_linear]
         twist.angular.z = data.axes[self.axis_angular]
@@ -37,7 +44,12 @@ class TeleopJoystick():
             twist.angular.z = 0
             twist.linear.x = 0
             self.cmd_pub.publish(twist)
-        
+
+    def ptuCMD(self,data): #Publishes the pan and tilt unit commands to the cmd_panTilt topic
+        twist = Twist()
+        twist.angular.y = data.axes[self.ptu_y_axis]
+        twist.angular.z = data.axes[self.ptu_z_axis]
+        self.cmd_ptu_pub.publish(twist)
 
 if __name__ == '__main__':
     teleop_joystick = TeleopJoystick()
