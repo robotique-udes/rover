6272f0ffc1c77848b3f9a3d140c0fdf39b8a769a
Simon Chamorro <simon.chamorro@usherbrooke.ca>
2021-02-08 14:05:35 -0500

    added motor drivers

---
+++ ros_talon/include/talon_bridge/talon.h
@@ -0,0 +1,133 @@
+#ifndef TALON_H
+#define TALON_H
+
+#include "ros/ros.h"
+#include "can_msgs/Frame.h"
+#include "std_msgs/Int32.h"
+#include "std_msgs/Float32.h"
+#include "ros_talon/Status.h"
+#include "ros_talon/SetPID.h"
+#include "ros_talon/FindCenter.h"
+
+#define MIN_SPEED	60 // Speed at which the motors will be driven during recovery routines.
+
+// Frame ID's. Completely borrowed from Phoenix library.
+#define TALON 	0x02040000
+#define CONTROL_3	0x00040080
+#define STATUS_01	0x00041400
+#define STATUS_02	0x00041440
+#define STATUS_03	0x00041480
+#define STATUS_04	0x000414C0
+#define STATUS_13	0x00041700
+#define PARAM_SET	0x041880
+
+// Constant used to convert a floating point number to a fixed point one int 10.22 format.
+#define FLOAT_TO_FXP_10_22 (float)0x400000
+
+// Constants used to assemble the corresponding CAN frames. Borrowed from Phoenix library.
+#define KP	310
+#define KI	311
+#define KD	312
+#define KF	313
+#define StickyFaults	390
+#define modeNoAction		0x00
+#define modePercentOutput	0x01
+#define modeServoPosition	0x02
+#define modeMotionProfile	0x03
+
+namespace talon
+{
+class TalonSRX
+{
+	public:
+		TalonSRX(ros::NodeHandle* nh, std::string motor_nb);
+		void setup(unsigned char ID, unsigned char mode);
+
+	private:
+		uint32_t _data32;
+		uint _baseArbID;
+		int32_t _percent;
+		float _pos;
+		unsigned char _center;
+		unsigned char _cwLimit;
+		unsigned char _ccwLimit;
+		unsigned char _ignoreTopics;
+		unsigned char _pauseFunction;
+		int32_t _currentPos;
+
+		float _statusTemp;
+		float _statusOutputCurrent;
+		float _statusBusVoltage;
+		float _statusClosedLoopError;
+
+		// Main function pointer.
+		void (TalonSRX::*_modeFunc)() = NULL;
+
+		// Recovery function pointer.
+		void (TalonSRX::*_recoverFunc)() = NULL;
+
+		ros::NodeHandle* _nh;
+
+		ros::Publisher _CANSender;
+		ros::Publisher _posPub;
+		ros::Publisher _statusPub;
+
+		ros::Subscriber _TalonInput;
+		ros::Subscriber _CANReceiver;
+
+		ros::Timer _talon_timer;
+
+		ros::ServiceServer _fcenter;
+		ros::ServiceServer _spid;
+
+		
+		void TalonLoop(const ros::TimerEvent& event);
+		void enableFrame();
+
+		void percentOutput();
+		void setPercentVal(const std_msgs::Int32 &f);
+		
+		void ServoPos();
+		void setPos(const std_msgs::Float32 &f);
+		void setFeedback2QuadEncoder(); //Actually, this function is not necessary as the Talon defaults to QuadEncoder.
+
+		void processCanFrame(const can_msgs::Frame &f);
+		void unpackStatus1(const can_msgs::Frame &f);
+		void unpackStatus2(const can_msgs::Frame &f);
+		void unpackStatus3(const can_msgs::Frame &f);
+		void unpackStatus4(const can_msgs::Frame &f);
+		void unpackStatus13(const can_msgs::Frame &f);
+
+		void publishStatus();
+		void ClearStickyFaults();
+
+
+		bool FindCenter(ros_talon::SetPID::Request  &req, ros_talon::SetPID::Response &res); //Service callback
+		void findCenter(); //Actual function
+		void findCenterR();
+		void findCenterL();
+		void setZero();
+		void recoverLims();
+		void recoverCW();
+		void recoverCCW();
+
+
+		bool setPID(ros_talon::SetPID::Request  &req, ros_talon::SetPID::Response &res);
+		void setKP(float value);
+		void setKI(float value);
+		void setKD(float value);
+		void setKF(float value);
+};// class TalonSRX
+};// namespace talon_interface
+
+#endif // TALON_INTERFACE_H
+
+/*
+To-Do
+Patch the CAN interface with an auto-recovery service.
+*/
+
+/*
+To-Do
+Add a service to retrieve the current faults and sticky faults
+*/
---
+++ ros_talon/src/servo_pos.cpp
@@ -0,0 +1,58 @@
+/*
+Node to control a TalonSRX device on position servo mode.
+Internally, it subscribes to
+
+/received_messages
+/sent_messages
+/ros_talon/steering_angle
+
+The two first of type can_msgs::Frame from the socketcan_bridge
+package, to talk to the Talon through CAN protocol. The last one
+of type std_msgs::Float32 which corresponds to the actual position
+being fixed by the motor.
+
+This node also publishes the following topics:
+
+/ros_talon/current_position
+/ros_talon/status
+
+The first one publishes a message of type std_msgs::Float32 holding
+the last position value read from the Talon, the last one publishes
+a message of type ros_talon::Status holding relevant status variables
+read from the Talon.
+
+The following services are also provided:
+
+/ros_talon/SetPID
+/ros_talon/FindCenter
+
+The first one takes three Float32 values as argument, corresponding to
+the Kp, Ki and Kd values that will be configured on the Talon. These
+values are used in the internal (to the Talon) PID control loop that
+is used to keep the position stable in position servo mode.
+
+The last one takes no arguments and simply takes care of finding the
+center of the drive train when called.
+*/
+
+#include <talon_bridge/talon.h>
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "talon_srx");
+	ros::NodeHandle n;
+
+	std::string motor_nb_str;
+	int motor_nb;  
+    
+    n.getParam(n.getNamespace() + "motor_nb", motor_nb_str); 
+    std::stringstream ss;  
+    ss << motor_nb_str;    
+    ss >> motor_nb;  
+
+	talon::TalonSRX talon(&n, motor_nb_str);
+	talon.setup(motor_nb, modeServoPosition); 
+
+	ros::spin();
+	return 0;
+}
