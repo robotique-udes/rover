31ce517b8c423efd78e6b544f182664a15599692
philippewarren <philippewarren31@gmail.com>
2022-03-30 18:32:36 -0400

    WIP

--- modprobe-setup.sh
+++ modprobe-setup.sh
@@ -1,13 +1,8 @@
 sudo modprobe peak_usb
-<<<<<<< HEAD
-sudo modprobe peak_pci
-=======
 if [ $(hostname) != "rover" ]
 then
     sudo modprobe peak_pci
 fi
->>>>>>> canbus_local
-# sudo modprobe pcan
 sudo modprobe esd_usb2
 
 sudo ip link set can0 up type can bitrate 1000000
