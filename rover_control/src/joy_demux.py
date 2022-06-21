8505c858b57e69604eff6770f57db0eb357fcc4a
philippewarren <philippewarren31@gmail.com>
2022-06-21 19:16:51 -0400

    Added joy demux for arm

--- rover_control/src/joy_demux.py
+++ rover_control/src/joy_demux.py
@@ -4,6 +4,7 @@
 from __future__ import print_function, division, unicode_literals, absolute_import
 
 import rospy
+from std_msgs.msg import Bool
 from sensor_msgs.msg import Joy
 from std_srvs.srv import SetBool, SetBoolResponse
 from threading import Lock
@@ -14,14 +15,28 @@ class JoyDemuxNode:
         self.joy_sub = rospy.Subscriber("/joy", Joy, self.joy_cb, queue_size=1)
         self.rover_joy_pub = rospy.Publisher("/rover_joy", Joy, queue_size=1)
         self.arm_joy_pub = rospy.Publisher("/arm_joy", Joy, queue_size=1)
+        self.joy_state_pub = rospy.Publisher("/joy_state_is_arm", Bool, queue_size=1)
 
         self.set_arm_joy_srv = rospy.Service("set_arm_joy", SetBool, self.set_arm_joy_cb)
 
         self._is_arm_joy_lock = Lock()
         self._is_arm_joy = rospy.get_param("~start_as_arm_joy", False)
+        self._toggle_arm_joy_btn = rospy.get_param("~toggle_arm_joy_btn", 3)
+        self._allow_toggle = rospy.get_param("~allow_toggle", True)
+        self._last_arm_joy_btn_state = False
+
+        self.joy_state_pub_rate = rospy.Timer(rospy.Duration(1), self.publish_joy_state)
 
     def joy_cb(self, msg):
         with self._is_arm_joy_lock:
+            if (
+                self._allow_toggle
+                and msg.buttons[self._toggle_arm_joy_btn] != self._last_arm_joy_btn_state
+                and msg.buttons[self._toggle_arm_joy_btn] == 1
+            ):
+                self._is_arm_joy = not self._is_arm_joy
+                rospy.logwarn(f"Toggling arm joy to {self._is_arm_joy}; last state is {self._last_arm_joy_btn_state}")
+            self._last_arm_joy_btn_state = msg.buttons[self._toggle_arm_joy_btn]
             is_arm = self._is_arm_joy
 
         if is_arm:
@@ -33,3 +48,13 @@ class JoyDemuxNode:
         with self._is_arm_joy_lock:
             self._is_arm_joy = req.data
         return SetBoolResponse(success=True, message="")
+
+    def publish_joy_state(self, _):
+        with self._is_arm_joy_lock:
+            self.joy_state_pub.publish(self._is_arm_joy)
+
+
+if __name__ == "__main__":
+    rospy.init_node("joy_demux")
+    node = JoyDemuxNode()
+    rospy.spin()
--- rover_control/src/teleop_joystick.py
+++ rover_control/src/teleop_joystick.py
@@ -5,11 +5,12 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 from rover_udes.msg import CamCommand
 
-class TeleopJoystick():
-    def __init__(self):
-        rospy.init_node("teleop_joystick", anonymous = True)
 
-        #Getting parameters initialized at the launch file
+class TeleopJoystick:
+    def __init__(self):
+        rospy.init_node("teleop_joystick", anonymous=True)
+
+        # Getting parameters initialized at the launch file
         self.enable_button = rospy.get_param("~enable_button", 4)
         self.enable_turbo_button = rospy.get_param("~enable_turbo_button", 5)
         self.linear_scaling = rospy.get_param("~scale_linear", 1.0)
@@ -22,43 +23,42 @@ class TeleopJoystick():
         self.ptu_horizontal_scaling = rospy.get_param("~ptu_horizontal_scaling", 1)
         self.ptu_vertical_scaling = rospy.get_param("~ptu_vertical_scaling", 1)
 
-        self.joy_sub = rospy.Subscriber('/joy', Joy, self.joyCB)
-        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
-        self.cmd_ptu_pub = rospy.Publisher('/cmd_ptu', CamCommand, queue_size=2)
+        self.joy_sub = rospy.Subscriber("/joy", Joy, self.joyCB)
+        self.cmd_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=2)
+        self.cmd_ptu_pub = rospy.Publisher("/cmd_ptu", CamCommand, queue_size=2)
 
-    def joyCB(self, data): #calls both commands functions (for PTU and vehicle movements)
+    def joyCB(self, data):  # calls both commands functions (for PTU and vehicle movements)
         self.vehicleCMD(data)
         self.ptuCMD(data)
 
-    def vehicleCMD(self,data): #Publishes the vehicle movements commands to the cmd_vel topic
+    def vehicleCMD(self, data):  # Publishes the vehicle movements commands to the cmd_vel topic
         twist = Twist()
         twist.linear.x = data.axes[self.axis_linear]
         twist.angular.z = data.axes[self.axis_angular]
 
-        if data.buttons[self.enable_button] == 1: #Verify the dead man button is active (LB)
-            twist.angular.z *= self.angular_scaling #Both the turbo mode and normal mode share the same angular speed
+        if data.buttons[self.enable_button] == 1:  # Verify the dead man button is active (LB)
+            twist.angular.z *= self.angular_scaling  # Both the turbo mode and normal mode share the same angular speed
 
-            if data.buttons[self.enable_turbo_button] == 1: #Verify if the turbo button is active (RB)
+            if data.buttons[self.enable_turbo_button] == 1:  # Verify if the turbo button is active (RB)
                 twist.linear.x *= self.linear_scaling_turbo
                 self.cmd_pub.publish(twist)
             else:
-                twist.linear.x *= self.linear_scaling 
+                twist.linear.x *= self.linear_scaling
                 self.cmd_pub.publish(twist)
         else:
             twist.angular.z = 0
             twist.linear.x = 0
             self.cmd_pub.publish(twist)
 
-    def ptuCMD(self,data): #Publishes the pan and tilt unit commands to the cmd_panTilt topic
+    def ptuCMD(self, data):  # Publishes the pan and tilt unit commands to the cmd_panTilt topic
         cam_cmd = CamCommand()
         cam_cmd.mode = 1  # Velocity mode
         cam_cmd.cam_horizontal = data.axes[self.ptu_vertical_axis]
         cam_cmd.cam_vertical = data.axes[self.ptu_horizontal_axis]
         self.cmd_ptu_pub.publish(cam_cmd)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     teleop_joystick = TeleopJoystick()
     rospy.loginfo("teleop_joystick ready")
     rospy.spin()
-
-    
