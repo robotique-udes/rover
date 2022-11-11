fb3269d185efa66d51566e76181d499a65b06138
Pmichaud69 <93682089+Pmichaud69@users.noreply.github.com>
2022-11-11 15:10:18 -0500

    Added gui_cmd publisher

--- rover_arm_gui/src/rover_arm_gui/rover_arm_gui_widget.py
+++ rover_arm_gui/src/rover_arm_gui/rover_arm_gui_widget.py
@@ -10,6 +10,7 @@ from PyQt5.QtCore import QObject
 from PyQt5.QtGui import QKeySequence
 from PyQt5.QtWidgets import QShortcut, QSlider, QLCDNumber, QLabel
 from rovus_bras.msg import feedback
+from rovus_bras.msg import arm_gui_cmd
 
 JOINT_NUMBER = 4
 
@@ -20,6 +21,8 @@ style_Limiting = "color: black; background-color: yellow"
 
 class RoverArmGuiWidget(QtWidgets.QWidget):
 
+    arm_gui_cmd_msg = arm_gui_cmd()
+
     def __init__(self):        
         super(RoverArmGuiWidget, self).__init__()
 
@@ -29,14 +32,23 @@ class RoverArmGuiWidget(QtWidgets.QWidget):
 
         self.is_active = False
         self.feedback_sub = rospy.Subscriber('rovus_bras_feedback', feedback, self.feedback_callback)
+        self.pub_arm_gui_cmd = rospy.Publisher('arm_gui_cmd', arm_gui_cmd, queue_size=1)
 
         self.currSpeeds = [self.j1_currSpeed, self.j2_currSpeed, self.j3_currSpeed, self.j4_currSpeed]
         self.currAngles = [self.j1_currAngle, self.j2_currAngle, self.j3_currAngle, self.j4_currAngle]
         self.speedLabels = [self.j1_speedLabel, self.j2_speedLabel, self.j3_speedLabel, self.j4_speedLabel]
 
+        self.j1_enable.pressed.connect(self.j1_enable_pressed_callback)
+        self.j1_enable.released.connect(self.j1_enable_released_callback)
+        self.j2_enable.pressed.connect(self.j2_enable_pressed_callback)
+        self.j2_enable.released.connect(self.j2_enable_released_callback)
+        self.j3_enable.pressed.connect(self.j3_enable_pressed_callback)
+        self.j3_enable.released.connect(self.j3_enable_released_callback)
+        self.j4_enable.pressed.connect(self.j4_enable_pressed_callback)
+        self.j4_enable.released.connect(self.j4_enable_released_callback)
+
 
     def feedback_callback(self, data):
-
         #________________________________________
         #Angles LCD displays
         for i in range(len(self.currAngles)):
@@ -57,7 +69,6 @@ class RoverArmGuiWidget(QtWidgets.QWidget):
             self.jogMode.setText("Joint")
         else:
             self.jogMode.setText("Cartesian")
-
 
         #________________________________________
         #Singular Matrix
@@ -100,7 +111,7 @@ class RoverArmGuiWidget(QtWidgets.QWidget):
 
         if data.limiteur:
             for i in range(len(self.speedLabels)):
-                if int(data.vitesses[i]) == 20:
+                if abs(int(data.vitesses[i])) == 20:
                     speedLabelsStyle[i] = style_Limiting
 
         for i in range(len(self.speedLabels)):
@@ -110,3 +121,54 @@ class RoverArmGuiWidget(QtWidgets.QWidget):
         #Speed Multiplicator
         self.currSpeedMultiplicator.display(data.speed_multiplier)
 
+        self.publish_command();
+
+    def publish_command(self):
+        self.pub_arm_gui_cmd.publish(self.arm_gui_cmd_msg)
+
+    def j1_enable_pressed_callback(self):
+        self.arm_gui_cmd_msg.enable[0] = 0;
+    
+    def j1_enable_released_callback(self):
+        self.arm_gui_cmd_msg.enable[0] = 1;
+
+    def j2_enable_pressed_callback(self):
+        self.arm_gui_cmd_msg.enable[1] = 0;
+    
+    def j2_enable_released_callback(self):
+        self.arm_gui_cmd_msg.enable[1] = 1;
+
+    def j3_enable_pressed_callback(self):
+        self.arm_gui_cmd_msg.enable[2] = 0;
+    
+    def j3_enable_released_callback(self):
+        self.arm_gui_cmd_msg.enable[2] = 1;
+
+    def j4_enable_pressed_callback(self):
+        self.arm_gui_cmd_msg.enable[3] = 0;
+    
+    def j4_enable_released_callback(self):
+        self.arm_gui_cmd_msg.enable[3] = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
