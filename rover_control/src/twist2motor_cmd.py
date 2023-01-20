4d8e727564a230caa8352784241df725c9b30254
philippewarren <philippewarren31@gmail.com>
2023-01-20 16:34:19 -0500

    Fix new command message for ros_talon

--- rover_control/src/twist2motor_cmd.py
+++ rover_control/src/twist2motor_cmd.py
@@ -15,33 +15,28 @@ ROS Node to go from a Twist to commands for the 4 robot motors
 
 """
 
-from __future__ import (print_function, unicode_literals,
-                        division, absolute_import)
+from __future__ import print_function, unicode_literals, division, absolute_import
 
 import rospy
 import numpy as np
-from std_msgs.msg import Int32
 from geometry_msgs.msg import Twist
+from ros_talon.msg import cmd
 
 
 class LowLevelControlNode(object):
     def __init__(self):
         """Node class to control the motors"""
-        rospy.init_node('twist2cmd', anonymous=False)
+        rospy.init_node("twist2cmd", anonymous=False)
         rospy.on_shutdown(self.on_shutdown)
 
         self.l_cmd = 0
         self.r_cmd = 0
 
         # Init publishers
-        self.m1_pub = rospy.Publisher(
-            '/ros_talon1/motor_percent', Int32, queue_size=10)  # Front left
-        self.m2_pub = rospy.Publisher(
-            '/ros_talon2/motor_percent', Int32, queue_size=10)  # Front right
-        self.m3_pub = rospy.Publisher(
-            '/ros_talon3/motor_percent', Int32, queue_size=10)  # Rear left
-        self.m4_pub = rospy.Publisher(
-            '/ros_talon4/motor_percent', Int32, queue_size=10)  # Rear right
+        self.m1_pub = rospy.Publisher("/ros_talon1/motor_percent", cmd, queue_size=10)  # Front left
+        self.m2_pub = rospy.Publisher("/ros_talon2/motor_percent", cmd, queue_size=10)  # Front right
+        self.m3_pub = rospy.Publisher("/ros_talon3/motor_percent", cmd, queue_size=10)  # Rear left
+        self.m4_pub = rospy.Publisher("/ros_talon4/motor_percent", cmd, queue_size=10)  # Rear right
 
         self.linear_factor_percentage = 100
         self.angular_factor_percentage = 30
@@ -49,13 +44,12 @@ class LowLevelControlNode(object):
         self.linear_gain = 0.8
 
         # Subscribe to joystick
-        self.twist_sub = rospy.Subscriber(
-            '/cmd_vel', Twist, self.twist_callback)
+        self.twist_sub = rospy.Subscriber("/cmd_vel", Twist, self.twist_callback)
 
         rospy.sleep(1)
 
         # Init command loop
-        rospy.Timer(rospy.Duration(1/50), self.send_cmd_callback)
+        rospy.Timer(rospy.Duration(1 / 50), self.send_cmd_callback)
 
     def run(self):
         rospy.spin()
@@ -89,7 +83,7 @@ class LowLevelControlNode(object):
         linear_part = -msg.linear.x * self.linear_factor_percentage
         angular_part = msg.angular.z * self.angular_factor_percentage * self.angular_gain
 
-        self.l_cmd = self.linear_gain * (- linear_part - angular_part)
+        self.l_cmd = self.linear_gain * (-linear_part - angular_part)
         self.r_cmd = self.linear_gain * (linear_part - angular_part)
 
     def send_cmd(self):
@@ -97,12 +91,12 @@ class LowLevelControlNode(object):
         Publishes commands
         """
         # Left wheels
-        self.m1_pub.publish(int(self.l_cmd))
-        self.m3_pub.publish(int(self.l_cmd))
+        self.m1_pub.publish(cmd(cmd_mode=0, cmd=self.l_cmd))
+        self.m3_pub.publish(cmd(cmd_mode=0, cmd=self.l_cmd))
 
         # Right wheels
-        self.m2_pub.publish(int(self.r_cmd))
-        self.m4_pub.publish(int(self.r_cmd))
+        self.m2_pub.publish(cmd(cmd_mode=0, cmd=self.r_cmd))
+        self.m4_pub.publish(cmd(cmd_mode=0, cmd=self.r_cmd))
 
     def on_shutdown(self):
         """
@@ -113,7 +107,7 @@ class LowLevelControlNode(object):
         self.send_cmd()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         node = LowLevelControlNode()
         node.run()
