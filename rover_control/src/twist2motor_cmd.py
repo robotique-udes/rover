bc7871c04cd1c134c28176f8a3993c704660a147
Pmichaud69 <micp1402@usherbrooke.ca>
2023-01-20 17:54:17 -0500

    fixed msg type from int8 to ros_talon/cmd and add script for CAN config

--- rover_control/src/twist2motor_cmd.py
+++ rover_control/src/twist2motor_cmd.py
@@ -22,6 +22,7 @@ import rospy
 import numpy as np
 from std_msgs.msg import Int32
 from geometry_msgs.msg import Twist
+from ros_talon.msg import cmd
 
 
 class LowLevelControlNode(object):
@@ -35,13 +36,13 @@ class LowLevelControlNode(object):
 
         # Init publishers
         self.m1_pub = rospy.Publisher(
-            '/ros_talon1/cmd', Int32, queue_size=10)  # Front left
+            '/ros_talon1/cmd', cmd, queue_size=10)  # Front left
         self.m2_pub = rospy.Publisher(
-            '/ros_talon2/cmd', Int32, queue_size=10)  # Front right
+            '/ros_talon2/cmd', cmd, queue_size=10)  # Front right
         self.m3_pub = rospy.Publisher(
-            '/ros_talon3/cmd', Int32, queue_size=10)  # Rear left
+            '/ros_talon3/cmd', cmd, queue_size=10)  # Rear left
         self.m4_pub = rospy.Publisher(
-            '/ros_talon4/cmd', Int32, queue_size=10)  # Rear right
+            '/ros_talon4/cmd', cmd, queue_size=10)  # Rear right
 
         self.linear_factor_percentage = 100
         self.angular_factor_percentage = 30
@@ -96,13 +97,26 @@ class LowLevelControlNode(object):
         """
         Publishes commands
         """
+
+        msgL = cmd()
+        msgR = cmd()
+
+
+        #Need to define cmd_mode
+        msgL.cmd = float(self.l_cmd)
+        msgL.cmd_mode = (int(0))
+
+        msgR.cmd = float(self.r_cmd)
+        msgR.cmd_mode = int(0)
+
+
         # Left wheels
-        self.m1_pub.publish(int(self.l_cmd))
-        self.m3_pub.publish(int(self.l_cmd))
+        self.m1_pub.publish(msgL)
+        self.m3_pub.publish(msgL)
 
         # Right wheels
-        self.m2_pub.publish(int(self.r_cmd))
-        self.m4_pub.publish(int(self.r_cmd))
+        self.m2_pub.publish(msgR)
+        self.m4_pub.publish(msgR)
 
     def on_shutdown(self):
         """
