ae957056c4eb3ad9d0672eaf56a5256ddada5ba4
Pmichaud69 <93682089+Pmichaud69@users.noreply.github.com>
2023-02-01 23:19:10 -0500

    Bug fix attemp
    Set a fixed update rate
    Added Lock
    changed LCD for QDoubleSpinBox

--- rover_arm_gui/src/rover_arm_gui/rover_arm_gui_widget.py
+++ rover_arm_gui/src/rover_arm_gui/rover_arm_gui_widget.py
@@ -15,21 +15,23 @@ from rover_arm.msg import feedback
 from rover_arm.msg import arm_gui_cmd
 from std_srvs.srv import SetBool, SetBoolResponse
 import rosservice
+from threading import Lock
 
 
 #Constantes
 JOINT_NUMBER = 4
+FEEDBACK_CALLBACK_FREQUENCY = 100 #Hz
 
 style_default = ""
 style_Selected = "color: white; background-color: green"
 style_Disable = "color: white; background-color: grey"
 style_Limiting = "color: black; background-color: yellow"
+style_Warn = "color: black; background-color: rgb(255, 124, 0);"
 STANDBY = 0
 RUN = 1
 CALIB = 2
 
 class RoverArmGuiWidget(QtWidgets.QWidget):
-
     j1_enable_state: bool = 1
     j2_enable_state: bool = 1
     j3_enable_state: bool = 1
@@ -39,10 +41,12 @@ class RoverArmGuiWidget(QtWidgets.QWidget):
     img_keybinding_state: bool = 0
     curr_state = RUN
     joy_demux_state: bool = 0
-    
+
     arm_gui_cmd_msg = arm_gui_cmd()
 
-    def __init__(self):        
+    def __init__(self):
+        
+        self.lock = Lock()        
         super(RoverArmGuiWidget, self).__init__()
 
         ui_file = os.path.join(rospkg.RosPack().get_path('rover_arm_gui'), 'resource', 'rover_arm_gui.ui')
@@ -52,8 +56,10 @@ class RoverArmGuiWidget(QtWidgets.QWidget):
         self.launch: roslaunch.ROSLaunch = roslaunch.scriptapi.ROSLaunch()
         self.launch.start()
 
+        self.lastTimeFeedback = rospy.Time.now()
+
         self.is_active = False
-        self.feedback_sub = rospy.Subscriber('rover_arm_feedback', feedback, self.feedback_callback)
+        self.feedback_sub = rospy.Subscriber('rover_arm_feedback', feedback, self.feedback_callback, queue_size=1)
         self.pub_arm_gui_cmd = rospy.Publisher('arm_gui_cmd', arm_gui_cmd, queue_size=1)
         self.set_arm_joy = rospy.ServiceProxy('set_arm_joy', SetBool)
 
@@ -71,90 +77,101 @@ class RoverArmGuiWidget(QtWidgets.QWidget):
         self.launch_all_views.released.connect(self.launch_all_views_released_callback)
         self.show_keybinding.released.connect(self.show_keybinding_callback)
         self.ctrl_joy_demux.released.connect(self.ctrl_joy_demux_callback)
-
+        
         self.state_standby.clicked.connect(self.state_standby_callback)
         self.state_run.clicked.connect(self.state_run_callback)
         self.state_calib.clicked.connect(self.state_calib_callback)
 
     def feedback_callback(self, data):
-        #________________________________________
-        #Angles LCD displays
-        for i in range(len(self.currAngles)):
-            self.currAngles[i].display(data.angles[i])
+        if self.lastTimeFeedback + rospy.Duration(1.0/FEEDBACK_CALLBACK_FREQUENCY) > rospy.Time.now() :
+            return
 
-        #________________________________________
-        #Speed LCD displays    
-        for i in range(len(self.currSpeeds)):
-            self.currSpeeds[i].display(data.vitesses[i])
-        
-        #________________________________________
-        #Control Mode
-        if data.ctrl_mode:
-            self.jogMode.setText("Joint")
-        else:
-            self.jogMode.setText("Cartesian")
+        #Testing
+        #self.shadowButton.
 
-        #________________________________________
-        #Singular Matrix
-        if data.singular_matrix:
-            self.singularMatrix.setText("Singular Matrix --> Jog in joint")
-        else:
-            self.singularMatrix.setText("")
+        self.lastTimeFeedback = rospy.Time.now()
+        with self.lock:
+            #________________________________________
+            #Angles LCD displays
+            for i in range(len(self.currAngles)):
+                self.currAngles[i].setValue(data.angles[i])
+                #rospy.loginfo("Displaying angle")
+            
+            #________________________________________
+            #Speed LCD displays    
+            for i in range(len(self.currSpeeds)):
+                self.currSpeeds[i].setValue(data.vitesses[i])
+            
+            #________________________________________
+            #Control Mode
+            if data.ctrl_mode:
+                self.jogMode.setText("Joint")
+            else:
+                self.jogMode.setText("Cartesian")
 
-        #________________________________________
-        #Style init
-        jointLabelsStyle = []
-        for i in range(JOINT_NUMBER):
-            jointLabelsStyle.append(style_default)
+            #________________________________________
+            #Singular Matrix
+            if data.singular_matrix:
+                self.singularMatrix.setText("Singular Matrix --> Jog in joint")
+            else:
+                self.singularMatrix.setText("")
 
-        #________________________________________
-        #Selected join
-        if data.ctrl_mode:
+            #________________________________________
+            #Style init
+            jointLabelsStyle = []
+            for i in range(JOINT_NUMBER):
+                jointLabelsStyle.append(style_default)
+
+
+
+            #________________________________________
+            #Selected join
+            if data.ctrl_mode:
+                for i in range(len(jointLabelsStyle)):
+                    if data.current_joint == i+1:
+                        jointLabelsStyle[i] = style_Selected
+
+            #________________________________________
+            #Enable
             for i in range(len(jointLabelsStyle)):
-                if data.current_joint == i+1:
-                    jointLabelsStyle[i] = style_Selected
+                if not data.enable[i]:
+                    jointLabelsStyle[i]=style_Disable
 
-        #________________________________________
-        #Enable
-        for i in range(len(jointLabelsStyle)):
-            if not data.enable[i]:
-                jointLabelsStyle[i]=style_Disable
-
-        #________________________________________
-        #Style update
-        self.moteur1Label.setStyleSheet(jointLabelsStyle[0])
-        self.moteur2Label.setStyleSheet(jointLabelsStyle[1])
-        self.moteur3Label.setStyleSheet(jointLabelsStyle[2])
-        self.moteur4Label.setStyleSheet(jointLabelsStyle[3])
-
-        #________________________________________
-        #Limiting
-        speedLabelsStyle = []
-        for i in range(len(self.speedLabels)):
-            speedLabelsStyle.append(style_default)
-
-        if data.limiteur:
+            #________________________________________
+            #Style update
+            self.moteur1Label.setStyleSheet(jointLabelsStyle[0])
+            self.moteur2Label.setStyleSheet(jointLabelsStyle[1])
+            self.moteur3Label.setStyleSheet(jointLabelsStyle[2])
+            self.moteur4Label.setStyleSheet(jointLabelsStyle[3])
+            #________________________________________
+            #Limiting
+            speedLabelsStyle = []
             for i in range(len(self.speedLabels)):
-                if abs(int(data.vitesses[i])) == 20:
-                    speedLabelsStyle[i] = style_Limiting
+                speedLabelsStyle.append(style_default)
 
-        for i in range(len(self.speedLabels)):
-            self.speedLabels[i].setStyleSheet(speedLabelsStyle[i])
-        
-        #________________________________________
-        #Speed Multiplicator
-        self.currSpeedMultiplicator.display(data.speed_multiplier)
+            if data.limiteur:
+                for i in range(len(self.speedLabels)):
+                    if abs(int(data.vitesses[i])) == 20:
+                        speedLabelsStyle[i] = style_Limiting
+
+            for i in range(len(self.speedLabels)):
+                self.speedLabels[i].setStyleSheet(speedLabelsStyle[i])
+            
+            #________________________________________
+            #Speed Multiplicator
+            self.currSpeedMultiplicator.setValue(data.speed_multiplier)
 
         self.publish_command();
 
     def publish_command(self):
-        self.arm_gui_cmd_msg.enable[0] = self.j1_enable_state;
-        self.arm_gui_cmd_msg.enable[1] = self.j2_enable_state;
-        self.arm_gui_cmd_msg.enable[2] = self.j3_enable_state;
-        self.arm_gui_cmd_msg.enable[3] = self.j4_enable_state;
-        self.arm_gui_cmd_msg.state = self.curr_state;
-        
-        self.pub_arm_gui_cmd.publish(self.arm_gui_cmd_msg)
+        with self.lock:
+            self.arm_gui_cmd_msg.enable[0] = self.j1_enable_state;
+            self.arm_gui_cmd_msg.enable[1] = self.j2_enable_state;
+            self.arm_gui_cmd_msg.enable[2] = self.j3_enable_state;
+            self.arm_gui_cmd_msg.enable[3] = self.j4_enable_state;
+            self.arm_gui_cmd_msg.state = self.curr_state;
+            
+            self.pub_arm_gui_cmd.publish(self.arm_gui_cmd_msg)
 
     def j1_enable_released_callback(self):
         if self.j1_enable_state:
@@ -237,11 +254,18 @@ class RoverArmGuiWidget(QtWidgets.QWidget):
 
     def ctrl_joy_demux_callback(self):
         if '/set_arm_joy' in rosservice.get_service_list():
+            self.ctrl_joy_demux.setText("Joy to arm")
+
+            rospy.wait_for_service('set_arm_joy')
             if self.joy_demux_state:
                 if(self.set_arm_joy(False)):
                     self.joy_demux_state = False;
+                    self.ctrl_joy_demux.setStyleSheet(style_default)
             else:
                 if(self.set_arm_joy(True)):
                     self.joy_demux_state = True;
+                    self.ctrl_joy_demux.setStyleSheet(style_Selected)
         else:
             rospy.logerr("Service 'set_arm_joy' not available")
+            self.ctrl_joy_demux.setText("'set_arm_joy' not available")
+            self.ctrl_joy_demux.setStyleSheet(style_Warn)
