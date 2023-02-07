dd450b5c814996528d427141938a2fbec2893c37
AudeR <rega2002@usherbrooke.ca>
2023-02-07 18:39:36 -0500

    Auto stash before merge of "relay_control" and "origin/relay_control"

--- rover_control/src/relay_board.py
+++ rover_control/src/relay_board.py
@@ -4,15 +4,15 @@ import rospy
 from robotnik_msgs.srv import set_digital_output
 import time
 
-def activateRelay10Hz():
+def activateRelay1sec(relay):
     rospy.wait_for_service('/rly_08_node/set_digital_outputs')
     outputs = rospy.ServiceProxy('/rly_08_node/set_digital_outputs', set_digital_output)
-    outputs(1, True)
+    outputs(relay, True)
     time.sleep(1)
-    outputs(1, False)
+    outputs(relay, False)
 
     return
 
 
 if __name__ == "__main__":
-    activateRelay10Hz()
+    activateRelay1sec(1)
