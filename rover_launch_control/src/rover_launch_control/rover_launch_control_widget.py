498a4a3d705f0fddbf7e6d7362754be690013414
Pmichaud69 <93682089+Pmichaud69@users.noreply.github.com>
2023-02-07 17:38:20 -0500

    Created cam_control_pkg
    Needs name change

--- rover_launch_control/src/rover_launch_control/rover_launch_control_widget.py
+++ rover_launch_control/src/rover_launch_control/rover_launch_control_widget.py
@@ -5,26 +5,21 @@ import rospkg
 import rospy
 import roslaunch
 import os
+from rospkg import RosPack
 from python_qt_binding import loadUi
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QLabel, QPushButton, QAbstractButton
-from threading import Lock
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QObject
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import QShortcut, QSlider, QLCDNumber, QLabel, QPushButton, QFrame, QListView
+import rosnode
 
 #Styling "MACROS"
-STYLE_DEFAULT = ""
-STYLE_SELECTED = "color: white; background-color: green"
-STYLE_DISABLE = "color: white; background-color: grey"
-STYLE_LIMITING = "color: black; background-color: yellow"
-STYLE_WARN = "color: black; background-color: rgb(255, 124, 0);"
+style_default = ""
+style_Selected = "color: white; background-color: green"
+style_Disable = "color: white; background-color: grey"
+style_Limiting = "color: black; background-color: yellow"
+style_Warn = "color: black; background-color: rgb(255, 124, 0);"
 
-class LaunchInterface():
-    def __init__(self, uuid, pkg_name: str, launchfile_name: str):
-        self.name_pkg = pkg_name
-        self.name_launchfile = launchfile_name
-        self.uuid = uuid
-        self.launchfile = roslaunch.rlutil.resolve_launch_arguments([pkg_name, launchfile_name + '.launch'])
-        self.launch_handler = roslaunch.parent.ROSLaunchParent(uuid, self.launchfile)
-        self.launch_handler_started: bool = False
 
 class RoverLaunchControlWidget(QtWidgets.QWidget):
     
@@ -35,25 +30,10 @@ class RoverLaunchControlWidget(QtWidgets.QWidget):
         loadUi(ui_file, self)
         self.setObjectName('RoverLaunchControlWidget')
 
-        self.lock = Lock()
+        self.is_active = False
 
-        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-        roslaunch.configure_logging(uuid)
+        self.fps_slider.sliderReleased.connect(self.updateValueFps)
 
-
-        #Button callbacks
-        joints_interface = LaunchInterface(uuid= uuid, pkg_name= 'rover_arm', launchfile_name='joints')
-        self.pb_joints.released.connect(lambda: self.launchFile(joints_interface, self.pb_joints))
-
-    def launchFile(self, launch_interface: LaunchInterface, button: QPushButton):
-        with self.lock:
-            if not launch_interface.launch_handler_started:
-                launch_interface.launch_handler = roslaunch.parent.ROSLaunchParent(launch_interface.uuid, launch_interface.launchfile)
-                launch_interface.launch_handler_started = True
-                button.setStyleSheet(STYLE_SELECTED)
-                launch_interface.launch_handler.start()
-            else:
-                launch_interface.launch_handler_started = False
-                button.setStyleSheet(STYLE_DEFAULT)
-                launch_interface.launch_handler.shutdown()
-                rospy.logwarn(launch_interface.name_pkg + ' ' + launch_interface.name_launchfile + '.launch shutdowned')
+    def updateValueFps(self):
+        size = self.fps_slider.value()
+        print(rospy.get_param_names())
