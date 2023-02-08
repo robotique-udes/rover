09fa1c2b1f6624090e5a349bb9bbc706024c8d67
Rovus_Ordi_Studio <micp1402@usherbrooke.ca>
2023-02-07 20:40:33 -0500

    Added rosparams control

--- rover_launch_control/src/rover_launch_control/rover_launch_control_widget.py
+++ rover_launch_control/src/rover_launch_control/rover_launch_control_widget.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QObject
 from PyQt5.QtGui import QKeySequence
 from PyQt5.QtWidgets import QShortcut, QSlider, QLCDNumber, QLabel, QPushButton, QFrame, QListView
-import rosnode
+import dynamic_reconfigure.client
 
 #Styling "MACROS"
 style_default = ""
@@ -20,6 +20,8 @@ style_Disable = "color: white; background-color: grey"
 style_Limiting = "color: black; background-color: yellow"
 style_Warn = "color: black; background-color: rgb(255, 124, 0);"
 
+#DEFINE
+CLIENT_TIMEOUT: int = 30
 
 class RoverLaunchControlWidget(QtWidgets.QWidget):
     
@@ -29,11 +31,12 @@ class RoverLaunchControlWidget(QtWidgets.QWidget):
         ui_file = os.path.join(rospkg.RosPack().get_path('rover_launch_control'), 'resource', 'rover_launch_control.ui')
         loadUi(ui_file, self)
         self.setObjectName('RoverLaunchControlWidget')
+        # self.is_active = False
 
-        self.is_active = False
+        self.cam_sonix_client = dynamic_reconfigure.client.Client("/cam_sonix/image/theora")
 
         self.fps_slider.sliderReleased.connect(self.updateValueFps)
 
     def updateValueFps(self):
-        size = self.fps_slider.value()
-        print(rospy.get_param_names())
+        size: int = self.fps_slider.value()
+        print(self.cam_sonix_client.get_configuration(timeout=CLIENT_TIMEOUT))
