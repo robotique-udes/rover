628caff78b4b791dbd564351b78be660dedd88ff
Pmichaud69 <93682089+Pmichaud69@users.noreply.github.com>
2023-05-22 14:00:53 -0400

    Added protection when changing joy topics

--- rover_control/src/joy_demux.py
+++ rover_control/src/joy_demux.py
@@ -5,7 +5,7 @@ from __future__ import print_function, division, unicode_literals, absolute_impo
 from email import message
 
 import rospy
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Header
 from sensor_msgs.msg import Joy
 from std_srvs.srv import SetBool, SetBoolResponse
 from threading import Lock
@@ -13,6 +13,15 @@ from threading import Lock
 
 class JoyDemuxNode:
     def __init__(self):
+        self._is_arm_joy_lock = Lock()
+
+        self._is_arm_joy = rospy.get_param("~start_as_arm_joy", False)
+        self._toggle_arm_joy_btn = rospy.get_param("~toggle_arm_joy_btn", 3)
+        self._allow_toggle_with_button = rospy.get_param("~allow_toggle_with_button", False)
+        self._last_arm_joy_btn_state = False
+
+        self.joy_state_pub_rate = rospy.Timer(rospy.Duration(1), self.publish_joy_state)
+
         self.joy_sub = rospy.Subscriber("/joy", Joy, self.joy_cb, queue_size=1)
         self.rover_joy_pub = rospy.Publisher("/rover_joy", Joy, queue_size=1)
         self.arm_joy_pub = rospy.Publisher("/arm_joy", Joy, queue_size=1)
@@ -20,14 +29,6 @@ class JoyDemuxNode:
 
         self.set_arm_joy_srv = rospy.Service("set_arm_joy", SetBool, self.set_arm_joy_cb)
         self.get_arm_joy_srv = rospy.Service("get_arm_joy", SetBool, self.get_arm_joy_cb)
-
-        self._is_arm_joy_lock = Lock()
-        self._is_arm_joy = rospy.get_param("~start_as_arm_joy", False)
-        self._toggle_arm_joy_btn = rospy.get_param("~toggle_arm_joy_btn", 3)
-        self._allow_toggle_with_button = rospy.get_param("~allow_toggle_with_button", True)
-        self._last_arm_joy_btn_state = False
-
-        self.joy_state_pub_rate = rospy.Timer(rospy.Duration(1), self.publish_joy_state)
 
     def joy_cb(self, msg):
         with self._is_arm_joy_lock:
@@ -48,7 +49,9 @@ class JoyDemuxNode:
     def set_arm_joy_cb(self, req):
         with self._is_arm_joy_lock:
             self._is_arm_joy = req.data
-        return SetBoolResponse(success=True, message="")
+            self.send_zeros()
+
+            return SetBoolResponse(success=True, message="")
 
     def get_arm_joy_cb(self, req):
         with self._is_arm_joy_lock:
@@ -57,6 +60,16 @@ class JoyDemuxNode:
     def publish_joy_state(self, _):
         with self._is_arm_joy_lock:
             self.joy_state_pub.publish(self._is_arm_joy)
+
+    # Sends msg of zeros to every joy topic for protection
+    def send_zeros(self):
+        msg = Joy()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = "joy_demux_protection"
+        msg.axes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        msg.buttons = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.arm_joy_pub.publish(msg)
+        self.rover_joy_pub.publish(msg)
 
 
 if __name__ == "__main__":
