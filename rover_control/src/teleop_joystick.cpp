97d26caf6f942182895f214a8cb48ea9ef0368a3
Pmichaud69 <93682089+Pmichaud69@users.noreply.github.com>
2023-06-12 21:56:10 -0400

    Comments and new crawler mode

--- rover_control/src/teleop_joystick.cpp
+++ rover_control/src/teleop_joystick.cpp
@@ -1,39 +1,40 @@
 // =============================================================================
 // teleop_joystick node
 // =============================================================================
-// This node translate joy msgs into a in-house "motor_cmd" sent through the 
+// This node translate joy msgs into a in-house "motor_cmd" sent through the
 // antenna
 //
 // Control modes:
 //    Speed:
+//      --> Crawler
 //      --> Normal
 //      --> Turbo
-//      --> Crawler /* TODO */
 //    Turn:
 //      --> Car (wheels turns in the same direction at different speeds)
 //      --> Tank (wheels turns in opposite direction at the same speed)
-//     
-// Available parameters are :
+//
+// Available parameters:
 //    button_enable: 4 --> Joy Mapping (button)
 //    axis_linear: 1 --> Joy Mapping (axes)
 //    axis_angular: 0 --> Joy Mapping (axes)
 //    button_enable_tank_mode: 2 --> Joy Mapping (button)
+//    button_crawler: /* FIND DEFAULT */ --> Joy Mapping
 //    button_turbo: 5 --> Joy Mapping (button)
-//    
+//
 //    speed_factor_crawler: 0.01
+//      --> Commands will be mapped: [0.0, 1.0] -> [0.0; speed_factor_crawler]
 //    speed_factor_normal: 0.25
-//      --> Commands will be mapped: [0.0, 1.0] -> [0.0; speed_factor_normal]  
-//    speed_factor_turbo: 1.0 
+//      --> Commands will be mapped: [0.0, 1.0] -> [0.0; speed_factor_normal]
+//    speed_factor_turbo: 1.0
 //      --> Overwrites normal speed: [0.0, 1.0] -> [0.0; speed_factor_turbo]
-//    smallest_radius: 0.30 
-//      --> While turning in car mode, it's the smallest speed in percent of fast wheel's
-//      --> speed the "slow" wheels can turn. 
-//      --> Ex while turning with smallest_radius: 0.30: At full speed, speed of wheels
-//      --> are in range of: ([0.30; 1.0]*speed_factor)
+//    smallest_radius: 0.30
+//      --> While turning in car mode, it's the smallest speed in percent of
+//          fast wheel's speed that the "slow" wheels can rotate.
+//          Ex: while turning with smallest_radius: 0.30: At full speed, speed
+//          of wheels are in range of: ([0.30; 1.0]*speed_factor)
 //
 // =============================================================================
 // =============================================================================
-
 
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
@@ -41,14 +42,18 @@
 
 #define LINEAR_INPUT joy_msg->axes[m_u_INDEX_AXIS_LINEAR]
 #define ANGULAR_INPUT joy_msg->axes[m_u_INDEX_AXIS_ANGULAR]
-#define TANK_MODE joy_msg->buttons[m_u_INDEX_BUTTON_ENABLE_TANK_MODE]
-#define TURBO_MODE joy_msg->buttons[m_u_INDEX_BUTTON_TURBO]
+#define MODE_TANK joy_msg->buttons[m_u_INDEX_BUTTON_ENABLE_TANK_MODE]
+#define MODE_CRAWLER joy_msg->buttons[m_u_INDEX_BUTTON_CRAWLER]
+#define MODE_TURBO joy_msg->buttons[m_u_INDEX_BUTTON_TURBO]
 
 float map(float x, float in_min, float in_max, float out_min, float out_max);
 
 class TeleopJoystick
 {
 public:
+    // =========================================================================
+    // Constructor / Destructor
+    // =========================================================================
     TeleopJoystick()
     {
         m_nh = ros::NodeHandle();
@@ -62,6 +67,11 @@ public:
         getParams();
     }
 
+    ~TeleopJoystick(){}
+
+    // =========================================================================
+    // Public Methods
+    // =========================================================================
     void Run()
     {
         while (!ros::isShuttingDown())
@@ -74,6 +84,9 @@ public:
     }
 
 private:
+    // =========================================================================
+    // Private members
+    // =========================================================================
     ros::NodeHandle m_nh;
     ros::Subscriber m_sub_joy;
     ros::Publisher m_pub_motor_cmd;
@@ -83,13 +96,18 @@ private:
     uint8_t m_u_INDEX_BUTTON_ENABLE_TANK_MODE;
     uint8_t m_u_INDEX_AXIS_LINEAR;
     uint8_t m_u_INDEX_AXIS_ANGULAR;
+    uint8_t m_u_INDEX_BUTTON_CRAWLER;
 
     float m_f_control_map_factor;
     bool m_b_control_mode_is_tank;
 
+    float m_f_SPEED_FACTOR_CRAWLER;
     float m_f_SPEED_FACTOR_NORMAL;
     float m_f_SPEED_FACTOR_TURBO;
 
+    // =========================================================================
+    // Private methods
+    // =========================================================================
     // The magic happens here
     void cbJoy(const sensor_msgs::Joy::ConstPtr &joy_msg)
     {
@@ -102,7 +120,7 @@ private:
             float f_speed_left_motor = LINEAR_INPUT;
             float f_speed_right_motor = LINEAR_INPUT;
 
-            if (!TANK_MODE)
+            if (!MODE_TANK)
             {
                 if (ANGULAR_INPUT > 0.0f)
                 {
@@ -119,7 +137,12 @@ private:
                 f_speed_right_motor = ANGULAR_INPUT;
             }
 
-            if (TURBO_MODE)
+            if (MODE_CRAWLER)
+            {
+                f_speed_left_motor *= m_f_SPEED_FACTOR_CRAWLER;
+                f_speed_right_motor *= m_f_SPEED_FACTOR_CRAWLER;
+            }
+            else if (MODE_TURBO)
             {
                 f_speed_left_motor *= m_f_SPEED_FACTOR_TURBO;
                 f_speed_right_motor *= m_f_SPEED_FACTOR_TURBO;
@@ -147,6 +170,8 @@ private:
         m_u_INDEX_AXIS_LINEAR = m_nh.param<int>(s_param_prefix + "axis_linear", 1);
         m_u_INDEX_AXIS_ANGULAR = m_nh.param<int>(s_param_prefix + "axis_angular", 0);
         m_u_INDEX_BUTTON_ENABLE_TANK_MODE = m_nh.param<int>(s_param_prefix + "button_enable_tank_mode", 2);
+        m_u_INDEX_BUTTON_CRAWLER = m_nh.param<float>(s_param_prefix + "button_enable_crawler_mode", 69 /* TODO */);
+        m_f_SPEED_FACTOR_CRAWLER = m_nh.param<float>(s_param_prefix + "speed_factor_crawler", 0.01);
         m_f_SPEED_FACTOR_NORMAL = m_nh.param<float>(s_param_prefix + "speed_factor_normal", 0.5);
         m_f_SPEED_FACTOR_TURBO = m_nh.param<float>(s_param_prefix + "speed_factor_turbo", 1.0);
     }
@@ -158,5 +183,6 @@ int main(int argc, char *argv[])
 
     TeleopJoystick teleop_joystick = TeleopJoystick();
     teleop_joystick.Run();
+
     return 0;
 }
