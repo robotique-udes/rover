297186cac2ee825cec452957e0447727b4c21560
Pmichaud69 <93682089+Pmichaud69@users.noreply.github.com>
2023-07-08 13:34:23 -0400

    Added low pass on joy input to simulate acceleration + fixed joy @ 115hz

--- rover_control/src/cmd_2_motors.cpp
+++ rover_control/src/cmd_2_motors.cpp
@@ -1,11 +1,13 @@
 // =============================================================================
 // cmd_2_motors_node
 // =============================================================================
-// This node receive thru the antena a msg of type motor_cmd and splits it 
+// This node receive thru the antena a msg of type motor_cmd and splits it
 // between the 4 motor topics. It is also where the rover watchdog is located.
-// If no signal comes from the hearbeat, this node will send column of zeros to
-// each motors.
+// If no signal comes from the hearbeat or no signal from joy(cmd_motor), this
+// node will send column of zeros to each motors.
 //
+// There's a low pass filter (moving average) on msg send to motors to create
+// software acceleration to limit jerkiness from the rover.
 // =============================================================================
 // =============================================================================
 
@@ -15,12 +17,75 @@
 #include "rover_control_msgs/motor_cmd.h"
 #include "ros_talon/cmd.h"
 
+class MovingAverage
+{
+private:
+    // =========================================================================
+    // Private Members
+    // =========================================================================
+    uint8_t AVG_SIZE;
+    uint8_t cursor = 0;
+    double *values;
+    double total = 0.0f;
+    double avg = 0.0f;
+
+public:
+    // =========================================================================
+    // Constructor / Destructor
+    // =========================================================================
+    MovingAverage(uint8_t avg_size = static_cast<uint8_t>(50u))
+    {
+        AVG_SIZE = avg_size;
+        values = new double[AVG_SIZE];
+
+        for (uint8_t i = 0; i < AVG_SIZE; i++)
+        {
+            values[i] = 0.0f;
+        }
+    }
+
+    ~MovingAverage()
+    {
+        delete[] values;
+    }
+
+    // =========================================================================
+    // Public Methods
+    // =========================================================================
+    double addValue(double value)
+    {
+        total -= values[cursor];
+        values[cursor] = value;
+        total += values[cursor];
+        cursor = (cursor + 1) == AVG_SIZE ? 0 : (cursor + 1);
+        avg = total / static_cast<double>(AVG_SIZE);
+        return avg;
+    };
+
+    double getAverage() { return avg; };
+};
+
 struct stMotorsSpeed
 {
-    float f_FL = 0.0f;
-    float f_FR = 0.0f;
-    float f_RL = 0.0f;
-    float f_RR = 0.0f;
+    MovingAverage f_FL;
+    MovingAverage f_FR;
+    MovingAverage f_RL;
+    MovingAverage f_RR;
+
+    // =========================================================================
+    // Constructor / Destructor
+    // =========================================================================
+    stMotorsSpeed(uint8_t window = static_cast<uint8_t>(10u)) : f_FL(window), f_FR(window), f_RL(window), f_RR(window)
+    {
+    }
+};
+
+struct stMotorsParam
+{
+    double f_FL = 1.0f;
+    double f_FR = 1.0f;
+    double f_RL = 1.0f;
+    double f_RR = 1.0f;
 };
 
 class CMD2Motors
@@ -29,7 +94,7 @@ public:
     // =========================================================================
     // Constructor / Destructor
     // =========================================================================
-    CMD2Motors()
+    CMD2Motors() : m_st_motors_speed(static_cast<uint8_t>(m_nh.param<int>(ros::this_node::getName() + "/low_pass_115hz_window", 1)))
     {
         m_nh = ros::NodeHandle();
         m_sub_heartbeat = m_nh.subscribe<std_msgs::Empty>("base_heartbeat", 1, &CMD2Motors::CBWatchdog, this);
@@ -39,6 +104,16 @@ public:
         m_pub_motor_RL = m_nh.advertise<ros_talon::cmd>("/ros_talon3/cmd", 1);
         m_pub_motor_RR = m_nh.advertise<ros_talon::cmd>("/ros_talon4/cmd", 1);
         m_pub_watchdog_state = m_nh.advertise<std_msgs::Bool>("rover_watchdog_is_alive", 1);
+
+        m_st_motors_dir.f_FL = (m_nh.param<int>(ros::this_node::getName() + "/reverse_FL", false) == true) ? -1.0 : 1.0;
+        m_st_motors_dir.f_FR = (m_nh.param<int>(ros::this_node::getName() + "/reverse_FR", false) == true) ? -1.0 : 1.0;
+        m_st_motors_dir.f_RL = (m_nh.param<int>(ros::this_node::getName() + "/reverse_RL", false) == true) ? -1.0 : 1.0;
+        m_st_motors_dir.f_RR = (m_nh.param<int>(ros::this_node::getName() + "/reverse_RR", false) == true) ? -1.0 : 1.0;
+
+        m_st_motors_strength.f_FL = (m_nh.param<double>(ros::this_node::getName() + "/strength_FL", 1.0));
+        m_st_motors_strength.f_FR = (m_nh.param<double>(ros::this_node::getName() + "/strength_FR", 1.0));
+        m_st_motors_strength.f_RL = (m_nh.param<double>(ros::this_node::getName() + "/strength_RL", 1.0));
+        m_st_motors_strength.f_RR = (m_nh.param<double>(ros::this_node::getName() + "/strength_RR", 1.0));
     }
 
     // =========================================================================
@@ -55,9 +130,6 @@ public:
             ros::spinOnce();
             sendMotorCommands();
 
-            // ROS_WARN("Time to beat is about: %f \tCurrent time is: %f",
-            //          (t_last_time + dur_watchdog_reset_duration).toSec(),
-            //          ros::Time::now().toSec());
             if ((t_last_time + dur_watchdog_reset_duration) < ros::Time::now())
             {
                 t_last_time = ros::Time::now();
@@ -79,8 +151,11 @@ private:
     ros::Publisher m_pub_motor_RR;
     ros::Publisher m_pub_watchdog_state;
     stMotorsSpeed m_st_motors_speed;
+    stMotorsParam m_st_motors_dir;
+    stMotorsParam m_st_motors_strength;
     bool m_b_watchdog_is_alive = false;
     bool m_b_com_is_alive = false;
+    bool m_b_joy_is_alive = false;
 
     // =========================================================================
     // Private methods
@@ -92,21 +167,23 @@ private:
 
     void resetWatchdog()
     {
-        std_msgs::Bool msg; 
-        msg.data = m_b_com_is_alive;
+        std_msgs::Bool msg;
+        msg.data = (m_b_com_is_alive && m_b_joy_is_alive);
 
-        m_b_watchdog_is_alive = m_b_com_is_alive;
+        m_b_watchdog_is_alive = (m_b_com_is_alive && m_b_joy_is_alive);
         m_b_com_is_alive = false;
+        m_b_joy_is_alive = false;
 
         m_pub_watchdog_state.publish(msg);
     }
 
     void CBMotorCMD(const rover_control_msgs::motor_cmdConstPtr &msg)
     {
-        m_st_motors_speed.f_FL = msg->front_left * 100.0f;
-        m_st_motors_speed.f_FR = msg->front_right * 100.0f;
-        m_st_motors_speed.f_RL = msg->rear_left * 100.0f;
-        m_st_motors_speed.f_RR = msg->rear_right * 100.0f;
+        m_b_joy_is_alive = true;
+        m_st_motors_speed.f_FL.addValue(msg->front_left * 100.0f);
+        m_st_motors_speed.f_FR.addValue(msg->front_right * 100.0f);
+        m_st_motors_speed.f_RL.addValue(msg->rear_left * 100.0f);
+        m_st_motors_speed.f_RR.addValue(msg->rear_right * 100.0f);
     }
 
     void sendMotorCommands()
@@ -118,11 +195,21 @@ private:
 
         if (m_b_watchdog_is_alive)
         {
-            cmd_motor_FL.cmd = -m_st_motors_speed.f_FL;
-            cmd_motor_FR.cmd = m_st_motors_speed.f_FR;
-            cmd_motor_RL.cmd = -m_st_motors_speed.f_RL;
-            cmd_motor_RR.cmd = m_st_motors_speed.f_RR;
+            cmd_motor_FL.cmd = m_st_motors_strength.f_FL * m_st_motors_dir.f_FL * m_st_motors_speed.f_FL.getAverage();
+            cmd_motor_FR.cmd = m_st_motors_strength.f_FR * m_st_motors_dir.f_FR * m_st_motors_speed.f_FR.getAverage();
+            cmd_motor_RL.cmd = m_st_motors_strength.f_RL * m_st_motors_dir.f_RL * m_st_motors_speed.f_RL.getAverage();
+            cmd_motor_RR.cmd = m_st_motors_strength.f_RR * m_st_motors_dir.f_RR * m_st_motors_speed.f_RR.getAverage();
         }
+        else
+        {
+            //Simulate joy rate which is 115 Hz
+            ros::Duration(1.0f/115.0f).sleep(); 
+            cmd_motor_FL.cmd = m_st_motors_dir.f_FL * m_st_motors_speed.f_FL.addValue(0.0);
+            cmd_motor_FR.cmd = m_st_motors_dir.f_FR * m_st_motors_speed.f_FR.addValue(0.0);
+            cmd_motor_RL.cmd = m_st_motors_dir.f_RL * m_st_motors_speed.f_RL.addValue(0.0);
+            cmd_motor_RR.cmd = m_st_motors_dir.f_RR * m_st_motors_speed.f_RR.addValue(0.0);
+        }
+
         m_pub_motor_FL.publish(cmd_motor_FL);
         m_pub_motor_FR.publish(cmd_motor_FR);
         m_pub_motor_RL.publish(cmd_motor_RL);
