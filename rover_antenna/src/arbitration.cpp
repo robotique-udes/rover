aef3e0e6bed89825dc5ad618d6c7fdcd4f3c0958
Aude Regnault <rega2002@usherbrooke.ca>
2024-01-14 20:50:22 -0500

    Creation of rover_antenna pkg
    creation of pkg and files for antenna control

---
+++ rover_antenna/src/arbitration.cpp
@@ -0,0 +1,82 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "rover_msgs/msg/joy.hpp"
+#include "rover_msgs/msg/antenna_cmd.hpp"
+#include "rovus_lib/macros.h"
+
+using namespace std::chrono_literals;
+
+class Arbitration : public rclcpp::Node
+{
+public:
+    Arbitration();
+    ~Arbitration() {}
+
+private:
+    void timer_callback()
+    {
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<rover_msgs::msg::AntennaCmd>::SharedPtr _sub_jog;
+    rclcpp::Subscription<rover_msgs::msg::AntennaCmd>::SharedPtr _sub_auto;
+    rclcpp::Publisher<rover_msgs::msg::AntennaCmd>::SharedPtr _pub_abtr;
+
+    void callbackJog(const rover_msgs::msg::AntennaCmd msg);
+    void callbackAuto(const rover_msgs::msg::AntennaCmd msg);
+    void sendCmd();
+
+    rover_msgs::msg::AntennaCmd cmd_jog;
+    rover_msgs::msg::AntennaCmd cmd_auto;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Arbitration>());
+    rclcpp::shutdown();
+    return 0;
+}
+
+Arbitration::Arbitration() : Node("arbitration")
+{
+    _sub_jog = this->create_subscription<rover_msgs::msg::AntennaCmd>("/base/antenna/cmd/in/teleop",
+                                                                     1,
+                                                                     [this](const rover_msgs::msg::AntennaCmd msg)
+                                                                     { callbackJog(msg); });
+                                                                     
+    _sub_auto = this->create_subscription<rover_msgs::msg::AntennaCmd>("/base/antenna/cmd/in/auto",
+                                                                     1,
+                                                                     [this](const rover_msgs::msg::AntennaCmd msg)
+                                                                     { callbackAuto(msg); });
+    _pub_abtr = this->create_publisher<rover_msgs::msg::AntennaCmd>("/base/antenna/cmd/out/goal", 1);
+}
+
+void Arbitration::callbackJog(const rover_msgs::msg::AntennaCmd msg)
+{
+    cmd_jog = msg;
+
+}
+
+void Arbitration::callbackAuto(const rover_msgs::msg::AntennaCmd msg)
+{
+    cmd_auto = msg;
+}
+
+void Arbitration::sendCmd()
+{
+    rover_msgs::msg::AntennaCmd cmd_abtr;
+    if (cmd_jog.speed != 0.0)
+    {
+        cmd_abtr = cmd_jog;
+    }
+    else 
+    {
+        cmd_abtr = cmd_auto;
+    }
+    _pub_abtr->publish(cmd_abtr);
+}
---
+++ rover_antenna/src/jog_antenna.cpp
@@ -0,0 +1,69 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "rover_msgs/msg/joy.hpp"
+#include "rover_msgs/msg/antenna_cmd.hpp"
+#include "rovus_lib/macros.h"
+
+#define MAX_SPEED 1.0 // deg/s
+
+using namespace std::chrono_literals;
+
+class JogAntenna : public rclcpp::Node
+{
+public:
+    JogAntenna();
+    ~JogAntenna() {}
+
+private:
+    void timer_callback()
+    {
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<rover_msgs::msg::Joy>::SharedPtr _sub_joy;
+    rclcpp::Publisher<rover_msgs::msg::AntennaCmd>::SharedPtr _pub_jog;
+
+    void callbackJoy(const rover_msgs::msg::Joy msg);
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<JogAntenna>());
+    rclcpp::shutdown();
+    return 0;
+}
+
+JogAntenna::JogAntenna() : Node("jog_antenna")
+{
+    _sub_joy = this->create_subscription<rover_msgs::msg::Joy>("/joy/main/formated" /*"/base/antenna/joy"*/,
+                                                               1,
+                                                               [this](const rover_msgs::msg::Joy msg)
+                                                               { callbackJoy(msg); });
+
+    _pub_jog = this->create_publisher<rover_msgs::msg::AntennaCmd>("/base/antenna/cmd/in/teleop", 1);
+    timer_ = this->create_wall_timer(500ms, std::bind(&JogAntenna::timer_callback, this));
+}
+
+void JogAntenna::callbackJoy(const rover_msgs::msg::Joy msg)
+{
+    rover_msgs::msg::AntennaCmd jog_cmd;
+    jog_cmd.status = true;
+    if (msg.l2 != 0.0 && msg.r2 == 0.0)
+    {   
+        jog_cmd.speed = -msg.l2 * MAX_SPEED;
+    }
+    else if (msg.r2 != 0.0 && msg.l2 == 0.0)
+    {
+        jog_cmd.speed = msg.r2 * MAX_SPEED;
+    }
+    else
+    {
+        jog_cmd.speed = 0.0;
+    }
+    _pub_jog->publish(jog_cmd);
+}
