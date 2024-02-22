9b351d3deab7f8793f649294b4ced176f6a00f26
emile <emilesavoie39@gmail.com>
2024-02-21 22:10:45 -0500

    Update button param

--- rover_drive_train/launch/driver_train.launch.py
+++ rover_drive_train/launch/driver_train.launch.py
@@ -15,7 +15,8 @@ def generate_launch_description():
                                     parameters=[{"speed_factor_crawler": 0.01},
                                                 {"speed_factor_normal": 0.25},
                                                 {"speed_factor_turbo": 1.0},
-                                                {"smalllest_radius": 0.30}],
+                                                {"smalllest_radius": 0.30},
+                                                {"currentPilot": 0}],
                                     remappings=[("driver", "raw")])
         
         node_joy_formatted = LaunchDescription(
