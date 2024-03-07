91c72d1ede7c17992d54753ad70ea41330399146
Aude Regnault <rega2002@usherbrooke.ca>
2024-03-06 21:12:52 -0500

    Tested gps tracking with rosbag

--- rover_antenna/src/autonomus.cpp
+++ rover_antenna/src/autonomus.cpp
@@ -9,10 +9,12 @@
 #include "rover_msgs/msg/gps.hpp"
 #include "rovus_lib/macros.h"
 #include "math.h"
+#include "rovus_lib/moving_average.hpp"
 
 #define PI 3.14159265359
 #define MAX_SPEED 5.0*PI/180 // rad/s
-#define N_SMA 10
+#define N_AVERAGE 10
+
 
 using namespace std::chrono_literals;
 
@@ -47,6 +49,12 @@ private:
 
     rclcpp::Parameter param_max_speed;
     rclcpp::Parameter param_n_average;
+
+    MovingAverage<float, N_AVERAGE> latitude_rover = MovingAverage<float, N_AVERAGE>(0.0);
+    MovingAverage<float, N_AVERAGE> longitude_rover = MovingAverage<float, N_AVERAGE>(0.0);
+
+    MovingAverage<float, N_AVERAGE> latitude_antenna = MovingAverage<float, N_AVERAGE>(0.0);
+    MovingAverage<float, N_AVERAGE> longitude_antenna = MovingAverage<float, N_AVERAGE>(0.0);
 };
 
 int main(int argc, char *argv[])
@@ -81,84 +89,103 @@ Autonomus::Autonomus() : Node("autonomus")
 
 void Autonomus::callbackGPSRover(const rover_msgs::msg::Gps msg)
 {
-    float rover_lat;
-    float rover_long;
-    if (count_rover < N_SMA && first_msgs_rover)
-    {
-        gps_rover_SMA[count_rover].latitude = msg.latitude;
-        gps_rover_SMA[count_rover].longitude = msg.longitude;
-        count_rover++;
-    }
-    else if (count_rover == N_SMA && !first_msgs_rover)
-    {
-        for (int i=0; i<N_SMA-1; i++)
-        {
-            gps_rover_SMA[i].latitude = gps_rover_SMA[i+1].latitude;
-            gps_rover_SMA[i].longitude = gps_rover_SMA[i+1].longitude;
-        }
-    }
-    if (count_rover == 10)
-    {
-        first_msgs_rover = false;
-    }
-    if (!first_msgs_rover)
-    {
-        rover_lat = 0;
-        rover_long = 0;
-        for (int i=0; i<N_SMA; i++)
-        {
-            rover_lat = rover_lat + gps_rover_SMA[i].latitude;
-            rover_long = rover_long + gps_rover_SMA[i].longitude;
-        }
-        rover_lat = rover_lat/N_SMA;
-        rover_long = rover_long/N_SMA;
-        gps_rover.latitude = rover_lat;
-        gps_rover.longitude = rover_long;
-        RCLCPP_INFO(rclcpp::get_logger("lat"), "Latitude rover %f", rover_lat);
-        autonomusCommand();
-    }
-    
-    
+    // float rover_lat;
+    // float rover_long;
+    // if (count_rover < N_SMA && first_msgs_rover)
+    // {
+    //     gps_rover_SMA[count_rover].latitude = msg.latitude;
+    //     gps_rover_SMA[count_rover].longitude = msg.longitude;
+    //     count_rover++;
+    // }
+    // else if (count_rover == N_SMA && !first_msgs_rover)
+    // {
+    //     for (int i=0; i<N_SMA-1; i++)
+    //     {
+    //         gps_rover_SMA[i].latitude = gps_rover_SMA[i+1].latitude;
+    //         gps_rover_SMA[i].longitude = gps_rover_SMA[i+1].longitude;
+    //     }
+    // }
+    // if (count_rover == 10)
+    // {
+    //     first_msgs_rover = false;
+    // }
+    // if (!first_msgs_rover)
+    // {
+    //     rover_lat = 0;
+    //     rover_long = 0;
+    //     for (int i=0; i<N_SMA; i++)
+    //     {
+    //         rover_lat = rover_lat + gps_rover_SMA[i].latitude;
+    //         rover_long = rover_long + gps_rover_SMA[i].longitude;
+    //     }
+    //     rover_lat = rover_lat/N_SMA;
+    //     rover_long = rover_long/N_SMA;
+    //     gps_rover.latitude = rover_lat;
+    //     gps_rover.longitude = rover_long;
+    //     RCLCPP_INFO(rclcpp::get_logger("lat"), "Latitude rover %f", rover_lat);
+    //     autonomusCommand();
+    // }
+    // latitude_rover.addValue(msg.latitude);
+    // longitude_rover.addValue(msg.longitude);
+
+    // gps_rover.latitude = latitude_rover.getAverage();
+    // gps_rover.longitude = longitude_rover.getAverage();
+
+    gps_rover.latitude = msg.latitude;
+    gps_rover.longitude = msg.longitude;
     
 }
 
 void Autonomus::callbackGPSAntenna(const rover_msgs::msg::Gps msg)
 {
-    float antenna_lat;
-    float antenna_long;
-    if (count_antenna < N_SMA && first_msgs_antenna)
-    {
-        gps_antenna_SMA[count_antenna].latitude = msg.latitude;
-        gps_antenna_SMA[count_antenna].longitude = msg.longitude;
-        count_antenna++;
-    }
-    else if (count_antenna == N_SMA && !first_msgs_antenna)
-    {
-        for (int i=0; i<N_SMA-1; i++)
-        {
-            gps_antenna_SMA[i].latitude = gps_antenna_SMA[i+1].latitude;
-            gps_antenna_SMA[i].longitude = gps_antenna_SMA[i+1].longitude;
-        }
-    }
-    if (count_antenna == 10)
-    {
-        first_msgs_antenna = false;
-    }
-    if (!first_msgs_antenna)
-    {
-        antenna_lat = 0;
-        antenna_long = 0;
-        for (int i=0; i<N_SMA; i++)
-        {
-            antenna_lat = antenna_lat + gps_antenna_SMA[i].latitude;
-            antenna_long = antenna_long + gps_antenna_SMA[i].longitude;
-        }
-        antenna_lat = antenna_lat/N_SMA;
-        antenna_long = antenna_long/N_SMA;
-        gps_antenna.latitude = antenna_lat;
-        gps_antenna.longitude = antenna_long;
-        RCLCPP_INFO(rclcpp::get_logger("lat"), "Latitude antenna %f", antenna_lat);
-    }
+    // float antenna_lat;
+    // float antenna_long;
+    // if (count_antenna < N_SMA && first_msgs_antenna)
+    // {
+    //     gps_antenna_SMA[count_antenna].latitude = msg.latitude;
+    //     gps_antenna_SMA[count_antenna].longitude = msg.longitude;
+    //     count_antenna++;
+    // }
+    // else if (count_antenna == N_SMA && !first_msgs_antenna)
+    // {
+    //     for (int i=0; i<N_SMA-1; i++)
+    //     {
+    //         gps_antenna_SMA[i].latitude = gps_antenna_SMA[i+1].latitude;
+    //         gps_antenna_SMA[i].longitude = gps_antenna_SMA[i+1].longitude;
+    //     }
+    // }
+    // if (count_antenna == 10)
+    // {
+    //     first_msgs_antenna = false;
+    // }
+    // if (!first_msgs_antenna)
+    // {
+    //     antenna_lat = 0;
+    //     antenna_long = 0;
+    //     for (int i=0; i<N_SMA; i++)
+    //     {
+    //         antenna_lat = antenna_lat + gps_antenna_SMA[i].latitude;
+    //         antenna_long = antenna_long + gps_antenna_SMA[i].longitude;
+    //     }
+    //     antenna_lat = antenna_lat/N_SMA;
+    //     antenna_long = antenna_long/N_SMA;
+    //     gps_antenna.latitude = antenna_lat;
+    //     gps_antenna.longitude = antenna_long;
+    //     RCLCPP_INFO(rclcpp::get_logger("lat"), "Latitude antenna %f", antenna_lat);
+    // }
+
+    // latitude_antenna.addValue(msg.latitude);
+    // longitude_antenna.addValue(msg.longitude);
+
+    // gps_antenna.latitude = latitude_antenna.getAverage();
+    // gps_antenna.longitude = longitude_antenna.getAverage();
+
+    gps_antenna.latitude = msg.latitude;
+    gps_antenna.longitude = msg.longitude;
+    gps_antenna.heading = msg.heading;
+    
+    autonomusCommand();
+
 }
 
 void Autonomus::autonomusCommand()
@@ -181,19 +208,24 @@ void Autonomus::autonomusCommand()
 
     rover_msgs::msg::AntennaCmd cmd;
 
-    if (heading > 15)
+    if (heading > 15.0)
     {
         cmd.status = true;
         cmd.speed = MAX_SPEED;
-        _pub_cmd->publish(cmd);
     }
-    else if (heading < -15)
+    else if (heading < -15.0)
     {
         cmd.status = true;
         cmd.speed = -MAX_SPEED;
-        _pub_cmd->publish(cmd);
+    }
+    else
+    {
+        cmd.status = false;
+        cmd.speed = 0.0;
+
     }
     RCLCPP_INFO(rclcpp::get_logger("angle"), "angle %f", heading);
+    _pub_cmd->publish(cmd);
 }
 
 float Autonomus::calculateHeading(float lat1, float lon1, float lat2, float lon2)
