b047403f4bbbc20554b1387c1a12aedb92f34f6c
emile <emilesavoie39@gmail.com>
2024-03-13 10:56:33 -0400

    Auto stash before revert of "Revert "double launch + pubsub""

--- rover_drive_train/src/teleop.cpp
+++ rover_drive_train/src/teleop.cpp
@@ -4,74 +4,11 @@
 #include <map>
 #include "rover_msgs/msg/joy_demux_status.hpp"
 
-using namespace std::placeholders;
-
-enum Pilotes: uint8_t {
-    phil = 0,
-    alex,
-    mimile_aintnoway,
-    senile,
-    rimoule,
-    MAX
-};
-
-struct Actions {
-    float* Deadman;
-    float* Throttle;
-    float* Turn;
-    float* Backward;
-    float* Tank;
-    float* Fast;
-    float* Normal;
-    float* Crawl;
-    float* Lights;
-    float* Drift_lol;
-};
-
-struct PilotConfig {
-    uint8_t Deadman_Active;
-    uint8_t Throttle_Active;
-    uint8_t Turn_Active;
-    uint8_t TurnTank_Active;
-    uint8_t Tank_Actve;
-    uint8_t Fast_Active;
-    uint8_t Normal_Active;
-    uint8_t Crawl_Active;
-    uint8_t Lights_Active;
-    uint8_t Drift_lol_Active;
-};
-
-std::map<Pilotes, PilotConfig> ActionToJoyMapping;
-
-void initConfig(Pilotes pilot, const PilotConfig& config)
-{
-    ActionToJoyMapping[pilot] = config;
-}
-
-void initConfigPilot()
-{
-    PilotConfig configPhil = {10, 0, 1, 4, };
-    initConfig(Pilotes::phil, configPhil);
-
-    PilotConfig configsenile = {10, 13, 1, 4, 15, 11, 14, 6};
-    initConfig(Pilotes::senile, configsenile);
-    
-}
-
 class Teleop : public rclcpp::Node
 {
 private:
-    std::map<Pilotes, Actions> ActionToJoyMapping;
-    PilotConfig currentConfig;
-    int lastKnownPilot = -1;
-
     rclcpp::Subscription<rover_msgs::msg::Joy>::SharedPtr _sub_joy_formatted;
     rclcpp::TimerBase::SharedPtr _timer;
-
-    void callbackTeleop(const rover_msgs::msg::Joy &msg);
-    void callbackTimer();
-    void initConfig(Pilotes pilot, const PilotConfig& config);
-    void initConfigPilot();
 
 public:
     Teleop();
@@ -79,29 +16,11 @@ public:
 
 Teleop::Teleop() : Node("teleop")
 {
-    _sub_joy_formatted = this->create_subscription<rover_msgs::msg::Joy>("main_formatted",
-                                                                         1,
-                                                                          [this](const rover_msgs::msg::Joy msg)
-                                                                        {callbackTeleop(msg); });
-
-    _timer = this->create_wall_timer(std::chrono::milliseconds(10), std::bind(&Teleop::callbackTimer, this));
-                                                                         
+                                                                        
     this->declare_parameter<float>("speed_factor_crawler", 0.01);
     this->declare_parameter<float>("speed_factor_normal", 0.25);
     this->declare_parameter<float>("speed_factor_turbo", 1.0);
     this->declare_parameter<float>("smallest_radius", 0.30);
-
-     initConfig(Pilotes::phil);
-}
-
-void Teleop::callbackTeleop(const rover_msgs::msg::Joy &msg)
-{
-
-}
-
-void Teleop::callbackTimer()
-{
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Timer is active");
 }
 
 int main(int argc, char *argv[])
