044ef9437443050ef22feebf6a75ecbbec72058b
Aude Regnault <rega2002@usherbrooke.ca>
2024-03-23 15:23:49 -0400

    Modification pull request

--- rover_antenna/src/jog_antenna.cpp
+++ rover_antenna/src/jog_antenna.cpp
@@ -13,10 +13,11 @@ public:
     ~JogAntenna() {}
 
 private:
+    void callbackJoy(const rover_msgs::msg::Joy msg_);
+
     rclcpp::Subscription<rover_msgs::msg::Joy>::SharedPtr _sub_joy;
     rclcpp::Publisher<rover_msgs::msg::AntennaCmd>::SharedPtr _pub_jog;
 
-    void callbackJoy(const rover_msgs::msg::Joy msg_);
     rclcpp::Parameter _paramMaxSpeed;
     rclcpp::Parameter _paramCoeffNb;
     MovingAverage<float, COEFF_NB> _jogAverage_r1 = MovingAverage<float, COEFF_NB>(0.0f);
@@ -41,37 +42,34 @@ JogAntenna::JogAntenna() : Node("jog_antenna")
 
     _pub_jog = this->create_publisher<rover_msgs::msg::AntennaCmd>("/base/antenna/cmd/in/teleop", 1);
 
-    this->declare_parameter<float>("max_speed", PI/2);
-    this->declare_parameter<int16_t>("coeff_nb", 10);
-
+    this->declare_parameter<float>("max_speed", PI/2.0f);
     _paramMaxSpeed = this->get_parameter("max_speed");
-    _paramCoeffNb = this->get_parameter("coeff_nb");
     
 }
 
 void JogAntenna::callbackJoy(const rover_msgs::msg::Joy msg_)
 {
-    rover_msgs::msg::AntennaCmd jog_cmd;
+    rover_msgs::msg::AntennaCmd jogCmd;
 
     _jogAverage_l1.addValue(msg_.l1);
 
     
     _jogAverage_r1.addValue(msg_.r1);
 
-    jog_cmd.status = true;
+    jogCmd.enable = true;
 
     if(_jogAverage_l1.getAverage() != 0.0f && _jogAverage_r1.getAverage() == 0.0f)
     {
-        jog_cmd.speed = -_jogAverage_l1.getAverage() * _paramMaxSpeed.as_double();
+        jogCmd.speed = -_jogAverage_l1.getAverage() * _paramMaxSpeed.as_double();
     }
     else if(_jogAverage_l1.getAverage() == 0.0f && _jogAverage_r1.getAverage() != 0.0f)
     {
-        jog_cmd.speed = _jogAverage_r1.getAverage() * _paramMaxSpeed.as_double();
+        jogCmd.speed = _jogAverage_r1.getAverage() * _paramMaxSpeed.as_double();
     }
     else
     {
-        jog_cmd.speed = 0.0f;
+        jogCmd.speed = 0.0f;
     }
 
-    _pub_jog->publish(jog_cmd);
+    _pub_jog->publish(jogCmd);
 }
