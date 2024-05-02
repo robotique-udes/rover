3f735a39d1447be4d44c6624485768934232e59d
Aude Regnault <rega2002@usherbrooke.ca>
2024-05-02 03:26:49 -0400

    Auto stash before rebase of "origin/release/circ2024"

--- rover_antenna/src/jog_antenna.cpp
+++ rover_antenna/src/jog_antenna.cpp
@@ -3,6 +3,7 @@
 #include "rover_msgs/msg/antenna_cmd.hpp"
 #include "rovus_lib/moving_average.hpp"
 #include "rovus_lib/macros.h"
+#include "rovus_lib/timer.hpp"
 
 #define COEFF_NB 10
 
@@ -14,14 +15,21 @@ public:
 
 private:
     void callbackJoy(const rover_msgs::msg::Joy msg_);
+    void cbTimerJogCmd();
 
     rclcpp::Subscription<rover_msgs::msg::Joy>::SharedPtr _sub_joy;
     rclcpp::Publisher<rover_msgs::msg::AntennaCmd>::SharedPtr _pub_jog;
+    rclcpp::TimerBase::SharedPtr _timer_JogCmd;
+
+    bool _l1;
+    bool _r1;
+    bool _joyMsgReceived = false;
 
     rclcpp::Parameter _paramMaxSpeed;
     rclcpp::Parameter _paramCoeffNb;
     MovingAverage<float, COEFF_NB> _jogAverage_r1 = MovingAverage<float, COEFF_NB>(0.0f);
     MovingAverage<float, COEFF_NB> _jogAverage_l1 = MovingAverage<float, COEFF_NB>(0.0f);
+    Timer<uint64_t, millis> _timer_joyMsg = Timer<uint64_t, millis>(250);
     
 };
 
@@ -42,13 +50,15 @@ JogAntenna::JogAntenna() : Node("jog_antenna")
 
     _pub_jog = this->create_publisher<rover_msgs::msg::AntennaCmd>("/base/antenna/cmd/in/teleop", 1);
 
+    _timer_JogCmd = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&JogAntenna::cbTimerJogCmd, this));
+
     this->declare_parameter<float>("max_speed", PI/2.0f);
     _paramMaxSpeed = this->get_parameter("max_speed");
     
 }
 
-void JogAntenna::callbackJoy(const rover_msgs::msg::Joy msg_)
-{
+void JogAntenna::cbTimerJogCmd()
+{   
     rover_msgs::msg::AntennaCmd jogCmd;
 
     _jogAverage_l1.addValue(msg_.L1);
@@ -59,16 +69,47 @@ void JogAntenna::callbackJoy(const rover_msgs::msg::Joy msg_)
 
     if(_jogAverage_l1.getAverage() != 0.0f && _jogAverage_r1.getAverage() == 0.0f)
     {
-        jogCmd.speed = -_jogAverage_l1.getAverage() * _paramMaxSpeed.as_double();
+        _joyMsgReceived = false;
     }
-    else if(_jogAverage_l1.getAverage() == 0.0f && _jogAverage_r1.getAverage() != 0.0f)
+
+    if(_joyMsgReceived)
     {
-        jogCmd.speed = _jogAverage_r1.getAverage() * _paramMaxSpeed.as_double();
+        
+        _jogAverage_l1.addValue(_l1);
+        
+        _jogAverage_r1.addValue(_r1);
+
+        jogCmd.enable = true;
+
+        if(_jogAverage_l1.getAverage() != 0.0f && _jogAverage_r1.getAverage() == 0.0f)
+        {
+            jogCmd.speed = -_jogAverage_l1.getAverage() * _paramMaxSpeed.as_double();
+        }
+        else if(_jogAverage_l1.getAverage() == 0.0f && _jogAverage_r1.getAverage() != 0.0f)
+        {
+            jogCmd.speed = _jogAverage_r1.getAverage() * _paramMaxSpeed.as_double();
+        }
+        else
+        {
+            jogCmd.speed = 0.0f;
+        }
+        jogCmd.enable = true;
+        _pub_jog->publish(jogCmd);
     }
     else
     {
-        jogCmd.speed = 0.0f;
+        RCLCPP_WARN(LOGGER, "No joy message is received");
+        jogCmd.enable = false;
+        jogCmd.speed = 0.0;
+        _pub_jog->publish(jogCmd);
     }
+}
 
-    _pub_jog->publish(jogCmd);
+void JogAntenna::callbackJoy(const rover_msgs::msg::Joy msg_)
+{
+    _l1 = msg_.l1;
+    _r1 = msg_.r1;
+
+    _joyMsgReceived = true;
+    _timer_joyMsg.init(250);
 }
