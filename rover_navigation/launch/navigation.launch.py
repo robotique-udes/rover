af572eabcf0d8b71cfc76a58b96b901c0b3ad388
pmichaud29 <micp1402@usherbrooke.ca>
2024-05-06 22:42:17 -0400

    launchfile

--- rover_navigation/launch/navigation.launch.py
+++ rover_navigation/launch/navigation.launch.py
@@ -4,16 +4,16 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-# def generate_launch_description():
-#     ld: LaunchDescription = LaunchDescription()
+def generate_launch_description():
+    ld: LaunchDescription = LaunchDescription()
 
-#     node_joy_main = Node(package="rover_can",
-#                          namespace="/rover/canbus",
-#                          executable="can_master",
-#                          name="can_master")
+    node_lights_main = Node(package="rover_navigation",
+                         namespace="/rover/auxiliarty",
+                         executable="light_control",
+                         name="light_controller")
 
-#     ld.add_action(node_joy_main)
+    ld.add_action(node_lights_main)
 
-#     return LaunchDescription([
-#                               node_joy_main
-#                              ])
+    return LaunchDescription([
+                              node_lights_main
+                             ])
