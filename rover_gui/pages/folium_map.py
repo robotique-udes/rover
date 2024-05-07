129c40b6d4b3b087c77fb7cd3d8ee9cb3be33c74
Ndumoulin <nathan.dumoulin11@outlook.com>
2024-05-06 20:22:39 -0400

    Added drivetrain arbitration + cleaning

--- rover_gui/pages/folium_map.py
+++ rover_gui/pages/folium_map.py
@@ -2,7 +2,6 @@ import io
 from PyQt5.QtWidgets import QVBoxLayout, QWidget
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 import folium
-from folium.plugins import Realtime
 
 class FoliumMapWidget(QWidget):
     def __init__(self, nav_widget):
