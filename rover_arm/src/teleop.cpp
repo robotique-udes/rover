b2c6073a98596e174e736e3d0abf78145b531fb6
Emile <emilesavoie39@gmail.com>
2024-08-03 16:01:20 -0400

    full working logic

--- rover_arm/src/teleop.cpp
+++ rover_arm/src/teleop.cpp
@@ -39,6 +39,8 @@ private:
     arma::mat55 _jacobian;
     arma::mat55 _inverseJacobian;
     arma::vec5 _computedJointVelocity;
+    arma::vec5 _constrainedJointVelocity;
+    arma::vec5 _maxJointVelocity;
     arma::mat _pointPositions;
 
     RobotState _currentState = RobotState(0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
@@ -49,6 +51,7 @@ private:
     bool _gripperMode;
     bool _controlMode;
     bool _controlModeToggle;
+    bool _fixedCartesianMode;
 
     enum eControlMode
     {
@@ -74,10 +77,10 @@ private:
 
     arma::mat55 computeJacobian(const RobotState &state);
     arma::mat computeDirectKinematic(const RobotState &state);
+    arma::vec5 constrainJointVelocity(arma::vec5 jointVelocity, arma::vec5 maxJointVelocity);
 
     rclcpp::Subscription<rover_msgs::msg::Joy>::SharedPtr _sub_joy_arm;
     rclcpp::Publisher<rover_msgs::msg::ArmCmd>::SharedPtr _pub_arm_teleop_in;
-    // rclcpp::Publisher<rover_msg
     rover_msgs::msg::ArmCmd armMsg;
 };
 
@@ -91,6 +94,7 @@ Teleop::Teleop() : Node("teleop")
     _pub_arm_teleop_in = this->create_publisher<rover_msgs::msg::ArmCmd>("/rover/arm/cmd/in/teleop", 1);
 
     _controlMode = JOINT;
+    _maxJointVelocity = {MAX_JOINT_SPEED * 0.001f, MAX_JOINT_SPEED * 0.001f, MAX_JOINT_SPEED * 0.001f, MAX_JOINT_SPEED * 0.001f, MAX_JOINT_SPEED * 0.001f};
 }
 
 void Teleop::joyCallback(const rover_msgs::msg::Joy::SharedPtr joyMsg)
@@ -99,6 +103,7 @@ void Teleop::joyCallback(const rover_msgs::msg::Joy::SharedPtr joyMsg)
     // =========================================================================
     _deadmanSwitch = joyMsg->joy_data[rover_msgs::msg::Joy::L1];
     _gripperMode = joyMsg->joy_data[rover_msgs::msg::Joy::R1];
+    _fixedCartesianMode = joyMsg->joy_data[rover_msgs::msg::Joy::R1];
     _controlModeToggle = joyMsg->joy_data[rover_msgs::msg::Joy::A];
 
     // JOINT controls
@@ -132,12 +137,12 @@ void Teleop::joyCallback(const rover_msgs::msg::Joy::SharedPtr joyMsg)
     armMsg.position[rover_msgs::msg::ArmCmd::J2] = _currentJ2Pos;
     armMsg.position[rover_msgs::msg::ArmCmd::GRIPPERTILT] = _currentGripperTilt;
     armMsg.position[rover_msgs::msg::ArmCmd::GRIPPERROT] = _currentGripperRot;
-    
+
     // Control mode toggle
     // =========================================================================
     if (_controlModeToggle)
     {
-        _controlMode = not _controlMode; // TO BE FIXED NOT PERFECT
+        _controlMode = !_controlMode;
     }
 
     // Control logic
@@ -146,28 +151,27 @@ void Teleop::joyCallback(const rover_msgs::msg::Joy::SharedPtr joyMsg)
     {
         if (_controlMode == CARTESIAN)
         {
-            _desiredCartesianVelocity = {_xVelocity, _yVelocity * 0.0f, _zVelocity * 0.0f, _alphaVelocity * 0.0f, _psiVelocity * 0.0f};
+            if (_fixedCartesianMode)
+            {
+                _desiredCartesianVelocity = {_xVelocity * 0.0f, _yVelocity * 0.0f, _zVelocity * 0.0f, _alphaVelocity, _psiVelocity};
+            }
+            else
+            {
+                _desiredCartesianVelocity = {_xVelocity, _yVelocity, _zVelocity, _alphaVelocity * 0.0f, _psiVelocity * 0.0f};
+            }
 
             _jacobian = computeJacobian(RobotState(_currentJLPos, _currentJ0Pos, _currentJ1Pos, _currentJ2Pos, _currentGripperTilt));
             _inverseJacobian = arma::inv(_jacobian);
 
             _computedJointVelocity = _inverseJacobian * _desiredCartesianVelocity;
 
-            _computedJointVelocity(0) * 0.001f;
-            _computedJointVelocity(1) * 0.001f;
-            _computedJointVelocity(2) * 0.001f;
-            _computedJointVelocity(3) * 0.001f;
-            _computedJointVelocity(4) * 0.001f;
+            _constrainedJointVelocity = constrainJointVelocity(_computedJointVelocity, _maxJointVelocity);
 
-            _currentJLPos += _computedJointVelocity(0) * 0.001f;       // Seems to be to fast with only one 0.001f
-            _currentJ0Pos += _computedJointVelocity(1) * 0.001f;       // Seems to be to fast with only one 0.001f
-            _currentJ1Pos += _computedJointVelocity(2) * 0.001f;       // Seems to be to fast with only one 0.001f
-            _currentJ2Pos += _computedJointVelocity(3) * 0.001f;       // Seems to be to fast with only one 0.001f
-            _currentGripperTilt += _computedJointVelocity(4) * 0.001f; // Seems to be to fast with only one 0.001f
-
-            _pointPositions = computeDirectKinematic(RobotState(_currentJLPos, _currentJ0Pos, _currentJ1Pos, _currentJ2Pos, _currentGripperTilt));
-
-            RCLCPP_INFO(rclcpp::get_logger("POS"), "EF X: %f, EF Y: %f, EF Z: %f", _pointPositions(5, 0), _pointPositions(5, 1), _pointPositions(5, 2));
+            _currentJLPos += _computedJointVelocity(0) * MAX_JOINT_SPEED;
+            _currentJ0Pos += _computedJointVelocity(1) * MAX_JOINT_SPEED;
+            _currentJ1Pos += _computedJointVelocity(2) * MAX_JOINT_SPEED;
+            _currentJ2Pos += _computedJointVelocity(3) * MAX_JOINT_SPEED;
+            _currentGripperTilt += _computedJointVelocity(4) * MAX_JOINT_SPEED;
         }
 
         if (_controlMode == JOINT)
@@ -195,30 +199,30 @@ arma::mat55 Teleop::computeJacobian(const RobotState &state)
 {
     arma::mat55 J(arma::fill::zeros);
 
-    float q0 = state.q0;
+    // float q0 = state.q0;
     float q1 = state.q1;
     float q2 = state.q2;
     float q3 = state.q3;
     float q4 = state.q4;
 
-    float J0x = state.J0x;
-    float J0y = state.J0y;
-    float J0z = state.J0z;
+    // float J0x = state.J0x;
+    // float J0y = state.J0y;
+    // float J0z = state.J0z;
 
     float J1x = state.J1x;
     float J1y = state.J1y;
-    float J1z = state.J1z;
+    // float J1z = state.J1z;
 
     float J2x = state.J2x;
     float J2y = state.J2y;
     float J2z = state.J2z;
 
     float J3x = state.J3x;
-    float J3y = state.J3y;
+    // float J3y = state.J3y;
     float J3z = state.J3z;
 
     float J4x = state.J4x;
-    float J4y = state.J4y;
+    // float J4y = state.J4y;
     float J4z = state.J4z;
 
     // dot(Po.GetPosition(No), nx>) Result = J0x + J1x*cos(q1) + J2x*cos(q1)*cos(0.5*PI-q2) + J2z*cos(q1)*sin(0.5*PI-q2) + J3x*cos(q1)*cos(0.5*PI-q2-q3) + J3z*cos(q1)*sin(0.5*PI-q2-q3) - sin(q1)*(J1y+J2y) + J4x*sin(q1)*cos(0.5*PI-q2-q3-q4) + J4z*sin(q1)*sin(0.5*PI-q2-q3-q4)
@@ -246,7 +250,7 @@ arma::mat55 Teleop::computeJacobian(const RobotState &state)
     J(3, 1) = 0.0f;
     J(3, 2) = 1.0f;
     J(3, 3) = 1.0f;
-    J(3, 4) = -1.0f;
+    J(3, 4) = 1.0f;
 
     J(4, 0) = 0.0f;
     J(4, 1) = 1.0f;
@@ -257,61 +261,28 @@ arma::mat55 Teleop::computeJacobian(const RobotState &state)
     return J;
 }
 
-arma::mat Teleop::computeDirectKinematic(const RobotState &state)
+arma::vec5 Teleop::constrainJointVelocity(arma::vec5 jointVelocity, arma::vec5 maxJointVelocity)
 {
-    arma::mat pointPositions(6, 3, arma::fill::zeros);
+    //NEEDS FIXING
+    float highestVelocityRatio = jointVelocity[1] / maxJointVelocity[1];
+    float currentVelocityRatio;
+    arma::vec5 constrainedVelocity;
 
-    float q0 = state.q0;
-    float q1 = state.q1;
-    float q2 = state.q2;
-    float q3 = state.q3;
-    float q4 = state.q4;
+    for (uint8_t i = 0; i < (uint8_t)jointVelocity.n_elem; i++)
+    {
+        currentVelocityRatio = jointVelocity[i] / maxJointVelocity[i];
+        if (currentVelocityRatio > highestVelocityRatio)
+        {
+            highestVelocityRatio = currentVelocityRatio;
+        }
+    }
 
-    float J0x = state.J0x;
-    float J0y = state.J0y;
-    float J0z = state.J0z;
+    for (uint8_t i = 0; i < (uint8_t)jointVelocity.n_elem; i++)
+    {
+        constrainedVelocity[i] = jointVelocity[i] / highestVelocityRatio;
+    }
 
-    float J1x = state.J1x;
-    float J1y = state.J1y;
-    float J1z = state.J1z;
-
-    float J2x = state.J2x;
-    float J2y = state.J2y;
-    float J2z = state.J2z;
-
-    float J3x = state.J3x;
-    float J3y = state.J3y;
-    float J3z = state.J3z;
-
-    float J4x = state.J4x;
-    float J4y = state.J4y;
-    float J4z = state.J4z;
-
-    pointPositions(0, 0) = 0.0f;
-    pointPositions(0, 1) = q0;
-    pointPositions(0, 2) = 0.0f;
-
-    pointPositions(1, 0) = J0x;
-    pointPositions(1, 1) = J0y + q0;
-    pointPositions(1, 2) = J0z;
-
-    pointPositions(2, 0) = J0x + J1x * cos(q1) - J1y * sin(q1);
-    pointPositions(2, 1) = J0y + q0 + J1x * sin(q1) + J1y * cos(q1);
-    pointPositions(2, 2) = J0z + J1z;
-
-    pointPositions(3, 0) = J0x + J1x * cos(q1) + J2x * cos(q1) * cos(0.5 * PI - q2) + J2z * cos(q1) * sin(0.5 * PI - q2) - sin(q1) * (J1y + J2y);
-    pointPositions(3, 1) = J0y + q0 + J1x * sin(q1) + cos(q1) * (J1y + J2y) + J2x * sin(q1) * cos(0.5 * PI - q2) + J2z * sin(q1) * sin(0.5 * PI - q2);
-    pointPositions(3, 2) = J0z + J1z + J2z * cos(0.5 * PI - q2) - J2x * sin(0.5 * PI - q2);
-
-    pointPositions(4, 0) = J0x + J1x * cos(q1) + J2x * cos(q1) * cos(0.5 * PI - q2) + J2z * cos(q1) * sin(0.5 * PI - q2) + J3x * cos(q1) * cos(0.5 * PI - q2 - q3) + J3z * cos(q1) * sin(0.5 * PI - q2 - q3) - J3y * sin(q1) - sin(q1) * (J1y + J2y);
-    pointPositions(4, 1) = J0y + q0 + J1x * sin(q1) + cos(q1) * (J1y + J2y) + J2x * sin(q1) * cos(0.5 * PI - q2) + J2z * sin(q1) * sin(0.5 * PI - q2) + J3x * sin(q1) * cos(0.5 * PI - q2 - q3) + J3z * sin(q1) * sin(0.5 * PI - q2 - q3);
-    pointPositions(4, 2) = J0z + J1z + J2z * cos(0.5 * PI - q2) + J3z * cos(0.5 * PI - q2 - q3) - J2x * sin(0.5 * PI - q2) - J3x * sin(0.5 * PI - q2 - q3);
-
-    pointPositions(5, 0) = J0x + J1x * cos(q1) + J2x * cos(q1) * cos(0.5 * PI - q2) + J2z * cos(q1) * sin(0.5 * PI - q2) + J3x * cos(q1) * cos(0.5 * PI - q2 - q3) + J3z * cos(q1) * sin(0.5 * PI - q2 - q3) - sin(q1) * (J1y + J2y) + J4x * sin(q1) * cos(0.5 * PI - q2 - q3 - q4) + J4z * sin(q1) * sin(0.5 * PI - q2 - q3 - q4);
-    pointPositions(5, 1) = J0y + q0 + J1x * sin(q1) + cos(q1) * (J1y + J2y) + J2x * sin(q1) * cos(0.5 * PI - q2) + J2z * sin(q1) * sin(0.5 * PI - q2) + J3x * sin(q1) * cos(0.5 * PI - q2 - q3) + J3z * sin(q1) * sin(0.5 * PI - q2 - q3) + J4x * sin(q1) * cos(0.5 * PI - q2 - q3 - q4) + J4z * sin(q1) * sin(0.5 * PI - q2 - q3 - q4);
-    pointPositions(5, 2) = J0z + J1z + J2z * cos(0.5 * PI - q2) + J3z * cos(0.5 * PI - q2 - q3) + J4z * cos(0.5 * PI - q2 - q3 - q4) - J2x * sin(0.5 * PI - q2) - J3x * sin(0.5 * PI - q2 - q3) - J4x * sin(0.5 * PI - q2 - q3 - q4);
-
-    return pointPositions;
+    return constrainedVelocity;
 }
 
 int main(int argc, char *argv[])
