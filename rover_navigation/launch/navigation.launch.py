2e3eaf80a0deddf0959e2ce66e7f8b044a9daa9d
ChristopherLajoie <lajc1503@usherbrooke.ca>
2024-08-08 13:33:27 -0400

    added panorama node

--- rover_navigation/launch/navigation.launch.py
+++ rover_navigation/launch/navigation.launch.py
@@ -16,11 +16,18 @@ def generate_launch_description():
                         namespace="/rover/auxiliary",
                         executable="compass_calibrator",
                         name="compass_calibrator")
+    
+    node_panorama = Node(package="rover_navigation",
+                        namespace="/rover/auxiliary",
+                        executable="panorama",
+                        name="panorama")
 
     ld.add_action(node_lights_main)
     ld.add_action(node_compass_main)
+    ld.add_action(node_panorama)
 
     return LaunchDescription([
                               node_lights_main,
-                              node_compass_main
+                              node_compass_main,
+                              node_panorama
                              ])
