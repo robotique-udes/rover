a9dad14778dd329fc12a03178c5fbf20fee59aae
pmichaud29 <micp1402@usherbrooke.ca>
2024-08-11 08:21:35 -0600

    Auto stash before merge of "dev/test_screenshot_CL" and "origin/dev/test_screenshot_CL"

--- rover_auxiliary/launch/auxiliary_rover.launch.py
+++ rover_auxiliary/launch/auxiliary_rover.launch.py
@@ -28,6 +28,21 @@ def generate_launch_description():
         name="light_control"
     )
     
+    node_redistribute_stream = Node(
+            package="rover_auxiliary",
+            namespace="/rover/auxiliary",
+            executable="redistribute_stream.py",
+            name="redistribute_stream",
+            parameters=[
+                {"streamIP": "192.168.144.62"},
+                {"streamPort": 69},
+                {"serverPort1": 8554},  
+                {"outputIP1": "192.168.144.101"},  
+                {"serverPort2": 8555},  
+                {"outputIP2": "192.168.144.101"},  
+            ]
+    )
+    
     # ld.add_action(node_aruco)
     ld.add_action(node_compass_calibrator)
     ld.add_action(node_light_control)
