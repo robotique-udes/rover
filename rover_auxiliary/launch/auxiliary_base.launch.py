f049d277e9475e8ce2cbf55a50235244f6dd8faf
Emile <emilesavoie39@gmail.com>
2024-08-11 07:44:31 -0400

    Auto stash before checking out "origin/dev/test_pipe_ES_ND"

--- rover_auxiliary/launch/auxiliary_base.launch.py
+++ rover_auxiliary/launch/auxiliary_base.launch.py
@@ -7,12 +7,44 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
 
+<<<<<<< Updated upstream
     node_aruco = Node(
         package="rover_auxiliary",
         namespace="/rover/auxiliary",
         executable="aruco_main.py",
         name="aruco"
     )
+=======
+    node_lights_main = Node(
+        package="rover_auxiliary",
+        namespace="/rover/auxiliary",
+        executable="light_control",
+        name="light_controller"
+    )
+
+    node_compass_main = Node(
+        package="rover_auxiliary",
+        namespace="/rover/auxiliary",
+        executable="compass_calibrator",
+        name="compass_calibrator"
+    )
+
+    node_redistribute_stream = Node(
+            package="rover_auxiliary",
+            namespace="/rover/auxiliary",
+            executable="redistribute_stream.py",
+            name="redistribute_stream",
+            parameters=[
+                {"streamIP": "127.0.0.2"},
+                {"streamPort": 69},
+                {"serverPort1": 8554},  
+                {"outputIP1": "192.168.144.198"},  
+                {"serverPort2": 8555},  
+                {"outputIP2": "192.168.144.199"},  
+            ]
+        )
+
+>>>>>>> Stashed changes
     ld.add_action(node_aruco)
 
     return ld
