a1be6d78748dcfe0c02711fae9b84d0cdb5b9a8e
Emile <emilesavoie39@gmail.com>
2024-08-11 07:49:48 -0400

    rebase to normal state

--- rover_auxiliary/launch/auxiliary_base.launch.py
+++ rover_auxiliary/launch/auxiliary_base.launch.py
@@ -7,37 +7,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
 
-    node_lights_main = Node(
+    node_aruco = Node(
         package="rover_auxiliary",
         namespace="/rover/auxiliary",
-        executable="light_control",
-        name="light_controller"
+        executable="aruco_main.py",
+        name="aruco"
     )
-
-    node_compass_main = Node(
-        package="rover_auxiliary",
-        namespace="/rover/auxiliary",
-        executable="compass_calibrator",
-        name="compass_calibrator"
-    )
-
-    node_redistribute_stream = Node(
-            package="rover_auxiliary",
-            namespace="/rover/auxiliary",
-            executable="redistribute_stream.py",
-            name="redistribute_stream",
-            parameters=[
-                {"streamIP": "127.0.0.2"},
-                {"streamPort": 69},
-                {"serverPort1": 8554},  
-                {"outputIP1": "192.168.144.198"},  
-                {"serverPort2": 8555},  
-                {"outputIP2": "192.168.144.199"},  
-            ]
-        )
-
-    ld.add_action(node_lights_main)
-    ld.add_action(node_compass_main)
-    ld.add_action(node_redistribute_stream)
+    ld.add_action(node_aruco)
 
     return ld
