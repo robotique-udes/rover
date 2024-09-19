595dd7c16809990cccd89b07333e87774960edb4
pmichaud29 <micp1402@usherbrooke.ca>
2024-09-19 00:38:32 -0400

    Split in hpp files + icon management

---
+++ rover_gui/include/q_file_transfer_widget.hpp
@@ -0,0 +1,21 @@
+// Ui files
+#include "ui_file_transfer_widget.h"
+
+// Custom objects
+#include "q_ssh_file_explorer.hpp"
+
+class QFileTransferWidget : public QWidget
+{
+public:
+    QFileTransferWidget()
+    {
+        ui.setupUi(this);
+        pRoverFileSystem = new QSshFileExplorer(*ui.tv_rover);
+    }
+
+    ~QFileTransferWidget() {}
+
+private:
+    Ui::FileTransfer ui;
+    QSshFileExplorer *pRoverFileSystem;
+};
