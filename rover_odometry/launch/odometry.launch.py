61c48b1bbaaa77fa05d235d4daa9742913f7c294
Zach3292 <zach3292@hotmail.com>
2024-10-09 17:39:19 -0400

    renamed package rover_odometry

--- rover_odometry/launch/odometry.launch.py
+++ rover_odometry/launch/odometry.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
         ld: LaunchDescription = LaunchDescription()
 
-        test_camera_node = Node (package="odometry",
+        test_camera_node = Node (package="rover_odometry",
                                     namespace="/rover/odometry",
                                     executable="test_camera",
                                     name="test_camera"
