618ba72adbef3fed459c40705f27bc74628a5d09
NikolayPapanchev <papn1801@usherbrooke.ca>
2024-10-09 17:37:23 -0400

    create launch folder odometry

---
+++ odometry/launch/odometry.launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+        ld: LaunchDescription = LaunchDescription()
+
+        test_camera_node = Node (package="odometry",
+                                    namespace="/rover/odometry",
+                                    executable="test_camera",
+                                    name="test_camera"
+                                    )
+        
+        ld.add_action(test_camera_node)
+
+        return ld
