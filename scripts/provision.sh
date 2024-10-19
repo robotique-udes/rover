1c89253a16a168491c25113fa37b2a960bc59211
pmichaud29 <micp1402@usherbrooke.ca>
2024-10-18 23:15:35 -0400

    First commit of building workflow

---
+++ scripts/provision.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+echo "=== Updating apt ... ==="
+sudo apt -y update
+echo -e "\e[0;32m[OK]\e[0m"
+
+echo "=== Installing dep from apt ... ==="
+sudo apt -y install pip
+sudo apt -y install python3-venv
+sudo apt -y install python-is-python3
+sudo apt -y install ros-humble-desktop
+sudo apt -y install ros-humble-ros-base
+sudo apt -y install ros-dev-tools
+sudo apt -y install ros-humble-joy
+sudo apt -y install can-utils
+sudo apt -y install qt6-base-dev
+sudo apt -y install python3-gi
+sudo apt -y install libssh-dev
+sudo apt -y install sl
+
+echo -e "\e[0;32m[OK]\e[0m"
+
+echo "=== Installing dep from pip ... ==="
+pip install --upgrade setuptools==58.2.0
+pip install --upgrade empy==3.3.4
+pip install --upgrade opencv-python
+pip install --upgrade opencv-contrib-python
+echo -e "\e[0;32m[OK]\e[0m"
+
+echo -e "=== \e[0;32m[SUCCESS]\e[0m Depedencies updated ==="
