765d3034cd03ac93e0cd03d7ce02369e668df1f5
pmichaud29 <micp1402@usherbrooke.ca>
2024-11-16 15:36:25 -0500

    Removed rover_helper to only use rover_msgs + cleanup

--- scripts/lint-apply.sh
+++ scripts/lint-apply.sh
@@ -3,9 +3,12 @@
 # Enable strict error handling
 set -e
 
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
 # Finding the .clang-format file
 echo "=== Finding .clang-format file... ==="
-if [ -z "$(find . -name '.clang-format' -print -quit)" ]; then
+if [ -z "$(find $SCRIPT_DIR/.. -name '.clang-format' -print -quit)" ]; then
+    echo "$SCRIPT_DIR"
     echo "[FAILED] .clang-format file not found in the repository!"
     exit 1
 else
