8b0b4dbed96c37f2ee35585fb6fb0a36ace5f530
emiles <emilesavoie39@gmail.com>
2025-01-25 18:42:27 -0500

    Working sim for arm

--- rover_arm/launch/arm.launch.py
+++ rover_arm/launch/arm.launch.py
@@ -9,16 +9,7 @@ def generate_launch_description():
                                     executable="teleop",
                                     name="teleop"
                                     )
-        arm_simulation = Node(
-            package="rover_arm",
-            namespace="/rover/arm",
-            executable="arm_sim.py",
-            name="arm_simulation",
-            output="screen",             
-        )
-
         
         ld.add_action(teleop_node)
-        ld.add_action(arm_simulation)
 
         return ld
--- rover_sim/launch/sim.launch.py
+++ rover_sim/launch/sim.launch.py
@@ -6,8 +6,8 @@ def generate_launch_description():
 
         sim_node = Node (package="rover_sim",
                                     namespace="/rover/arm",
-                                    executable="arm_sim.py",
-                                    name="arm_sim"
+                                    executable="sim.py",
+                                    name="sim"
                                     )
         
         ld.add_action(sim_node)
