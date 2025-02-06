92dbad91247912bcae83cb2193bfe59bf7ca72ab
Araa2876 <anibal.arango@usherbrooke.ca>
2025-02-05 21:39:10 -0500

    cpp to ROS2 architecture

--- rover_video/src/media_server.cpp
+++ rover_video/src/media_server.cpp
@@ -1,5 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rovus_lib/macros.h"
+#include "rover_msgs/msg/Screenshot.hpp"
+#include "rover_msgs/msg/Recording.hpp"
+
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgcodecs.hpp"
@@ -7,12 +10,71 @@
 #include <cstdlib>
 #include <sys/stat.h>
 
-std::string selectCam(int camID)
+class CameraNode : public rclcpp::Node
 {
+    public:
+
+    private:
+    std::string cameraURL = "";
+
+    //Gotta ask Philippe if i can do this
+    std::string dir = GET_PACKAGE_SOURCE_DIR("rover_video"); // finds the path to our package 
+    // Path necessary for the screenshots folder 
+    /* Look for const in doc, i think they are illegal in Rovus */
+    const std::string screenshotFolderPath = std::string(dir) + "/src/screenshots";
+    // Path necessary for the recordings folder
+    const std::string recordingFolderPath = std::string(dir) + "/src/recordings";   
+
+    std::string selectCameraURL(int camID);
+    int screenshotIPCam(std::string cameraURL, std::string folder); 
+    int recordingIPCam(std::string cameraURL, std::string folder);
+    bool folderExists(const std::string& path);
+    bool createFolder(const std::string& path);
+
+    public:
+    CameraNode();
+    ~CameraNode() {}
+};
+
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+
+    try
+    {
+        rclcpp::spin(std::make_shared<CameraNode>());
+    }
+    catch (const std::exception& e)
+    {
+        RCLCPP_FATAL(rclcpp::get_logger("Dead Node"), "Killing node on exception: %s", e.what());
+    }
+
+    rclcpp::shutdown();
+    return 0;
+}
+
+CameraNode::CameraNode(): Node("media_server")
+{
+    // Default value set to 0, probably will change it later
+    this->declare_parameter<int>("cameraID", 0);
+    int cameraID = this->get_parameter("cameraID").as_int();
+    RCLCPP_INFO_ONCE(this->get_logger(), "Camera ID: %d", cameraID);
+
+    _srv_screenshot = this->create_service<rover_msgs::srv::Screenshot>(
+        "screenshot",
+        std::bind(&CameraNode::screenshotIPCam, this, std::placeholders::_1, std::placeholders::_2))
+
+    _srv_recording = this->create_service<rover_msgs::srv::Recording>(
+        "recording",
+        std::bind(&CameraNode::recordingIPCam, this, std::placeholders::_1, std::placeholders::_2))
+
+}
+
+std::string CameraNode::selectCameraURL(int camID)
+{
+    // Rethink the while(camURL == "") loop
     std::string camURL = "";
 
-    while(camURL == "")
-    {
         switch(camID)
         {
             case 25:
@@ -24,18 +86,16 @@ std::string selectCam(int camID)
                 break;
 
             default:
-                std::cout << "Invalid camera ID" << std::endl;
-                std::cout << "Enter new camera ID: ";
-                std::cin >> camID;
+                RCLCPP_ERROR(LOGGER, "Invalid Camera ID");
                 break;
 
         }
-    }
-    
+
     return camURL;
+
 }
 
-int screenshotIPCam(std::string cameraURL, std::string folder) 
+int CameraNode::screenshotIPCam(std::string cameraURL, std::string folder) 
 {
     // The URL format will depend on the camera model and configuration
     //std::string camera_url = "rtsp://rover:roverrover@192.168.144.25:554/1/h264major";
@@ -43,10 +103,18 @@ int screenshotIPCam(std::string cameraURL, std::string folder)
     // Open the video stream
     cv::VideoCapture cap(cameraURL);
 
+    if(!createFolder(folder))
+    {
+        RCLCPP_ERROR(LOGGER, "Failed to create screenshots folder or it already exists.");
+        response->success = false;
+        return;
+    }
+
     if (!cap.isOpened()) 
     {
-        std::cerr << "Error: Unable to access the camera stream." << std::endl;
-        return -1;
+        RCLCPP_ERROR(LOGGER "Failed to open camera stream.");
+        response->success = false; //what does it do??
+        return; //necessary??
     }
 
     // Read a single frame
@@ -59,7 +127,7 @@ int screenshotIPCam(std::string cameraURL, std::string folder)
         // Save the frame as a sceenshot:
         std::string filename = folder + "/ip_camera_screenshot.png";
         cv::imwrite(filename, frame);
-        std::cout << "Screenshot saved as " << filename << std::endl;
+        RCLCPP_INFO(LOGGER, "Screenshot saved as %s", filename);
         
 
         // Display the frame
@@ -78,14 +146,22 @@ int screenshotIPCam(std::string cameraURL, std::string folder)
     return 0;
 }
 
-int recordingIpCam(std::string cameraURL, std::string folder)
+int CameraNode::recordingIPCam(std::string cameraURL, std::string folder)
 {
     cv::VideoCapture cap(cameraURL);
 
+        if(!createFolder(folder))
+    {
+        RCLCPP_ERROR(LOGGER, "Failed to create screenshots folder or it already exists.");
+        response->success = false;
+        return;
+    }
+
     if(!cap.isOpened())
     {
-        std::cerr << "Error: Unable to access the camera stream." << std::endl;
-        return -1;
+        RCLCPP_ERROR(LOGGER, "Failed to open camera stream.");
+        response->success = false;
+        return;
     }
 
 
@@ -95,27 +171,29 @@ int recordingIpCam(std::string cameraURL, std::string folder)
     int frame_height = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
     int fps = static_cast<int>(cap.get(cv::CAP_PROP_FPS));
 
-    std::string path_to_folder = folder + "/ip_cam_recording.avi";
+    std::string pathToFolder = folder + "/ip_cam_recording.avi";
 
     // Define the codec and create a VideoWriter object     
     /* Also from ChatGPT --> more information on OpenCV 
     --> https://docs.opencv.org/4.x/dd/d9e/classcv_1_1VideoWriter.html */
-    cv::VideoWriter video_writer(path_to_folder, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),
+    cv::VideoWriter video_writer(pathToFolder, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),
     (fps > 0 ? fps : 30), cv::Size(frame_width, frame_height));
 
     if(!video_writer.isOpened())
     {
-        std::cerr << "Error: Could not open the output video file for writing!" << std::endl;
+        RCLCPP_ERROR(LOGGER, "Error: Could not open the output video file for writing!");
+        response->success = false;
     }
 
-    std::cout << "Recording... Press 'q' to stop." << std::endl;
+    RCLCPP_INFO(LOGGER, "Recording... Press 'q' to stop.");
 
     cv::Mat frame;
     for(EVER) 
     {
         cap >> frame;
-        if (frame.empty()) {
-            std::cerr << "Error: Blank frame grabbed!" << std::endl;
+        if (frame.empty()) 
+        {
+            RCLCPP_ERROR(LOGGER, "Error: Blank frame grabbed!");
             break;
         }
 
@@ -136,20 +214,20 @@ int recordingIpCam(std::string cameraURL, std::string folder)
     video_writer.release();
     cv::destroyAllWindows();
 
-    std::cout << "Recording stopped." << std::endl;
+    RCLCPP_INFO(LOGGER, "Recording stopped.");
 
     return 0;
 }
 
 // Verifies if screenshot folder already exists
-bool folderExists(const std::string& path)    
+bool CameraNode::folderExists(const std::string& path)    
 {
     struct stat info;
     return (stat(path.c_str(), &info) == 0 && (info.st_mode & S_IFDIR)); // I dont exactly understand this part
 }
 
 // Creating screenshot Folder if doesnt already exists
-bool createFolder(const std::string& path)    
+bool CameraNode::createFolder(const std::string& path)    
 {
     if (!folderExists(path)) 
     {
@@ -166,54 +244,4 @@ bool createFolder(const std::string& path)
     }
    // std::cout << "Directory already exists: " << path << std::endl;
     return true;
-}
-
-
-
-int main()
-{
-    int camID = 0;
-    int status = 0;
-
-    std::string dir = GET_PACKAGE_SOURCE_DIR("rover_video"); // finds the path to our package 
-    const std::string screenshotFolderPath = std::string(dir) + "/src/screenshots"; // Path necessary for the screenshots folder /* Look for const in doc, i think they are illegal in Rovus */
-    const std::string recordingFolderPath = std::string(dir) + "/src/recordings";   // Path necessary for the recordings folder
-
-    createFolder(screenshotFolderPath);
-    createFolder(recordingFolderPath);
-
-    std::cout << "Enter camera ID:" << std::endl;
-    std::cin >> camID;
-    std::string cameraURL = selectCam(camID);
-
-   while(status != 'e') // Keeps printing the menu until the user wants to quit
-   {
-        std::cout << "What do you want to do?" << std::endl;
-        std::cout << "1: Screenshot" << std::endl << "2: Recording" << std::endl << "3: Exit" << std::endl;
-        std::cin >> status;
-
-        switch (status) // Basic switch case to create a menu using the different functions
-        {
-            case 1:
-                screenshotIPCam(cameraURL, screenshotFolderPath);
-                status = 0;
-                break;
-
-            case 2:
-                recordingIpCam(cameraURL, recordingFolderPath);
-                status = 0;
-                break;
-
-            case 3: 
-                status = 'e';
-                break;
-                
-            default:
-                std::cout << "Invalid option." << std::endl;
-                status = 0;
-                break;
-        }
-    
-    }
-    return 0;
 }
