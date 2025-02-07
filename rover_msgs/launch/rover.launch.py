3a80d9aba437d34858472aeb11216e0dc14424d1
emiles <emilesavoie39@gmail.com>
2025-02-07 18:23:21 -0500

    Added jacobian matrix

--- rover_msgs/launch/rover.launch.py
+++ rover_msgs/launch/rover.launch.py
@@ -14,8 +14,8 @@ def generate_launch_description():
 
     return LaunchDescription([
         simulate_arm_arg,
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('rover_can'), 'launch', 'can.launch.py'])])),
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('rover_can'), 'launch', 'can.launch.py'])])),
         
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('rover_security'), 'launch', 'security_rover.launch.py'])])),
@@ -27,10 +27,10 @@ def generate_launch_description():
             PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('rover_auxiliary'), 'launch', 'auxiliary_rover.launch.py'])])),
 
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('rover_arm'), 'launch', 'arm.launch.py'])])),
+            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('rover_arm'), 'launch', 'arm.launch.py'])]))
 
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('rover_sim'), 'launch', 'sim.launch.py'])]),
-            launch_arguments={'simulate_arm': simulate_arm}.items()
-        )
+        # IncludeLaunchDescription(
+            # PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('rover_sim'), 'launch', 'sim.launch.py'])]),
+            # launch_arguments={'simulate_arm': simulate_arm}.items()
+        # )
     ])
