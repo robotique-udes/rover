2a21d387b17e0df4322cbdc75afb2dc4ae8fce96
Charles-robot <chucklafond2005@gmail.com>
2025-03-08 00:45:49 -0500

    Thread V2

--- rover_video/src/media_server.cpp
+++ rover_video/src/media_server.cpp
@@ -82,7 +82,6 @@ void CameraNode::controlIPCam(const std::shared_ptr<rover_msgs::srv::CameraContr
             }
 
                 break;
-
         case rover_msgs::srv::CameraControl::Request::STOP_RECORDING:
             if(stopRecording(cameraURL))
             {
