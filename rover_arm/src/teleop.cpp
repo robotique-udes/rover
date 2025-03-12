c2f45277a83cf2cbae3e7b0967def1f03001d694
emiles <emilesavoie39@gmail.com>
2025-03-11 23:22:31 -0400

    Working arm. Needs cleanup + singularity avoidance

--- rover_arm/src/teleop.cpp
+++ rover_arm/src/teleop.cpp
@@ -19,6 +19,14 @@ constexpr uint64_t TOGGLE_DEBOUNCE_TIME_MS = 150ul;
 constexpr float JOINT_CONTROL_SPEED_FACTOR = 0.5f;  // Factor of max speed
 constexpr uint8_t MAX_RECORDED_POINTS = 3;
 
+// This is the threshold below which singularity avoidance activates. 
+// It defines the "danger zone" for approaching singularities.
+constexpr float MANIPULABILITY_THRESHOLD = 0.1f;
+
+// This determines how aggressively the robot responds to avoid singularities. 
+// It scales the nullspace component added to joint velocities.
+constexpr float NULLSPACE_GAIN = 0.4f;
+
 bool isPressed(float buttonValue_);
 
 class Teleop : public rclcpp::Node
@@ -27,7 +35,6 @@ public:
     enum class eJointIndex : uint8_t
     {
         JL = rover_msgs::msg::ArmMsg::JL,
-        // J0 = rover_msgs::msg::ArmMsg::J0,
         J1 = rover_msgs::msg::ArmMsg::J1,
         J2 = rover_msgs::msg::ArmMsg::J2,
         GRIPPER_TILT = rover_msgs::msg::ArmMsg::GRIPPER_TILT,
@@ -39,6 +46,7 @@ public:
     enum class eButtonId : uint8_t
     {
         RECORD = KEYBINDING::RECORD,
+        CLEAR_POINTS = KEYBINDING::CLEAR_POINTS,
         CREATE_PLAN = KEYBINDING::CREATE_PLAN
     };
 
@@ -71,43 +79,47 @@ private:
     Eigen::MatrixXd _jacobian = Eigen::MatrixXd(4, 4);
     Eigen::MatrixXd _inverseJacobian = Eigen::MatrixXd(4, 4);
     Eigen::MatrixXd _rotationMatrix = Eigen::MatrixXd(3, 3);
+    Eigen::MatrixXd _nullspaceProjector = Eigen::MatrixXd(4, 4);
     Eigen::VectorXd _computedVelocity = Eigen::VectorXd(4);
-    Eigen::VectorXd _currentEndEffectorPosition = Eigen::VectorXd(3); 
+    Eigen::VectorXd _currentEndEffectorPosition = Eigen::VectorXd(3);
+    Eigen::VectorXd _currentJointsPos = Eigen::VectorXd::Zero((uint8_t)eJointIndex::eLAST);
+    Eigen::VectorXd _desiredCartesian = Eigen::VectorXd::Zero((uint8_t)eDesiredCartesianVel::eLAST);
+    Eigen::VectorXd _gradientManipulability = Eigen::VectorXd(4);
     std::array<Eigen::VectorXd, 3> _poseArray;
+
+    eControlMode _controlMode = eControlMode::CARTESIAN;
     
+    uint8_t _pointsRecorded = 0;
+    float _manipulabilityMeasure = 0.0f;
+
     bool _currentPoseFailure = false;
+    bool _applyPlan = false;
     bool _gripperClose = false;
     bool _gripperCloseLatchFlag = false;
 
     std::map<eButtonId, bool> _buttonFlags = {
         {eButtonId::RECORD, false},
+        {eButtonId::CLEAR_POINTS, false},
         {eButtonId::CREATE_PLAN, false}
     };
-
-    uint8_t _pointsRecorded = 0;
 
     RoverLib::Timer<uint64_t, RoverLib::millis> timerDebounce
         = RoverLib::Timer<uint64_t, RoverLib::millis>(TOGGLE_DEBOUNCE_TIME_MS);
     
-    bool isSelected(float buttonValue_, eButtonId buttonId);
-    void positionCallback(const rover_msgs::msg::ArmMsg::SharedPtr positionMsg);
-    void joyCallback(const rover_msgs::msg::Joy::SharedPtr positionMsg);
-    void scaleVelocities(Eigen::VectorXd& jointVelocities);
-    void addPoint(Eigen::VectorXd pose);
-    // void applyTransform(Eigen::VectorXd currentPose, std::array<Eigen::VectorXd, 3> poseArray);
-
-    // TODO : remove return values for these functions as they modify class elements directly
-    Eigen::MatrixXd computeJacobian(const Eigen::VectorXd& currentJointPosition);
-
-    // FOR TESTING PURPOSES ONLY
-    void printEndEffectorPosition();
-
-    void watchdog(bool& rLostHeartbeat);
+    bool isSelected(float buttonValue_, eButtonId buttonId_);
+    void positionCallback(const rover_msgs::msg::ArmMsg::SharedPtr positionMsg_);
+    void joyCallback(const rover_msgs::msg::Joy::SharedPtr positionMsg_);
+    void scaleVelocities(Eigen::VectorXd& jointVelocities_);
+    void addPoint(Eigen::VectorXd pose_);
+    void calcManipulability(void);
     rover_msgs::msg::ArmMsg getZeroMsg(void);
 
-    eControlMode _controlMode = eControlMode::CARTESIAN;
-    Eigen::VectorXd _currentJointsPos = Eigen::VectorXd::Zero((uint8_t)eJointIndex::eLAST);
-    Eigen::VectorXd _desiredCartesian = Eigen::VectorXd::Zero((uint8_t)eDesiredCartesianVel::eLAST);
+    // TODO FIX WATCHDOG
+    void watchdog(bool& rLostHeartbeat_);
+
+    // TODO : remove return values for these functions as they modify class elements directly
+    Eigen::MatrixXd computeJacobian(const Eigen::VectorXd& currentJointPosition_);
+
 };
 
 Teleop::Teleop() : Node("teleop")
@@ -151,16 +163,6 @@ void Teleop::joyCallback(const rover_msgs::msg::Joy::SharedPtr joyMsg_)
         {
             goalJointsSpeed((uint8_t)eJointIndex::JL) = -ARM_CONFIGURATION::JL::MAX_VELOCITY;
         }
-
-        // CMD J0
-        // if (isPressed(joyMsg_->joy_data[KEYBINDING::J0_FWD]))
-        // {
-        //     goalJointsSpeed((uint8_t)eJointIndex::J0) = ARM_CONFIGURATION::J0::MAX_VELOCITY;
-        // }
-        // else if (isPressed(joyMsg_->joy_data[KEYBINDING::J0_REV]))
-        // {
-        //     goalJointsSpeed((uint8_t)eJointIndex::J0) = -ARM_CONFIGURATION::J0::MAX_VELOCITY;
-        // }
 
         // CMD J1
         goalJointsSpeed((uint8_t)eJointIndex::J1) = joyMsg_->joy_data[KEYBINDING::J1] * ARM_CONFIGURATION::J1::MAX_VELOCITY;
@@ -221,17 +223,19 @@ void Teleop::joyCallback(const rover_msgs::msg::Joy::SharedPtr joyMsg_)
         {
             desiredCartesian((uint8_t)eDesiredCartesianVel::ALPA) = joyMsg_->joy_data[KEYBINDING::ALPHA];
         }
-        
-        // PSI
-        // if (isPressed(joyMsg_->joy_data[KEYBINDING::PSI]))
-        // {
-        //     desiredCartesian((uint8_t)eDesiredCartesianVel::PSI) = joyMsg_->joy_data[KEYBINDING::PSI];
-        // }
 
         // RECORD END-EFFECTOR POSE
         if (isSelected(joyMsg_->joy_data[KEYBINDING::RECORD], eButtonId::RECORD))
         {
             addPoint(_currentEndEffectorPosition);
+        }
+
+        // CLEAR POSE ARRAY
+        if (isSelected(joyMsg_->joy_data[KEYBINDING::CLEAR_POINTS], eButtonId::CLEAR_POINTS))
+        {
+            std::array<Eigen::VectorXd, 3>{};
+            _pointsRecorded = 0;
+            RCLCPP_WARN(LOGGER, "Point array has been cleared");
         }
 
         if (isSelected(joyMsg_->joy_data[KEYBINDING::CREATE_PLAN], eButtonId::CREATE_PLAN))
@@ -242,21 +246,35 @@ void Teleop::joyCallback(const rover_msgs::msg::Joy::SharedPtr joyMsg_)
             }
             else
             {
-                Eigen::Vector3d vector12 = _poseArray[1] - _poseArray[0];
-                Eigen::Vector3d vector13 = _poseArray[2] - _poseArray[0];
-                Eigen::Vector3d zAxis = vector12.cross(vector13).normalized();
-                Eigen::Vector3d xAxis = vector12.normalized();     
-                Eigen::Vector3d yAxis = zAxis.cross(xAxis);
+                _applyPlan = !_applyPlan;
 
-                _rotationMatrix.col(0) = xAxis;
-                _rotationMatrix.col(1) = yAxis;
-                _rotationMatrix.col(2) = zAxis;
-
-                desiredCartesian.head(3) = _rotationMatrix * desiredCartesian.head(3);
+                if(_applyPlan)
+                {
+                    RCLCPP_INFO(LOGGER, "Applying plan");
+                }
+                else if(!_applyPlan)
+                {
+                    RCLCPP_INFO(LOGGER, "Unapplying plan");
+                }
             }
         }
 
-    _inverseJacobian = computeJacobian(_currentJointsPos).completeOrthogonalDecomposition().pseudoInverse();
+        if(_applyPlan)
+        {
+            Eigen::Vector3d vector12 = _poseArray[1] - _poseArray[0];
+            Eigen::Vector3d vector13 = _poseArray[2] - _poseArray[0];
+            Eigen::Vector3d zAxis = vector12.cross(vector13).normalized();
+            Eigen::Vector3d xAxis = vector12.normalized();     
+            Eigen::Vector3d yAxis = zAxis.cross(xAxis);
+
+            _rotationMatrix.col(0) = xAxis;
+            _rotationMatrix.col(1) = yAxis;
+            _rotationMatrix.col(2) = zAxis;
+            
+            desiredCartesian.head(3) = _rotationMatrix * desiredCartesian.head(3);
+        }
+
+        _inverseJacobian = computeJacobian(_currentJointsPos).completeOrthogonalDecomposition().pseudoInverse();
         _computedVelocity = _inverseJacobian * desiredCartesian;
         scaleVelocities(_computedVelocity);
         
@@ -299,8 +317,6 @@ void Teleop::positionCallback(const rover_msgs::msg::ArmMsg::SharedPtr positionM
     {
         _currentJointsPos(i) = positionMsg_->data[i];
     }
-
-    printEndEffectorPosition();
 }
 
 void Teleop::addPoint(Eigen::VectorXd pose_)
@@ -315,12 +331,6 @@ void Teleop::addPoint(Eigen::VectorXd pose_)
         _pointsRecorded ++;
     
         RCLCPP_INFO(LOGGER, "Point has been added to array");
-
-        for (size_t i = 0; i < _pointsRecorded; ++i)
-        {
-            RCLCPP_INFO(LOGGER, "Point %zu: [%f, %f, %f]", i,
-                        pose_(0) ,pose_(1), pose_(2));
-        }
     }
 }
 
@@ -342,12 +352,12 @@ void Teleop::watchdog(bool& rLostHeartbeat_)
     }
 }
 
-Eigen::MatrixXd Teleop::computeJacobian(const Eigen::VectorXd& currentJointPosition)
+Eigen::MatrixXd Teleop::computeJacobian(const Eigen::VectorXd& currentJointPosition_)
 {
-    float q0 = currentJointPosition(0);
-    float q1 = currentJointPosition(1);
-    float q2 = currentJointPosition(2);
-    float q3 = currentJointPosition(3);
+    float q0 = currentJointPosition_(0);
+    float q1 = currentJointPosition_(1);
+    float q2 = currentJointPosition_(2);
+    float q3 = currentJointPosition_(3);
 
     float s1 = sin(q1);
     float c1 = cos(q1);
@@ -356,71 +366,36 @@ Eigen::MatrixXd Teleop::computeJacobian(const Eigen::VectorXd& currentJointPosit
     float s123 = sin(q1 + q2 + q3);
     float c123 = cos(q1 + q2 + q3);
 
-    // float s2 = sin(0.5 * PI - q2);
-    // float c2 = cos(0.5 * PI - q2);
-    // float s23 = sin(0.5 * PI - q2 - q3);
-    // float c23 = cos(0.5 * PI - q2 - q3);
-
     _currentEndEffectorPosition.x() = q0;
-
     _currentEndEffectorPosition.y() = J1 * s1 + J2 * s12 + J3 * s123;
-
     _currentEndEffectorPosition.z() = J1 * s1 + J2 * s12 + J3 * s123;
 
-    // printEndEffectorPosition();
-    
-    _jacobian(0, 0) = 1.0f; // dx/dq0
-    _jacobian(0, 1) = 0.0f; // dx/dq1
-    _jacobian(0, 2) = 0.0f; // dx/dq2
-    _jacobian(0, 3) = 0.0f; // dx/dq3
+    _jacobian(0, 0) = 1.0f;                             // dx/dq0
+    _jacobian(0, 1) = 0.0f;                             // dx/dq1
+    _jacobian(0, 2) = 0.0f;                             // dx/dq2
+    _jacobian(0, 3) = 0.0f;                             // dx/dq3
 
-    _jacobian(1, 0) = J1 * c1 + J2 * c12 + J3 * c123 + J2; // dy/dq0
-    _jacobian(1, 1) = J1 * c1 + J2 * c12 + J3 * c123; // dy/dq1
-    _jacobian(1, 2) = J2 * c12 + J3 * c123; // dy/dq2
-    _jacobian(1, 3) = J3 * c123; // dy/dq3
+    _jacobian(1, 0) = 0.0f;                             // dy/dq0
+    _jacobian(1, 1) = J1 * c1 + J2 * c12 + J3 * c123;   // dy/dq1
+    _jacobian(1, 2) = J2 * c12 + J3 * c123;             // dy/dq2
+    _jacobian(1, 3) = J3 * c123;                        // dy/dq3
 
-    _jacobian(2, 0) = 0.0f; // dz/dq0
-    _jacobian(2, 1) = J1 * s1 + J2 * s12 + J3 * s123; // dz/dq1
-    _jacobian(2, 2) = J2 * s12 + J3 * s123; // dz/dq2
-    _jacobian(2, 3) = J3 * s123; // dz/dq3
+    _jacobian(2, 0) = 0.0f;                             // dz/dq0
+    _jacobian(2, 1) = J1 * s1 + J2 * s12 + J3 * s123;   // dz/dq1
+    _jacobian(2, 2) = J2 * s12 + J3 * s123;             // dz/dq2
+    _jacobian(2, 3) = J3 * s123;                        // dz/dq3
 
-    _jacobian(3, 0) = 0.0f; //dxalpha/dq0
-    _jacobian(3, 1) = 0.0f; //dxalpha/dq0 
-    _jacobian(3, 2) = 0.0f; //dxalpha/dq2
-    _jacobian(3, 3) = 0.0f; //dxalpha/dq3
-
-    // _jacobian(0, 0) = 0.0f; // dx/dq0
-    // _jacobian(0, 1) = J1x * -s1 + J2x * -s1 * c2 + J2z * -s1 * s2 + J3x * -s1 * c23 + J3z * -s1 * s23 - c1 * (J1y + J2y) + J4x * c1 * c234 + J4z * c1 * s234; // ∂x/dq1
-    // _jacobian(0, 2) = J2x * c1 * -s2 + J2z * c1 * c2 + J3x * c1 * -s23 + J3z * c1 * c23 + J4x * s1 * -s234 + J4z * s1 * c234; // dx/dq2
-    // _jacobian(0, 3) = J3x * c1 * -s23 + J3z * c1 * c23 + J4x * s1 * -s234 + J4z * s1 * c234; // dx/dq3
-    // _jacobian(0, 4) = J4x * s1 * -s234 + J4z * s1 * c234; // dx/dq4
-
-    // _jacobian(1, 0) = 1.0f;  // dy/dq0
-    // _jacobian(1, 1) = J1x * c1 + -s1 * (J1y + J2y) + J2x * c1 * c2 + J2z * c1 * s2 + J3x * c1 * c23 + J3z * c1 * s23 + J4x * c1 * c234 + J4z * c1 * s234;  // dy/dq1
-    // _jacobian(1, 2) = J2x * s1 * -s2 + J2z * s1 * c2 + J3x * s1 * -s23 + J3z * s1 * c23 + J4x * s1 * -s234 + J4z * s1 * c234;  // dy/dq2
-    // _jacobian(1, 3) = J3x * s1 * -s23 + J3z * s1 * c23 + J4x * s1 * -s234 + J4z * s1 * c234;  // dy/dq3
-    // _jacobian(1, 4) = J4x * s1 * -s234 + J4z * s1 * c234;  // dy/dq4
-
-    // _jacobian(2, 0) = 0.0f; // dz/dq0
-    // _jacobian(2, 1) = 0.0f; // ∂z/dq1
-    // _jacobian(2, 2) = J2z * -s2 + J3z * -s23 + J4z * -s234 - J2x * c2 - J3x * c23 - J4x * c234; // dz/dq2
-    // _jacobian(2, 3) = J3z * -s23 + J4z * -s234 - J3x * c23 - J4x * c234; // dz/dq3
-    // _jacobian(2, 4) = J4z * -s234 - J4x * c234; // dz/dq4
-
-    // NULLSPACE
-    // _jacobian(3, 0) = 0.0f;
-    // _jacobian(3, 1) = 0.0f;
-    // _jacobian(3, 2) = 1.0f;
-    // _jacobian(3, 3) = 1.0f;
-    // _jacobian(3, 4) = 1.0f;
-
-    // _jacobian(4, 0) = 0.0f;
-    // _jacobian(4, 1) = 1.0f;
-    // _jacobian(4, 2) = 0.0f;
-    // _jacobian(4, 3) = 0.0f;
-    // _jacobian(4, 4) = 0.0f;
+    _jacobian(3, 0) = 0.0f;                             // dxalpha/dq0
+    _jacobian(3, 1) = 0.0f;                             // dxalpha/dq0 
+    _jacobian(3, 2) = 0.0f;                             // dxalpha/dq2
+    _jacobian(3, 3) = 0.0f;                             // dxalpha/dq3
 
     return _jacobian;
+}
+
+void Teleop::calcManipulability(void)
+{
+
 }
 
 rover_msgs::msg::ArmMsg Teleop::getZeroMsg(void)
@@ -431,44 +406,31 @@ rover_msgs::msg::ArmMsg Teleop::getZeroMsg(void)
     return msg;
 }
 
-void Teleop::scaleVelocities(Eigen::VectorXd& velocities)
+void Teleop::scaleVelocities(Eigen::VectorXd& velocities_)
 {
-    // float velocityRatio = std::max({
-    //     abs(static_cast<float>(velocities(0) / ARM_CONFIGURATION::JL::MAX_VELOCITY)),  
-    //     abs(static_cast<float>(velocities(1) / ARM_CONFIGURATION::J0::MAX_VELOCITY)),
-    //     abs(static_cast<float>(velocities(2) / ARM_CONFIGURATION::J1::MAX_VELOCITY)),
-    //     abs(static_cast<float>(velocities(3) / ARM_CONFIGURATION::J2::MAX_VELOCITY)),
-    //     abs(static_cast<float>(velocities(4) / ARM_CONFIGURATION::GRIPPER_TILT::MAX_VELOCITY))
-    // });
+    float velocityRatio = std::max({
+        abs(static_cast<float>(velocities_(0) / ARM_CONFIGURATION::JL::MAX_VELOCITY)),  
+        abs(static_cast<float>(velocities_(1) / ARM_CONFIGURATION::J1::MAX_VELOCITY)),
+        abs(static_cast<float>(velocities_(2) / ARM_CONFIGURATION::J2::MAX_VELOCITY)),
+        abs(static_cast<float>(velocities_(3) / ARM_CONFIGURATION::GRIPPER_TILT::MAX_VELOCITY))
+    });
 
-
-    // if(velocityRatio > 1.0f)
-    // {
-    //     velocities /= velocityRatio;
-    // }
-}
-
-void Teleop::printEndEffectorPosition()
-{
-    RCLCPP_INFO(
-        this->get_logger(),
-        "End Effector Position - X: %.3f, Y: %.3f, Z: %.3f",
-        _currentEndEffectorPosition.x(),
-        _currentEndEffectorPosition.y(),
-        _currentEndEffectorPosition.z()
-    );
-}
-
-bool Teleop::isSelected(float buttonValue_, eButtonId buttonId)
-{
-    if(isPressed(buttonValue_) && !_buttonFlags[buttonId])
+    if(velocityRatio > 1.0f)
     {
-        _buttonFlags[buttonId] = true;
+        velocities_ /= velocityRatio;
+    }
+}
+
+bool Teleop::isSelected(float buttonValue_, eButtonId buttonId_)
+{
+    if(isPressed(buttonValue_) && !_buttonFlags[buttonId_])
+    {
+        _buttonFlags[buttonId_] = true;
         return isPressed(buttonValue_);
     }
-    else if(!isPressed(buttonValue_) && _buttonFlags[buttonId])
+    else if(!isPressed(buttonValue_) && _buttonFlags[buttonId_])
     {
-        _buttonFlags[buttonId] = false;
+        _buttonFlags[buttonId_] = false;
         return false;
     }
     else
