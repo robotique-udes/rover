877f5271d0fb76fa802f3f2e9fd1b54842aa4b9e
Charles-robot <chucklafond2005@gmail.com>
2025-03-26 18:21:47 -0400

    formal temp dir

--- rover_video/src/screenshot_server.hpp
+++ rover_video/src/screenshot_server.hpp
@@ -27,6 +27,15 @@ constexpr uint8_t RECORDING_INTERVAL = 30;  // in seconds
 
 /* This folder is used for the functions' declarations */
 
+std::string getTempdir()
+{
+    const char* temp = std::getenv("TMPDIR");
+    if (!temp) temp = std::getenv("TMP");
+    if (!temp) temp = std::getenv("TEMP");
+    if (!temp) temp = "/tmp"; // Default for Ubuntu
+    return std::string(temp);
+}
+
 class Recording
 {
   public:
@@ -380,7 +389,7 @@ bool CameraNode::getScreenshot(std::string screenshotFolderPath, std::string fil
 bool Recording::startRecording()
 {
     // Use the provided file name or a default name
-    std::string filePath =  "/tmp/" + this->filename;
+    std::string filePath =  getTempdir() + "/" + this->filename;
     filePath.insert(filePath.length() - 4, '_' + std::to_string(this->recordingNumber++));  // add recording number before .avi
     this->files.push_back(filePath);                                                        // add file to list of recordings
 
@@ -447,7 +456,7 @@ bool Recording::recordFrame()
 
     if (difftime(time(0), this->startTime) >= RECORDING_INTERVAL)  // save every RECORDING_INTERVAL seconds
     {
-        std::string filePath = "/tmp/" + this->filename;
+        std::string filePath = getTempdir() + "/" + this->filename;
         filePath.insert(filePath.length() - 4, '_' + std::to_string(this->recordingNumber++));
         this->files.push_back(filePath);
         this->video_writer.release();
