492c02d9b34ed6e6c54ec0a9b51ea75ff6234c66
ChristopherLajoie <lajc1503@usherbrooke.ca>
2025-03-26 17:38:50 -0600

    little changes

--- rover_gui/src/QRtspPlayer/QRtspPlayerWidget.hpp
+++ rover_gui/src/QRtspPlayer/QRtspPlayerWidget.hpp
@@ -12,6 +12,7 @@
 class RtspPlayerWidget : public QWidget
 {
     Q_OBJECT
+    static constexpr int RECONNECT_INTERVAL = 5000;
 
   public:
     explicit RtspPlayerWidget(QWidget* parent = nullptr);
