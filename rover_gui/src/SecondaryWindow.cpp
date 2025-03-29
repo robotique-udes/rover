ac559ea1a9da562f080401c0f20c16adb76996a8
gabduf <gab.duf04@icloud.com>
2025-03-28 20:12:03 -0400

    test

--- rover_gui/src/SecondaryWindow.cpp
+++ rover_gui/src/SecondaryWindow.cpp
@@ -1,12 +1,16 @@
 #include "SecondaryWindow.hpp"
 
-SecondaryWindow::SecondaryWindow():
+#include <QStackedWidget>
+
+SecondaryWindow::SecondaryWindow(std::shared_ptr<rclcpp::Node> guiNode_):
     QMainWindow(nullptr),
     _centralWidget(this),
     _layout(&_centralWidget),
-    _tempLabel("Future camera window", this)
+    _tempLabel("Future camera window", this),
+    _videoPlayerWidget(guiNode_,this)
 {
     _tempLabel.setAlignment(Qt::AlignCenter);
     _layout.addWidget(&_tempLabel);
+    _layout.addWidget(&_videoPlayerWidget);
     this->setCentralWidget(&_centralWidget);
 }
