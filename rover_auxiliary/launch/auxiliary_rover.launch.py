f7e9141c6dac23cac8f0e2b8d6ed752f49762c6a
Araa2876 <anibal.arango@usherbrooke.ca>
2025-04-19 12:47:46 -0400

    Removing compass_calibrator and light_control

--- rover_auxiliary/launch/auxiliary_rover.launch.py
+++ rover_auxiliary/launch/auxiliary_rover.launch.py
@@ -7,20 +7,6 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
     
-    node_compass_calibrator = Node(
-        package="rover_auxiliary",
-        namespace="/rover/auxiliary",
-        executable="compass_calibrator",
-        name="compass_calibrator"
-    )
-    
-    node_light_control = Node(
-        package="rover_auxiliary",
-        namespace="/rover/auxiliary",
-        executable="light_control",
-        name="light_control"
-    )
-
     node_ddb_control = Node(
         package="rover_auxiliary",
         namespace="/rover/auxiliary",
@@ -28,8 +14,6 @@ def generate_launch_description():
         name="ddb_control"
     )
     
-    
-    ld.add_action(node_compass_calibrator)
-    ld.add_action(node_light_control)
+    ld.add_action(node_ddb_control)
 
     return ld
