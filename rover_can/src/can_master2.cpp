b87f49526bf07bfab188261d0ac727e1bc5d2a1e
Ndumoulin <nathan.dumoulin11@outlook.com>
2025-04-29 22:36:04 -0400

    deleted libs in rover_can

--- rover_can/src/can_master2.cpp
+++ rover_can/src/can_master2.cpp
@@ -1,5 +1,25 @@
-int main(void)
+// ROS
+#include "rclcpp/rclcpp.hpp"
+
+#include "rover_can2/src/rover_can2/drivers/driver_linux.hpp"
+
+class CanMaster2 : public rclcpp::Node
 {
-    /* TODO */
+  public:
+    CanMaster2():
+        Node("can_master2")
+    {
+        
+    };
+};
+
+int main(int argc, char* argv[])
+{
+    RoverCan2::Drivers::DriverLinux canDriver;
+
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<CanMaster2>());
+    rclcpp::shutdown();
+
     return 0;
 }
