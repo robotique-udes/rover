1ef1a9ee81996cf2b211e3bf609a69e2aaa6620f
Araa2876 <anibal.arango@usherbrooke.ca>
2025-05-04 13:32:41 -0400

    push button test

--- rover_gui/src/QDeviceStatus/QDeviceStatus.cpp
+++ rover_gui/src/QDeviceStatus/QDeviceStatus.cpp
@@ -3,6 +3,7 @@
 QDeviceStatus::QDeviceStatus(std::shared_ptr<rclcpp::Node> guiNode_ , QWidget* parent_) : QWidget(parent_), _node(guiNode_)
 {
 	_ui.setupUi(this);
+    connect(_ui.test, &QPushButton::clicked, this, &QDeviceStatus::setStatusReport(RoverCan2::Constant::eDeviceId::MASTER_COMPUTER_UNIT));
 
 	_sub_deviceStatus = _node->create_subscription<rover_msgs::msg::CanDeviceStatus>(
 		"/rover/can/devices_status",
@@ -38,4 +39,54 @@ int QDeviceStatus::heightForWidth(int width_) const
 void QDeviceStatus::callbackDeviceInfos(const rover_msgs::msg::CanDeviceStatus& msg_)
 {
 	_deviceStatusInfo[msg_.id] = msg_;
+}
+
+void QDeviceStatus::setStatusReport(RoverCan2::Constant::eDeviceId id_)
+{
+    uint16_t deviceID = TO_UNDERLYING(id_);
+
+    auto label = _ui.StatusReport->findChild<QLabel*>("StatusInfo");
+    if (!label) {
+        RCLCPP_ERROR(_node->get_logger(), "StatusInfo not found!");
+        return;
+    }
+
+
+    // Look up device info
+    auto it = _deviceStatusInfo.find(deviceID);
+    if (it != _deviceStatusInfo.end()) {
+        const auto& deviceStatus = it->second;
+
+        // Compose your status string however you like
+        QString statusText = QString("ID: %1\nStatus: %2")
+                             .arg(deviceStatus.id)
+                             .arg(this->setErrorMsg(deviceStatus.error_state));
+
+        // Set label text
+        label->setText(statusText);
+    } else {
+        label->setText("Device not found.");
+    }
+}
+
+QString QDeviceStatus::setErrorMsg(uint8_t errorCode_)
+{
+    switch (errorCode_)
+    {
+        case rover_msgs::msg::CanDeviceStatus::STATUS_OK:
+            return "OK";
+            break;
+
+        case rover_msgs::msg::CanDeviceStatus::STATUS_WARNING:
+            return "Warning";
+            break;
+
+        case rover_msgs::msg::CanDeviceStatus::STATUS_ERROR:
+            return "Error";
+            break;
+
+        default:
+            return "Unknown";
+            break;
+    }
 }
--- rover_gui/src/QDeviceStatus/QDeviceStatus.hpp
+++ rover_gui/src/QDeviceStatus/QDeviceStatus.hpp
@@ -1,9 +1,11 @@
-//#ifndef __QDEVICESTATUS_HPP__
-#pragma once
+#ifndef __QDEVICESTATUS_HPP__
+#define __QDEVICESTATUS_HPP__
 
 // ROS
 #include "rclcpp/rclcpp.hpp"
 #include "rover_msgs/msg/can_device_status.hpp"
+#include "rover_can2/src/rover_can2/constant.hpp"
+#include "rover_lib2/src/rover_lib2/helpers/macros.hpp"
 
 // QT
 #include <QtWidgets/QGridLayout>
@@ -13,24 +15,25 @@
 #include <unordered_map>
 
 class QDeviceStatus : public QWidget
-   {
+{
+    Q_OBJECT
 
-      Q_OBJECT
+  public:
+    QDeviceStatus(std::shared_ptr<rclcpp::Node> guiNode_, QWidget* parent_);
+    ~QDeviceStatus() = default;
 
-   public:
-      QDeviceStatus(std::shared_ptr<rclcpp::Node> guiNode_, QWidget* parent_);
-   	~QDeviceStatus() = default;
+  protected:
+    int heightForWidth(int width_) const override;
 
-   protected:
-      int heightForWidth(int width_) const override;
+  private:
+    void callbackDeviceInfos(const rover_msgs::msg::CanDeviceStatus& msg_);
+    void setStatusReport(RoverCan2::Constant::eDeviceId id_);
+    QString setErrorMsg(uint8_t errorCode_);
 
-   private:
-      void callbackDeviceInfos(const rover_msgs::msg::CanDeviceStatus& msg_);
+    std::shared_ptr<rclcpp::Node> _node;
+    Ui::DeviceStatus _ui;
+    rclcpp::Subscription<rover_msgs::msg::CanDeviceStatus>::SharedPtr _sub_deviceStatus;
+    std::unordered_map<uint16_t, rover_msgs::msg::CanDeviceStatus> _deviceStatusInfo;
+};
 
-      std::shared_ptr<rclcpp::Node> _node;
-   	Ui::DeviceStatus _ui;
-      rclcpp::Subscription<rover_msgs::msg::CanDeviceStatus>::SharedPtr _sub_deviceStatus;
-      std::unordered_map<uint16_t, rover_msgs::msg::CanDeviceStatus> _deviceStatusInfo;
-   };
-
-//#endif // __QDEVICESTATUS_HPP__
+#endif  // __QDEVICESTATUS_HPP__
