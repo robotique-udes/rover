7487f25a636c15b0f7f7bf67ef46465ae2680891
Pmichaud29 <93682089+Pmichaud29@users.noreply.github.com>
2025-05-07 20:21:16 -0400

    Migrated to new arch (#130)
    Co-authored-by: Araa2876 <anibal.arango@usherbrooke.ca>

--- rover_camera/launch/camera.launch.py
+++ rover_camera/launch/camera.launch.py
@@ -6,15 +6,15 @@ def generate_launch_description():
     ld: LaunchDescription = LaunchDescription()
 
     node_camera_node = Node(
-        package= "rover_video",
-        namespace = "/rover/video",
+        package= "rover_camera",
+        namespace = "/rover/camera",
         executable = "media_server",
         name = "media_server",
     )
 
     node_aruco = Node(
-        package="rover_video",
-        namespace="/rover/video",
+        package="rover_camera",
+        namespace="/rover/camera",
         executable="aruco_detection",
         name="aruco_detection"
     )
