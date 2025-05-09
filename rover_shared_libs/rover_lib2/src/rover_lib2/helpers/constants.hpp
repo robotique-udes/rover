d2f379dd36b5ba2828f1679f6a26882aec0f72fe
Charles-robot <chucklafond2005@gmail.com>
2025-05-09 17:11:50 -0400

    Revert "Created the const"
    This reverts commit d730d316e065ed844c4a0478377d086c88050fc6.

--- rover_shared_libs/rover_lib2/src/rover_lib2/helpers/constants.hpp
+++ rover_shared_libs/rover_lib2/src/rover_lib2/helpers/constants.hpp
@@ -4,36 +4,29 @@
 #if defined(__linux__)
 #include <map>
 #include <string>
-#include <rclcpp/qos.hpp>
 #endif  // defined(__linux__)
 
-namespace Constants
+namespace Constants::CameraInfo
 {
 #if defined(__linux__)
+    const std::map<std::string, std::string> CAMERA_URL_MAP = {
+        {"Main", "rtsp://192.168.144.30:554/1/h264major"},
+        {"Antenna", "rtsp://192.168.144.31:554/1/h264major"},
+        {"Front-Side", "rtsp://192.168.144.32:554/1/h264major"},
+        {"Arm-Top", "rtsp://192.168.144.35:554/1/h264major"},
+        {"Arm-Side", "rtsp://192.168.144.36:554/1/h264major"},
+    };
 
-    namespace CameraInfo
-    {
-        const std::map<std::string, std::string> CAMERA_URL_MAP = {
-            {"Main", "rtsp://192.168.144.30:554/1/h264major"},
-            {"Antenna", "rtsp://192.168.144.31:554/1/h264major"},
-            {"Front-Side", "rtsp://192.168.144.32:554/1/h264major"},
-            {"Arm-Top", "rtsp://192.168.144.35:554/1/h264major"},
-            {"Arm-Side", "rtsp://192.168.144.36:554/1/h264major"},
-        };
+    /**
+     * @brief Tries to find a name from a camera URL
+     *
+     * @param url_ URL of the camera
+     * @param rName_ Overwrite value if found
+     * @return Success on camera name found
+     */
 
-        /**
-         * @brief Tries to find a name from a camera URL
-         *
-         * @param url_ URL of the camera
-         * @param rName_ Overwrite value if found
-         * @return Success on camera name found
-         */
-
-        bool getNameFromURL(const std::string& url_, std::string& rName_);
-    } // namespace Constants::CameraInfo
-
-    inline const rclcpp::QoS QOS_DEFAULT = rclcpp::QoS(rclcpp::KeepLast(1));
-
+    bool getNameFromURL(const std::string& url_, std::string& rName_);
 #endif  // defined(__linux__)
-}  // namespace Constants
+}  // namespace Constants::CameraInfo
+
 #endif  // CONSTANTS_HPP
