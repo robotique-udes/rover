1927e2d81ef477af426f562bc578983b2d17c5ba
pmichaud29 <micp1402@usherbrooke.ca>
2025-05-09 20:23:06 -0400

    deps

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -16,6 +16,9 @@ sudo apt -y install ros-humble-joy
 sudo apt -y install can-utils
 sudo apt -y install qt6-base-dev
 sudo apt -y install qt6-tools-dev
+sudo apt -y install qt6-webengine-dev
+sudo apt -y install qt6-webengine-dev-tools
+sudo apt -y install libqt6webenginecore6-bin
 sudo apt -y install python3-gi
 sudo apt -y install libssh-dev
 sudo apt -y install sl
