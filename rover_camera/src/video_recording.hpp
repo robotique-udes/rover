3faad400ee02fd58cbc44132a7cad7c50d7cec90
Charles-robot <chucklafond2005@gmail.com>
2025-05-18 23:46:54 -0400

    Added sScreenshotResult

--- rover_camera/src/video_recording.hpp
+++ rover_camera/src/video_recording.hpp
@@ -16,6 +16,12 @@
 #include <unordered_set>
 #include <vector>
 
+struct sScreenshotResult
+{
+  bool success;
+  std::string msg;
+};
+
 /**
  * @brief Each Recording objects handle 1 camera stream \n
  * @brief They must be initialized with startRecording()
