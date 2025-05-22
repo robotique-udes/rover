a477987609d7bafc434debf1857b73994337781c
Ndumoulin <nathan.dumoulin11@outlook.com>
2025-05-21 21:32:46 -0400

    new base, new ui

--- rover_gui/src/QDashboard/QArbitration.hpp
+++ rover_gui/src/QDashboard/QArbitration.hpp
@@ -3,10 +3,8 @@
 #include "UI_Arbitration.h"
 // ROS
 #include "rclcpp/rclcpp.hpp"
-#include "rover_msgs/srv/joy_demux_set_state.hpp"
 #include "rover_msgs/msg/joy_demux_status.hpp"
-#include "rover_msgs/srv/light_control.hpp"
-#include "rovus_lib/macros.h"
+
 // QT
 #include <QButtonGroup>
 #include <QtWidgets/QGridLayout>
@@ -15,11 +13,11 @@
 
 #include <QTimer>
 
-
 class QArbitration : public QWidget
 {
     Q_OBJECT
 
+    /*
     enum eControllerType
     {
         main = (int8_t)rover_msgs::srv::JoyDemuxSetState_Request::CONTROLLER_MAIN,
@@ -33,100 +31,18 @@ class QArbitration : public QWidget
         antenna = (int8_t)rover_msgs::srv::JoyDemuxSetState_Request::DEST_ANTENNA,
         none = (int8_t)rover_msgs::srv::JoyDemuxSetState_Request::DEST_NONE
     };
+    */
 
   public:
-    QArbitration(std::shared_ptr<rclcpp::Node> guiNode_, QWidget* parent_): QWidget(parent_), _guiNode(guiNode_)
-    {
-        _ui.setupUi(this);
-
-        _sub_joyDemux = _guiNode->create_subscription<rover_msgs::msg::JoyDemuxStatus>(
-				"/joy/demux/status",
-				1,
-				std::bind(&QArbitration::joyDemuxCallback, this, std::placeholders::_1));
-
-        QButtonGroup* mainButtonGroup = new QButtonGroup(this);
-        mainButtonGroup->addButton(_ui.checkBox);
-        mainButtonGroup->addButton(_ui.checkBox_3);
-        mainButtonGroup->addButton(_ui.checkBox_4);
-        mainButtonGroup->addButton(_ui.checkBox_2);
-        mainButtonGroup->setExclusive(true);
-
-        QButtonGroup* secondaryButtonGroup = new QButtonGroup(this);
-        secondaryButtonGroup->addButton(_ui.checkBox_5);
-        secondaryButtonGroup->addButton(_ui.checkBox_7);
-        secondaryButtonGroup->addButton(_ui.checkBox_6);
-        secondaryButtonGroup->addButton(_ui.checkBox_8);
-        secondaryButtonGroup->setExclusive(true);
-
-        //MAIN
-        connect(_ui.checkBox, &QCheckBox::stateChanged, this, [this](int8_t state){
-          if (state == Qt::Checked){
-            sendRequest(main, drive_train);
-          }
-        });
-        connect(_ui.checkBox_3, &QCheckBox::stateChanged, this, [this](int8_t state){
-          if (state == Qt::Checked){
-            sendRequest(main, arm);
-          }
-        });
-        connect(_ui.checkBox_4, &QCheckBox::stateChanged, this, [this](int8_t state){
-          if (state == Qt::Checked){
-            sendRequest(main, antenna);
-          }
-        });
-        connect(_ui.checkBox_2, &QCheckBox::stateChanged, this, [this](int8_t state){
-          if (state == Qt::Checked){
-            sendRequest(main, none);
-          }
-        });
-
-        //SECONDARY
-          connect(_ui.checkBox_5, &QCheckBox::stateChanged, this, [this](int8_t state){
-          if (state == Qt::Checked){
-            sendRequest(secondary, drive_train);
-          }
-        });
-        connect(_ui.checkBox_7, &QCheckBox::stateChanged, this, [this](int8_t state){
-          if (state == Qt::Checked){
-            sendRequest(secondary, arm);
-          }
-        });
-        connect(_ui.checkBox_6, &QCheckBox::stateChanged, this, [this](int8_t state){
-          if (state == Qt::Checked){
-            sendRequest(secondary, antenna);
-          }
-        });
-        connect(_ui.checkBox_8, &QCheckBox::stateChanged, this, [this](int8_t state){
-          if (state == Qt::Checked){
-            sendRequest(secondary, none);
-          }
-        });
-
-        _clientJoy = _guiNode->create_client<rover_msgs::srv::JoyDemuxSetState>("/demux_control");
-        _clientLights = _guiNode->create_client<rover_msgs::srv::LightControl>("/rover/auxiliary/set/lights");
-        
-        _ui.checkBox_2->setChecked(true);  
-        _ui.checkBox_8->setChecked(true);  
-
-        
-    }
+    QArbitration(std::shared_ptr<rclcpp::Node> guiNode_, QWidget* parent_);
 
     ~QArbitration() {};
 
   private:
-  	void joyDemuxCallback(const rover_msgs::msg::JoyDemuxStatus::SharedPtr rosMsg_);
-    void setControlButtonsEnabled(bool enabled);
+    void initComboBoxItems();
 
-		
-		rclcpp::Subscription<rover_msgs::msg::JoyDemuxStatus>::SharedPtr _sub_joyDemux;
-    Ui::Form _ui;
-    std::shared_ptr<rclcpp::Node> _guiNode;
-    rclcpp::Client<rover_msgs::srv::JoyDemuxSetState>::SharedPtr _clientJoy;
-    rclcpp::Client<rover_msgs::srv::LightControl>::SharedPtr _clientLights;
-    QTimer* _reconnectTimer = nullptr;
-
-
-  private slots:
-    void sendRequest(eControllerType controllerType_, eDemuxDestination demuxDestination_);
+  std::shared_ptr<rclcpp::Node> _node;
+  Ui::Arbitration _ui;
+  rclcpp::Subscription<rover_msgs::msg::JoyDemuxStatus>::SharedPtr _demuxStatusSub;
 };
 #endif
--- rover_gui/src/QDashboard/QDashboard.hpp
+++ rover_gui/src/QDashboard/QDashboard.hpp
@@ -18,23 +18,18 @@ class QDashboard : public QWidget
         QWidget(parent_),
         _node(guiNode_),
         _dashboardLayout(this),
-        _exampleWidget(guiNode_, this),
         _arbitrationWidget(guiNode_, this)
     {
         this->setLayout(&_dashboardLayout);
 
         // Add your dashboard widget here
-        _dashboardLayout.addWidget(&_exampleWidget);
         _dashboardLayout.addWidget(&_arbitrationWidget);
-
-
     }
 
   private:
     std::shared_ptr<rclcpp::Node> _node;
 
     QGridLayout _dashboardLayout;
-    QExample _exampleWidget;
     QArbitration _arbitrationWidget;
 };
 
