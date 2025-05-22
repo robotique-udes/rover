939ee904893aa698bb371512e18348cf7baf7b74
Ndumoulin <nathan.dumoulin11@outlook.com>
2025-05-21 22:42:28 -0400

    sending requests

--- rover_gui/src/QDashboard/QArbitration.cpp
+++ rover_gui/src/QDashboard/QArbitration.cpp
@@ -1,4 +1,5 @@
 #include "QArbitration.hpp"
+#include "rover_lib2/helpers/log.hpp"
 #include <rover_msgs/msg/detail/joy_demux_status__struct.hpp>
 
 QArbitration::QArbitration(std::shared_ptr<rclcpp::Node> guiNode_, QWidget* parent_):
@@ -8,17 +9,41 @@ QArbitration::QArbitration(std::shared_ptr<rclcpp::Node> guiNode_, QWidget* pare
     _ui.setupUi(this);
 
     this->initComboBoxItems();
+
+    this->_clientJoy = _node->create_client<rover_msgs::srv::JoyDemuxSetState>("/base/joy/demux_control");
+
+    connect(_ui.mainComboBox, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &QArbitration::onMainComboChanged);
 }
 
 void QArbitration::initComboBoxItems()
 {
-    this->_ui.mainComboBox->addItem("None", 0);
-    this->_ui.mainComboBox->addItem("Drive Train", 1);
-    this->_ui.mainComboBox->addItem("Arm", 2);
-    this->_ui.mainComboBox->addItem("Antenna", 3);
+    this->_ui.mainComboBox->addItem("Drive Train", 0);
+    this->_ui.mainComboBox->addItem("Arm", 1);
+    this->_ui.mainComboBox->addItem("Antenna", 2);
+    this->_ui.mainComboBox->addItem("None", 3);
 
-    this->_ui.secComboBox->addItem("None", 0);
-    this->_ui.secComboBox->addItem("Drive Train", 1);
-    this->_ui.secComboBox->addItem("Arm", 2);
-    this->_ui.secComboBox->addItem("Antenna", 3);
+    this->_ui.secComboBox->addItem("Drive Train", 0);
+    this->_ui.secComboBox->addItem("Arm", 1);
+    this->_ui.secComboBox->addItem("Antenna", 2);
+    this->_ui.secComboBox->addItem("None", 3);
+}
+
+void QArbitration::onMainComboChanged(int index)
+{
+    auto request = std::make_shared<rover_msgs::srv::JoyDemuxSetState::Request>();
+    request->controller_type = main;
+    request->destination = index;
+    request->force = true;
+
+    auto result = _clientJoy->async_send_request(request);
+}
+
+void QArbitration::onSecComboChanged(int index)
+{
+    auto request = std::make_shared<rover_msgs::srv::JoyDemuxSetState::Request>();
+    request->controller_type = secondary;
+    request->destination = index;
+    request->force = true;
+
+    auto result = _clientJoy->async_send_request(request);
 }
--- rover_gui/src/QDashboard/QArbitration.hpp
+++ rover_gui/src/QDashboard/QArbitration.hpp
@@ -1,23 +1,26 @@
 #ifndef __QARBITRATION_HPP__
 #define __QARBITRATION_HPP__
+
 #include "UI_Arbitration.h"
+#include "rover_lib2/helpers/log.hpp"
+
 // ROS
 #include "rclcpp/rclcpp.hpp"
 #include "rover_msgs/msg/joy_demux_status.hpp"
+#include "rover_msgs/srv/joy_demux_set_state.hpp"
 
 // QT
-#include <QButtonGroup>
+#include <QComboBox>
 #include <QtWidgets/QGridLayout>
 #include <QtWidgets/QWidget>
-#include <QMessageBox>
 
-#include <QTimer>
+DEFINE_LOG_NODE(QArbitration, Logger::eNodeState::OFF);
 
 class QArbitration : public QWidget
 {
     Q_OBJECT
 
-    /*
+    
     enum eControllerType
     {
         main = (int8_t)rover_msgs::srv::JoyDemuxSetState_Request::CONTROLLER_MAIN,
@@ -31,18 +34,24 @@ class QArbitration : public QWidget
         antenna = (int8_t)rover_msgs::srv::JoyDemuxSetState_Request::DEST_ANTENNA,
         none = (int8_t)rover_msgs::srv::JoyDemuxSetState_Request::DEST_NONE
     };
-    */
+    
 
   public:
     QArbitration(std::shared_ptr<rclcpp::Node> guiNode_, QWidget* parent_);
 
     ~QArbitration() {};
 
+  private slots:
+    void onMainComboChanged(int index);
+    void onSecComboChanged(int index);
+
   private:
     void initComboBoxItems();
 
-  std::shared_ptr<rclcpp::Node> _node;
-  Ui::Arbitration _ui;
-  rclcpp::Subscription<rover_msgs::msg::JoyDemuxStatus>::SharedPtr _demuxStatusSub;
+    std::shared_ptr<rclcpp::Node> _node;
+    Ui::Arbitration _ui;
+
+    rclcpp::Subscription<rover_msgs::msg::JoyDemuxStatus>::SharedPtr _demuxStatusSub;
+    rclcpp::Client<rover_msgs::srv::JoyDemuxSetState>::SharedPtr _clientJoy;
 };
 #endif
