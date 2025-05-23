fc9d06f9d0f616904370e72e689365f0eaef5823
Araa2876 <anibal.arango@usherbrooke.ca>
2025-05-23 16:07:55 -0400

    Working service call

--- rover_gui/src/QDeviceStatus/Worker/QStatusWorker.cpp
+++ rover_gui/src/QDeviceStatus/Worker/QStatusWorker.cpp
@@ -19,8 +19,12 @@ void QStatusWorker::requestDeviceStatusManager(std::shared_ptr<rclcpp::Client<ro
         });
 }
 
-void QStatusWorker::requestDeviceStatusInternal(std::shared_ptr<rclcpp::Client<rover_msgs::srv::Empty>> client_requestErrorStatus_)
+void QStatusWorker::requestDeviceStatusInternal(
+    std::shared_ptr<rclcpp::Client<rover_msgs::srv::Empty>> client_requestErrorStatus_)
 {
+    bool success = false;
+    std::string status;
+
     std::shared_ptr<rover_msgs::srv::Empty::Request> request = std::make_shared<rover_msgs::srv::Empty::Request>();
 
     if (!client_requestErrorStatus_)
@@ -29,14 +33,20 @@ void QStatusWorker::requestDeviceStatusInternal(std::shared_ptr<rclcpp::Client<r
         return;
     }
 
-    auto result_future = client_requestErrorStatus_->async_send_request(
-        request,
-        [this](rclcpp::Client<rover_msgs::srv::Empty>::SharedFuture future)
+    rclcpp::Client<rover_msgs::srv::Empty>::FutureAndRequestId future_and_request
+        = client_requestErrorStatus_->async_send_request(request);
+
+    std::future<std::shared_ptr<rover_msgs::srv::Empty::Response>> future_result = std::move(future_and_request.future);
+    if (future_result.valid())
+    {
+        std::shared_ptr<rover_msgs::srv::Empty::Response> response = future_result.get();
+
+        if (response != nullptr)
         {
-            auto response = future.get();
-            if (response->success)
-            {
-                emit this->onRequestDeviceStatusSuccessful(response->success, response->message);
-            }
-        });
+            success = response->success;
+            status = response->message;
+        }
+
+        emit this->onRequestDeviceStatusSuccessful(success, status);
+    }
 }
