fad0a7ca21105935f4dda594b9ec0ddd8946eed7
nikopapi <papn1801@usherbrooke.ca>
2025-05-24 17:09:59 -0400

    fixes

--- rover_gui/src/QScreenInhibitor.hpp
+++ rover_gui/src/QScreenInhibitor.hpp
@@ -1,31 +1,33 @@
-#ifndef SCREENINHIBITOR_HPP
-#define SCREENINHIBITOR_HPP
+#ifndef SCREEN_INHIBITOR_HPP
+#define SCREEN_INHIBITOR_HPP
 
 #include <QDBusInterface>
 #include <QDBusReply>
-#include <QDebug>
+
+/**
+ * @brief Construct a ScreenInhibitor object to prevent screen dimming during teleop, using DBus interface
+ * @brief The Rover Base application becomes a priority over the screen saver
+ */
 
 class ScreenInhibitor
 {
   public:
-    ScreenInhibitor()
-    {
-        QDBusInterface interface("org.freedesktop.ScreenSaver",
+    ScreenInhibitor(): interface("org.freedesktop.ScreenSaver",
                                  "/ScreenSaver",
                                  "org.freedesktop.ScreenSaver",
-                                 QDBusConnection::sessionBus());
-
+                                 QDBusConnection::sessionBus())
+    {
         if (!interface.isValid())
         {
             RCLCPP_WARN(rclcpp::get_logger("GUI"), "Failed to connect to ScreenSaver interface");
             return;
         }
 
-        QDBusReply<uint> reply = interface.call("Inhibit", "Rover GUI", "Running critical task");
+        QDBusReply<uint> reply = interface.call("Inhibit", "Rover GUI", "Screen dimming during rover teleop can cause issues");
         if (reply.isValid())
         {
             cookie = reply.value();
-            qDebug() << "Screen inhibition activated, cookie:" << cookie;
+            RCLCPP_INFO(rclcpp::get_logger("GUI"), "Inhibition cookie activated: %u", cookie);
             inhibited = true;
         }
         else
@@ -38,14 +40,10 @@ class ScreenInhibitor
     {
         if (inhibited)
         {
-            QDBusInterface interface("org.freedesktop.ScreenSaver",
-                                     "/ScreenSaver",
-                                     "org.freedesktop.ScreenSaver",
-                                     QDBusConnection::sessionBus());
             if (interface.isValid())
             {
                 interface.call("UnInhibit", cookie);
-                qDebug() << "Screen inhibition released";
+                RCLCPP_INFO(rclcpp::get_logger("GUI"), "Inhibition cookie released: %u", cookie);
             }
         }
     }
@@ -53,6 +51,7 @@ class ScreenInhibitor
   private:
     uint cookie = 0;
     bool inhibited = false;
+    QDBusInterface interface;
 };
 
-#endif  // SCREENINHIBITOR_HPP
+#endif  // SCREEN_INHIBITOR_HPP
