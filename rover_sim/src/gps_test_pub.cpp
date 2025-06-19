daa5bf17a0e69231b1b0a883873b291b1c352021
emiles <emilesavoie39@gmail.com>
2025-06-18 23:22:39 -0400

    lint

--- rover_sim/src/gps_test_pub.cpp
+++ rover_sim/src/gps_test_pub.cpp
@@ -3,56 +3,54 @@
 
 class GpsPublisher : public rclcpp::Node
 {
-public:
-  GpsPublisher()
-  : Node("gps_publisher"),
-    latitude_(45.404476),
-    longitude_(-71.888351),
-    satellite_(8),
-    fix_quality_(3),
-    heading_deg_(0.0)
-  {
-    publisher_ = this->create_publisher<rover_msgs::msg::Gps>("/rover/gps/position", 10);
-    timer_ = this->create_wall_timer(
-      std::chrono::seconds(1),
-      std::bind(&GpsPublisher::publish_gps, this));
-  }
-
-private:
-  void publish_gps()
-  {
-    auto msg = rover_msgs::msg::Gps();
-    msg.latitude    = latitude_;
-    msg.longitude   = longitude_;
-    msg.satellite   = satellite_;
-    msg.fix_quality = fix_quality_;
-
-    msg.heading = heading_deg_;
-
-    publisher_->publish(msg);
-
-    heading_deg_ += 20.0F;
-    if (heading_deg_ >= 360.0) {
-      heading_deg_ -= 360.0;
+  public:
+    GpsPublisher():
+        Node("gps_publisher"),
+        latitude_(45.404476),
+        longitude_(-71.888351),
+        satellite_(8),
+        fix_quality_(3),
+        heading_deg_(0.0)
+    {
+        publisher_ = this->create_publisher<rover_msgs::msg::Gps>("/rover/gps/position", 10);
+        timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&GpsPublisher::publish_gps, this));
     }
-  }
 
-  rclcpp::Publisher<rover_msgs::msg::Gps>::SharedPtr publisher_;
-  rclcpp::TimerBase::SharedPtr timer_;
+  private:
+    void publish_gps()
+    {
+        auto msg = rover_msgs::msg::Gps();
+        msg.latitude = latitude_;
+        msg.longitude = longitude_;
+        msg.satellite = satellite_;
+        msg.fix_quality = fix_quality_;
 
-  // fixed GPS position and status
-  double latitude_;
-  double longitude_;
-  int    satellite_;
-  int    fix_quality_;
+        msg.heading = heading_deg_;
 
-  double heading_deg_;
+        publisher_->publish(msg);
+
+        heading_deg_ += 20.0F;
+        if (heading_deg_ >= 360.0)
+        {
+            heading_deg_ -= 360.0;
+        }
+    }
+
+    rclcpp::Publisher<rover_msgs::msg::Gps>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    double latitude_;
+    double longitude_;
+    int satellite_;
+    int fix_quality_;
+
+    double heading_deg_;
 };
 
 int main(int argc, char* argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<GpsPublisher>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<GpsPublisher>());
+    rclcpp::shutdown();
+    return 0;
 }
