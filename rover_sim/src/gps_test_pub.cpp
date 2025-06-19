ba08ff8ebd60b226e44ffd9959933423dc555399
emilesavoie <104540019+emilesavoie@users.noreply.github.com>
2025-06-19 00:14:37 -0400

    Working heading in UI (#179)

--- rover_sim/src/gps_test_pub.cpp
+++ rover_sim/src/gps_test_pub.cpp
@@ -13,7 +13,7 @@ class GpsPublisher : public rclcpp::Node
         heading_deg_(0.0)
     {
         publisher_ = this->create_publisher<rover_msgs::msg::Gps>("/rover/gps/position", 10);
-        timer_ = this->create_wall_timer(std::chrono::milliseconds(200), std::bind(&GpsPublisher::publish_gps, this));
+        timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&GpsPublisher::publish_gps, this));
     }
 
   private:
@@ -29,11 +29,11 @@ class GpsPublisher : public rclcpp::Node
 
         publisher_->publish(msg);
 
-        // heading_deg_ += 1.0F;
-        // if (heading_deg_ >= 360.0)
-        // {
-        //     heading_deg_ -= 360.0;
-        // }
+        heading_deg_ += 20.0F;
+        if (heading_deg_ >= 360.0)
+        {
+            heading_deg_ -= 360.0;
+        }
     }
 
     rclcpp::Publisher<rover_msgs::msg::Gps>::SharedPtr publisher_;
