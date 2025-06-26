20122766d7539e419a4752b45d948ff0bed63c34
Araa2876 <anibal.arango@usherbrooke.ca>
2025-06-25 20:12:02 -0400

    Implementing flow layout from example online

---
+++ rover_gui/src/Global/QFlowLayout.hpp
@@ -0,0 +1,36 @@
+#ifndef GLOBAL_QFLOWLAYOUT_HPP
+#define GLOBAL_QFLOWLAYOUT_HPP
+
+#include <QtWidgets/QLayout>
+#include <QtWidgets/QStyle>
+
+class QFlowLayout : public QLayout
+{
+  public:
+    explicit QFlowLayout(QWidget* parent, int margin = -1, int hSpacing = -1, int vSpacing = -1);
+    explicit QFlowLayout(int margin = -1, int hSpacing = -1, int vSpacing = -1);
+    ~QFlowLayout();
+
+    void addItem(QLayoutItem* item) override;
+    int horizontalSpacing() const;
+    int verticalSpacing() const;
+    Qt::Orientations expandingDirections() const override;
+    bool hasHeightForWidth() const override;
+    int heightForWidth(int) const override;
+    int count() const override;
+    QLayoutItem* itemAt(int index) const override;
+    QSize minimumSize() const override;
+    void setGeometry(const QRect& rect) override;
+    QSize sizeHint() const override;
+    QLayoutItem* takeAt(int index) override;
+
+  private:
+    int doLayout(const QRect& rect, bool testOnly) const;
+    int smartSpacing(QStyle::PixelMetric pm) const;
+
+    QList<QLayoutItem*> itemList;
+    int m_hSpace;
+    int m_vSpace;
+};
+
+#endif  // GLOBAL_QFLOWLAYOUT_HPP
