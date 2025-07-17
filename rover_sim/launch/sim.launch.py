4917193c403a103275bf184e07005af6fd04f452
Charles-robot <chucklafond2005@gmail.com>
2025-07-17 00:19:55 -0400

    Small changes
    Look to add a .msg to can2
    Virutal can class

--- rover_sim/launch/sim.launch.py
+++ rover_sim/launch/sim.launch.py
@@ -67,6 +67,13 @@ def generate_launch_description():
         name="connection_speed_test"
     )
 
+    can_sim_node = Node(
+        package="rover_sim",
+        namespace="/rover/sim",
+        executable="can_test",
+        name="can_test"
+    )
+
     ld = LaunchDescription()
     ld.add_action(arm_arg)
     ld.add_action(gps_arg)
@@ -74,5 +81,6 @@ def generate_launch_description():
     ld.add_action(gps_sim_node)
     ld.add_action(battery_sim_node)
     ld.add_action(connection_speed_sim_node)
+    ld.add_action(can_sim_node)
 
     return ld
