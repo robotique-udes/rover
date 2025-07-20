b7295e72fad86826885462986287628127d60ad6
Charles-robot <chucklafond2005@gmail.com>
2025-07-19 22:09:33 -0400

    Lint

--- rover_auxiliary/src/Antenna/antenna_driver.cpp
+++ rover_auxiliary/src/Antenna/antenna_driver.cpp
@@ -38,13 +38,14 @@ sCommandResult AntennaDriver::CbAntennaPublisher(sAntennaMsg& msg_)
         result = cmd->execute(_session, msg_);
         if (!result)
         {
-            if (result.httpStatus == std::to_underlying(eHttpStatus::FORBIDDEN) || result.httpStatus == std::to_underlying(eHttpStatus::UNAUTHORIZED))
+            if (result.httpStatus == std::to_underlying(eHttpStatus::FORBIDDEN)
+                || result.httpStatus == std::to_underlying(eHttpStatus::UNAUTHORIZED))
             {
                 result = this->handleDisconnect(msg_);
             }
-            else if(result.httpStatus == std::to_underlying(eHttpStatus::OFFLINE))
+            else if (result.httpStatus == std::to_underlying(eHttpStatus::OFFLINE))
             {
-                //Clear result so no logging when offline
+                // Clear result so no logging when offline
                 result = sCommandResult{};
             }
             else
