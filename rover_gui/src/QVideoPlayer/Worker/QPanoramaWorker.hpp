657bb6ed330243cd62691b86ae550ee2bce55c4e
Charles-robot <chucklafond2005@gmail.com>
2025-08-05 20:14:53 -0400

    Lint

--- rover_gui/src/QVideoPlayer/Worker/QPanoramaWorker.hpp
+++ rover_gui/src/QVideoPlayer/Worker/QPanoramaWorker.hpp
@@ -13,7 +13,8 @@ class QPanoramaWorker : public QWorker
 
   private:
     static constexpr const char* CAMERA_PATH = "/camera";
-    static constexpr uint16_t SERVICE_TIMEOUT_MS = 4'000U; // Additional time to account for initializing camera position, stitching the frames and correction warp
+    static constexpr uint16_t SERVICE_TIMEOUT_MS
+        = 4'000U;  // Additional time to account for initializing camera position, stitching the frames and correction warp
 
   public:
     QPanoramaWorker(bool start_ = false, QObject* parent_ = nullptr);
