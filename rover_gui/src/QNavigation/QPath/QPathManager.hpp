2a20ab3aa97cf5f841205676706cb1c4338a3c03
Araa2876 <anibal.arango@usherbrooke.ca>
2025-08-08 20:58:48 -0400

    template for class

---
+++ rover_gui/src/QNavigation/QPath/QPathManager.hpp
@@ -0,0 +1,13 @@
+#ifndef QNAVIGATION_QPATH_QPATHMANAGER
+#define QNAVIGATION_QPATH_QPATHMANAGER
+
+class QPathManager
+{
+  public:
+    QPathManager(){};
+
+  private:
+    
+};
+
+#endif  // QNAVIGATION_QPATH_QPATHMANAGER
