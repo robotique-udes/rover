c6b0e058b849b55156172d8e68de877ca9b352f2
emile <emilesavoie39@gmail.com>
2026-02-04 18:09:31 -0500

    bridge lib from micro

--- rover_shared_libs/rover_lib2/src/rover_lib2/actuators/AK60_6/AK60_6.hpp
+++ rover_shared_libs/rover_lib2/src/rover_lib2/actuators/AK60_6/AK60_6.hpp
@@ -1,10 +1,10 @@
-#ifndef ROVER_LIB2_ACTUATORS_AK10_9_AK10_9_HPP
-#define ROVER_LIB2_ACTUATORS_AK10_9_AK10_9_HPP
+#ifndef ROVER_LIB2_ACTUATORS_AK60_6_AK60_6_HPP
+#define ROVER_LIB2_ACTUATORS_AK60_6_AK60_6_HPP
 
 #include <concepts>
 #include <Stream.h>
 
-#include "AK10_9_internals.hpp"
+#include "AK60_6_internals.hpp"
 
 #include "rover_lib2/helpers/log.hpp"
 #include "rover_lib2/helpers/log_plot.hpp"
@@ -19,12 +19,12 @@
 #include "rover_lib2/controllers/PID.hpp"
 #include "rover_lib2/rover_object.hpp"
 
-DEFINE_LOG_NODE(AK109, Logger::eNodeState::OFF);
-DEFINE_LOG_NODE(AK109Plot, Logger::eNodeState::OFF);
+DEFINE_LOG_NODE(AK106, Logger::eNodeState::OFF);
+DEFINE_LOG_NODE(AK106Plot, Logger::eNodeState::OFF);
 
 namespace Actuators
 {
-    class AK10_9
+    class AK60_6
     {
         static constexpr float RAD_S_TO_RPM = 9.549296596425384F;
         static constexpr float N_POLE_PAIRS = 17.0F;
@@ -56,7 +56,7 @@ namespace Actuators
         };
 
       public:
-        AK10_9(eControlType controlType_, std::reference_wrapper<Stream> motorSerial_):
+        AK60_6(eControlType controlType_, std::reference_wrapper<Stream> motorSerial_):
             _controlType(controlType_),
             _motorSerial(motorSerial_)
         {
@@ -68,7 +68,7 @@ namespace Actuators
         {
             if (_errorMode)
             {
-                LOG_WARN(Logger::Nodes::AK109, "Fallen in error mode, motor stopped");
+                LOG_WARN(Logger::Nodes::AK106, "Fallen in error mode, motor stopped");
                 this->sendRadSCmd(FULL_STOP_CMD);
                 return;
             }
@@ -101,8 +101,8 @@ namespace Actuators
                 cmd = std::clamp(cmd, 0.0F, _maxSpeed);
             }
 
-            LOG_DEBUG(Logger::Nodes::AK109, "_goalSpeed: %f | cmd: %f", _goalSpeed, cmd);
-            LOG_PLOT(Logger::Nodes::AK109Plot, _goalSpeed, cmd, this->getSpeed(), this->getPosition());
+            LOG_DEBUG(Logger::Nodes::AK106, "_goalSpeed: %f | cmd: %f", _goalSpeed, cmd);
+            LOG_PLOT(Logger::Nodes::AK106Plot, _goalSpeed, cmd, this->getSpeed(), this->getPosition());
 
             this->sendRadSCmd(cmd);
         }
@@ -160,15 +160,15 @@ namespace Actuators
             float rpm = radSToRPM(rad_s_);
             float elecRpm_f = radToERPM(rpm);
 
-            constexpr float MAX_ERPM = Actuators::AK10_9_Constants::RATED_SPEED_ERPM;
+            constexpr float MAX_ERPM = Actuators::AK60_6_Constants::RATED_SPEED_ERPM;
             elecRpm_f = std::clamp(elecRpm_f, -MAX_ERPM, MAX_ERPM);
 
             int32_t eRpm = static_cast<int32_t>(ROUND(elecRpm_f));
 
-            _sendCmdBuffer[TO_UNDERLYING(eSendCmd::FRAME_HEAD)] = Actuators::AK10_9_Constants::FRAME_HEAD;
+            _sendCmdBuffer[TO_UNDERLYING(eSendCmd::FRAME_HEAD)] = Actuators::AK60_6_Constants::FRAME_HEAD;
             _sendCmdBuffer[TO_UNDERLYING(eSendCmd::DATA_LENGTH)]
-                = sizeof(eRpm) + sizeof(Actuators::AK10_9_Constants::COMMAND_SET_RPM);
-            _sendCmdBuffer[TO_UNDERLYING(eSendCmd::COMMAND_SET_RPM)] = Actuators::AK10_9_Constants::COMMAND_SET_RPM;
+                = sizeof(eRpm) + sizeof(Actuators::AK60_6_Constants::COMMAND_SET_RPM);
+            _sendCmdBuffer[TO_UNDERLYING(eSendCmd::COMMAND_SET_RPM)] = Actuators::AK60_6_Constants::COMMAND_SET_RPM;
             _sendCmdBuffer[TO_UNDERLYING(eSendCmd::RPM_MSB)] = (eRpm >> 24) & 0xFF;
             _sendCmdBuffer[TO_UNDERLYING(eSendCmd::RPM_BYTE2)] = (eRpm >> 16) & 0xFF;
             _sendCmdBuffer[TO_UNDERLYING(eSendCmd::RPM_BYTE1)] = (eRpm >> 8) & 0xFF;
@@ -178,7 +178,7 @@ namespace Actuators
 
             _sendCmdBuffer[TO_UNDERLYING(eSendCmd::CHECKSUM_MSB)] = (checksum >> 8) & 0xFF;
             _sendCmdBuffer[TO_UNDERLYING(eSendCmd::CHECKSUM_LSB)] = checksum & 0xFF;
-            _sendCmdBuffer[TO_UNDERLYING(eSendCmd::FRAME_TAIL)] = Actuators::AK10_9_Constants::FRAME_TAIL;
+            _sendCmdBuffer[TO_UNDERLYING(eSendCmd::FRAME_TAIL)] = Actuators::AK60_6_Constants::FRAME_TAIL;
 
             _motorSerial.get().write(_sendCmdBuffer.data(), sizeof(_sendCmdBuffer));
         }
@@ -188,7 +188,7 @@ namespace Actuators
             uint16_t cksum = 0;
             for (size_t i = 0; i < len_; i++)
             {
-                cksum = Actuators::AK10_9_Constants::CRC16_TAB[(((cksum >> 8) ^ *buf_++) & 0xFF)] ^ (cksum << 8);
+                cksum = Actuators::AK60_6_Constants::CRC16_TAB[(((cksum >> 8) ^ *buf_++) & 0xFF)] ^ (cksum << 8);
             }
             return cksum;
         }
@@ -217,8 +217,8 @@ namespace Actuators
         float _goalSpeed = 0.0F;
         float _maxSpeed = std::numeric_limits<float>::max();
 
-        VALIDATE_CONCEPT(Actuator, AK10_9);
+        VALIDATE_CONCEPT(Actuator, AK60_6);
     };
 }  // namespace Actuators
 
-#endif  // ROVER_LIB2_ACTUATORS_AK10_9_AK10_9_HPP
+#endif  // ROVER_LIB2_ACTUATORS_AK60_6_AK60_6_HPP
