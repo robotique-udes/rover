b12010ac2cc40e99a6f4c4c181980a22cd98f2b2
JoeyBilodeuL <bilj1577@usherbrooke.ca>
2026-02-17 20:16:14 -0500

    updates msg messages

--- rover_auxiliary/src/BMS/bms_node.hpp
+++ rover_auxiliary/src/BMS/bms_node.hpp
@@ -19,31 +19,27 @@ class BMSDataNode : public rclcpp::Node
     static constexpr const char* DATA_FILE_PATH = "rover_auxiliary/src/BMS/script/bms_data_log.txt";
     static constexpr uint64_t DELAY_PUBLISHER_MS = 1000UL;
     static constexpr uint16_t MAX_CELL = 6;
+    static constexpr uint16_t AMP_START_INDEX = 5;
+    static constexpr uint16_t CELL_START_INDEX = 17;
 
 
     public:
 
-        BMSDataNode(int argc, char** argv);
+        BMSDataNode(int argc_, char** argv_);
         void callbackBMSData(void);
         void getData(void);
-        void serialConfig(int fileDesc);
-        void serialWrite(int fileDesc, const std::string& cmd);
-        std::string serialRead(int fileDesc);
+        void serialConfig(int fileDesc_);
+        void serialWrite(int fileDesc_, const std::string& cmd_);
+        std::string serialRead(int fileDesc_);
 
     private:
 
-        uint16_t _batteryAmps;
+        double _batteryAmps;
         uint16_t _batterySOC;
         uint16_t _batteryVolt;
         std::vector<uint16_t> _cellVolt;
         rclcpp::Publisher<rover_msgs::msg::BmsData>::SharedPtr _publisher;
         rclcpp::TimerBase::SharedPtr _timer_publisher;
 };
-
-
-
-
-
-
 
 #endif
