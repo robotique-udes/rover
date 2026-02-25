0cb0f89da7e141a17fbaa2b0eeb61fa98581e943
JoeyBilodeuL <bilj1577@usherbrooke.ca>
2026-02-24 20:19:34 -0500

    deleted serialCom functions

--- rover_auxiliary/src/BMS/bms_node.cpp
+++ rover_auxiliary/src/BMS/bms_node.cpp
@@ -40,6 +40,7 @@ void BMSDataNode::getData(void)
     std::string cellsVoltSerialOutput;
     int fileDesc;
     uint16_t ampIndex = 0;
+    SerialCom terminal(eBaudRate::B_1152000, eCharSize::C_S8, false, true, false, false, false, 0, 10);
 
     _cellVolt.clear();
     _cellVolt.reserve(MAX_CELL);
@@ -51,13 +52,13 @@ void BMSDataNode::getData(void)
         std::cout << "Error encountered when opening the serial" << std::endl;
     }
 
-    serialConfig(fileDesc);
+    terminal.serialConfig(fileDesc);
     tcflush(fileDesc, TCIOFLUSH);
 
-    serialWrite(fileDesc, "?A\r");
-    ampSerialOutput = serialRead(fileDesc);
-    serialWrite(fileDesc, "?V\r");
-    cellsVoltSerialOutput = serialRead(fileDesc);
+    terminal.serialWrite(fileDesc, "?A\r");
+    ampSerialOutput = terminal.serialRead(fileDesc);
+    terminal.serialWrite(fileDesc, "?V\r");
+    cellsVoltSerialOutput = terminal.serialRead(fileDesc);
 
     ampSerialOutput = ampSerialOutput.substr(AMP_START_INDEX);
     cellsVoltSerialOutput = cellsVoltSerialOutput.substr(CELL_START_INDEX);
@@ -84,55 +85,4 @@ void BMSDataNode::getData(void)
     close(fileDesc);
 }
 
-void BMSDataNode::serialConfig(int fileDesc_)
-{
-    struct termios tty;
 
-    memset(&tty, 0, sizeof(tty));
-    if (tcgetattr(fileDesc_, &tty) != 0)
-    {
-        std::cout << "tcgetattr failed" << std::endl;
-    }
-
-    cfsetospeed(&tty, B115200);
-    cfsetispeed(&tty, B115200);
-
-    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;  // Sets 8 bits characters
-    tty.c_cflag |= CLOCAL | CREAD;               // Enable receiver and ignore modem control lines
-    tty.c_cflag &= ~(PARENB | PARODD);           // Disable parity
-    tty.c_cflag &= ~CSTOPB;                      // 1 stop bit
-    tty.c_cflag &= ~CRTSCTS;                     // Disables RTS/CTS hardware flow control
-
-    tty.c_lflag = 0;  // Disables all flags
-    tty.c_iflag = 0;  // Disables all flags
-    tty.c_oflag = 0;  // Disables all flags
-
-    tty.c_cc[VMIN] = 0;
-    tty.c_cc[VTIME] = 10;
-
-    if (tcsetattr(fileDesc_, TCSANOW, &tty) != 0)
-    {
-        std::cout << "tcsetattr failed" << std::endl;
-    }
-}
-
-void BMSDataNode::serialWrite(int fileDesc_, const std::string& cmd_)
-{
-    ssize_t bytesWritten = write(fileDesc_, cmd_.c_str(), cmd_.size());
-    (void)bytesWritten;
-}
-
-std::string BMSDataNode::serialRead(int fileDesc_)
-{
-    char buffer[256];
-    std::string response;
-
-    ssize_t n = read(fileDesc_, buffer, sizeof(buffer));
-
-    if (n > 0)
-    {
-        response.assign(buffer, n);
-    }
-
-    return response;
-}
