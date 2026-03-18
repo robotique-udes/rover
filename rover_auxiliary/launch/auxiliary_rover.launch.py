1a6c8e51d58b568e56767ae4fd95dfccd3888b41
emile <emilesavoie39@gmail.com>
2026-03-18 19:41:45 -0400

    Full working logic

--- rover_auxiliary/launch/auxiliary_rover.launch.py
+++ rover_auxiliary/launch/auxiliary_rover.launch.py
@@ -3,10 +3,27 @@ import yaml
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import ExecuteProcess
+
+web_dir = os.path.join(get_package_share_directory('rover_auxiliary'), 'web')
 
 def generate_launch_description():
     ld = LaunchDescription()
-    
+
+    rosbridge = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('rosbridge_server'),
+                'launch',
+                'rosbridge_websocket_launch.xml'
+            )
+        ])
+    )
+    ld.add_action(rosbridge)
+
     node_ddb_control = Node(
         package="rover_auxiliary",
         namespace="/rover/auxiliary",
@@ -14,7 +31,7 @@ def generate_launch_description():
         name="ddb_control"
     )
     ld.add_action(node_ddb_control)
-    
+
     node_camera_manager = Node(
         package="rover_auxiliary",
         namespace="/rover/camera_manager",
@@ -22,5 +39,19 @@ def generate_launch_description():
         name="camera_manager"
     )
     ld.add_action(node_camera_manager)
+
+    node_emergency_stop = Node(
+        package="rover_auxiliary",
+        namespace="/rover/remote_estop",
+        executable="remote_estop",
+        name="remote_estop"
+    )
+    ld.add_action(node_emergency_stop)
+
+    web_server = ExecuteProcess(
+    cmd=['python3', '-m', 'http.server', '5500', '--directory', web_dir],
+    output='screen'
+    )
+    ld.add_action(web_server)
 
     return ld
---
+++ rover_auxiliary/src/RemoteEstop/remote_estop_node.hpp
@@ -0,0 +1,26 @@
+#ifndef __ESTOP_NODE_HPP__
+#define __ESTOP_NODE_HPP__
+
+#include <rclcpp/rclcpp.hpp>
+#include <rover_msgs/srv/emergency_stop.hpp>
+#include <rover_msgs/srv/drive_train_arbitration.hpp>
+#include <rover_msgs/msg/drivetrain_arbitration.hpp>
+
+class RemoteStop : public rclcpp::Node
+{
+    static constexpr const char* SERVICE_ESTOP = "estop_srv";
+    static constexpr const char* SERVICE_ARBITRATION_CONTROL = "/rover/drive_train/demux_control";
+
+  private:
+    rclcpp::Service<rover_msgs::srv::EmergencyStop>::SharedPtr _srvRemoteEstop;
+    rclcpp::Client<rover_msgs::srv::DriveTrainArbitration>::SharedPtr _clientDriveTrainArb;
+
+  public:
+    RemoteStop();
+
+  private:
+    void CB_webServer(const rover_msgs::srv::EmergencyStop::Request& request_,
+                      rover_msgs::srv::EmergencyStop::Response& response_);
+};
+
+#endif
