7ea188d7c511e772a568fbf776e910387db00ce4
JoeyBilodeuL <bilj1577@usherbrooke.ca>
2026-03-17 20:45:35 -0400

    added parse function

--- rover_auxiliary/src/BMS/bms_node.hpp
+++ rover_auxiliary/src/BMS/bms_node.hpp
@@ -12,25 +12,22 @@
 #include <termios.h>
 #include "rover_lib2/communication/Serial/serial_com.hpp"
 
+DEFINE_LOG_NODE(BmsInfo, Logger::eNodeState::ON);
+
 class BMSDataNode : public rclcpp::Node
 {
     static constexpr const char* TOPIC_BMS_DATA = "rover/auxiliary/bms_data";
-    static constexpr const char* DATA_FILE_PATH = "rover_auxiliary/src/BMS/script/bms_data_log.txt";
     static constexpr uint64_t DELAY_PUBLISHER_MS = 1000UL;
     static constexpr uint16_t MAX_CELL = 6;
-    static constexpr uint16_t AMP_START_INDEX = 5;
-    static constexpr uint16_t CELL_START_INDEX = 17;
 
   public:
     BMSDataNode();
     void callbackBMSData(void);
     void getData(void);
-    void serialWrite(int fileDesc_, const std::string& cmd_);
-    std::string serialRead(int fileDesc_);
+    void parse(std::string rawOutput, uint16_t dataNumber, std::vector<uint16_t>& dataArray);
 
   private:
     double _batteryAmps;
-    uint16_t _batterySOC;
     uint16_t _batteryVolt;
     std::vector<uint16_t> _cellVolt;
     rclcpp::Publisher<rover_msgs::msg::BmsData>::SharedPtr _publisher;
