424d1ee42b2938c1c248781e75712ac1982be68c
JoeyBilodeuL <bilj1577@usherbrooke.ca>
2026-04-08 11:20:44 -0400

    started implementing BMS data GUI

--- rover_gui/src/QBmsData/QBmsData.cpp
+++ rover_gui/src/QBmsData/QBmsData.cpp
@@ -1,5 +1,11 @@
 #include "QBmsData.hpp"
 
+constexpr const char* STATUS_DEFAULT = "QWidget {"
+                                       "background-color: #3c3f41;"
+                                       "border-radius: 5px;"
+                                       "padding: 5px 10px;"
+                                       "}";
+
 QBmsData::QBmsData(std::shared_ptr<rclcpp::Node> guiNode_, QWidget* parent_):
     QWidget(parent_),
     _node(guiNode_)
@@ -14,6 +20,8 @@ QBmsData::QBmsData(std::shared_ptr<rclcpp::Node> guiNode_, QWidget* parent_):
 
     _ui.bmsData->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
     _ui.bmsData->adjustSize();
+
+    this->initializeWidget();
 
     _sub_bmsData = _node->create_subscription<rover_msgs::msg::BmsData>("rover/auxiliary/bms_data",
                                                                         QOS_DEFAULT,
@@ -31,8 +39,178 @@ QBmsData::QBmsData(std::shared_ptr<rclcpp::Node> guiNode_, QWidget* parent_):
 }
 
 
-
-void QBmsData::callbackBmsData(const rover_msgs::msg::BmsData& msg)
+void QBmsData::initializeWidget(void)
 {
+    this->addMeasureWidget(eMeasurementType::BATTERY_AMPS);
+    this->addMeasureWidget(eMeasurementType::CELL_1_VOLT);
+    this->addMeasureWidget(eMeasurementType::CELL_2_VOLT);
+    this->addMeasureWidget(eMeasurementType::CELL_3_VOLT);
+    this->addMeasureWidget(eMeasurementType::CELL_4_VOLT);
+    this->addMeasureWidget(eMeasurementType::CELL_5_VOLT);
+    this->addMeasureWidget(eMeasurementType::CELL_6_VOLT);
+}
 
+void QBmsData::addMeasureWidget(eMeasurementType measurementType_)
+{
+    std::unique_ptr<QWidget> bmsDataContainer = std::make_unique<QWidget>(_ui.bmsData);
+    bmsDataContainer->setStyleSheet(STATUS_DEFAULT);
+
+    std::unique_ptr<QHBoxLayout> containerLayout = std::make_unique<QHBoxLayout>(bmsDataContainer.get());
+    containerLayout->setContentsMargins(1, 1, 1, 1);
+    containerLayout->setSpacing(1);
+
+    std::unique_ptr<QLabel> iconLabel = std::make_unique<QLabel>(bmsDataContainer.get());
+    iconLabel->setFixedSize(ICON_DIMENSION, ICON_DIMENSION);
+    iconLabel->setScaledContents(true);
+    iconLabel->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+
+    QPixmap defaultIcon(QString::fromStdString(this->getBmsDataIcon(measurementType_)));
+    if (!defaultIcon.isNull())
+    {
+        QPixmap scaledIcon = defaultIcon.scaled(iconLabel->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+        iconLabel->setAlignment(Qt::AlignCenter);
+        iconLabel->setPixmap(scaledIcon);
+    }
+
+    std::unique_ptr<QLabel> bmsInfoLabel = std::make_unique<QLabel>(bmsDataContainer.get());
+    bmsInfoLabel ->setAlignment(Qt::AlignCenter);
+    bmsInfoLabel ->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    bmsInfoLabel ->setWordWrap(false);
+
+    QString infoText = QString::fromStdString(this->getBmsDataName(measurementType_));
+
+    bmsInfoLabel->setText(infoText);
+
+    containerLayout->addWidget(iconLabel.release());
+    containerLayout->addWidget(bmsInfoLabel.get());
+
+    _bmsDataTypes[measurementType_] = {bmsDataContainer.get(), bmsInfoLabel.release()};
+
+    bmsDataContainer->setLayout(containerLayout.release());
+    _layout->addWidget(bmsDataContainer.release());
+
+}
+
+
+void QBmsData::callbackBmsData(const rover_msgs::msg::BmsData& msg_)
+{
+    this->updateBmsData(eMeasurementType::BATTERY_AMPS, msg_);
+    this->updateBmsData(eMeasurementType::CELL_1_VOLT, msg_);
+    this->updateBmsData(eMeasurementType::CELL_2_VOLT, msg_);
+    this->updateBmsData(eMeasurementType::CELL_3_VOLT, msg_);
+    this->updateBmsData(eMeasurementType::CELL_4_VOLT, msg_);
+    this->updateBmsData(eMeasurementType::CELL_5_VOLT, msg_);
+    this->updateBmsData(eMeasurementType::CELL_6_VOLT, msg_);
+}
+
+void QBmsData::updateBmsData(eMeasurementType measurementType_, const rover_msgs::msg::BmsData& msg_)
+{
+    QLabel* infoLabel = _bmsDataTypes[measurementType_].bmsInfoLabel;
+    
+    QString infoText;
+
+    switch (measurementType_)
+    {
+        case eMeasurementType::BATTERY_AMPS:
+            infoText = QString::number(msg_.battery_amps);
+            break;
+        case eMeasurementType::CELL_1_VOLT:
+            infoText = QString::number(msg_.cell_volt[0]);
+            break;
+        case eMeasurementType::CELL_2_VOLT:
+            infoText = QString::number(msg_.cell_volt[1]);
+            break;
+        case eMeasurementType::CELL_3_VOLT:
+            infoText = QString::number(msg_.cell_volt[2]);
+            break;
+        case eMeasurementType::CELL_4_VOLT:
+            infoText = QString::number(msg_.cell_volt[3]);
+            break;
+        case eMeasurementType::CELL_5_VOLT:
+            infoText = QString::number(msg_.cell_volt[4]);
+            break;
+        case eMeasurementType::CELL_6_VOLT:
+            infoText = QString::number(msg_.cell_volt[5]);
+            break;
+    }
+
+    infoLabel->setText(infoText);
+}
+
+std::string QBmsData::getBmsDataIcon(eMeasurementType measurementType_)
+{
+    switch (measurementType_)
+    {
+        case eMeasurementType::AH:
+            [[fallthrough]];
+        case eMeasurementType::BATTERY_AMPS:
+            return ":/icons/motor.png";
+        case eMeasurementType::BATTERY_VOLT:
+            [[fallthrough]];
+        case eMeasurementType::CELL_1_VOLT:
+            [[fallthrough]];
+        case eMeasurementType::CELL_2_VOLT:
+            [[fallthrough]];
+        case eMeasurementType::CELL_3_VOLT:
+            [[fallthrough]];
+        case eMeasurementType::CELL_4_VOLT:
+            [[fallthrough]];
+        case eMeasurementType::CELL_5_VOLT:
+            [[fallthrough]];
+        case eMeasurementType::CELL_6_VOLT:
+            [[fallthrough]];
+        case eMeasurementType::CHARGE_AMPS:
+            [[fallthrough]];
+        case eMeasurementType::CHARGE_VOLT:
+            [[fallthrough]];
+        case eMeasurementType::LOAD_AMPS:
+            [[fallthrough]];
+        case eMeasurementType::LOAD_VOLT:
+            [[fallthrough]];
+        case eMeasurementType::MAX_AH:
+            [[fallthrough]];
+        case eMeasurementType::SOC:
+            [[fallthrough]];
+        default:
+            return ":/icons/motor.png";
+    }
+}
+
+std::string QBmsData::getBmsDataName(eMeasurementType measurementType_)
+{
+    switch (measurementType_)
+    {
+        case eMeasurementType::AH:
+            return "Ah";
+        case eMeasurementType::BATTERY_AMPS:
+            return "Battery Amps";
+        case eMeasurementType::BATTERY_VOLT:
+            return "Battery Volt";
+        case eMeasurementType::CELL_1_VOLT:
+            return "Cell 1 Volt";
+        case eMeasurementType::CELL_2_VOLT:
+            return "Cell 2 Volt";
+        case eMeasurementType::CELL_3_VOLT:
+            return "Cell 3 Volt";
+        case eMeasurementType::CELL_4_VOLT:
+            return "Cell 4 Volt";
+        case eMeasurementType::CELL_5_VOLT:
+            return "Cell 5 Volt";
+        case eMeasurementType::CELL_6_VOLT:
+            return "Cell 6 Volt";
+        case eMeasurementType::CHARGE_AMPS:
+            return "Charge Amps";
+        case eMeasurementType::CHARGE_VOLT:
+            return "Charge Volt";
+        case eMeasurementType::LOAD_AMPS:
+            return "Load Amps";
+        case eMeasurementType::LOAD_VOLT:
+            return "Load Volt";
+        case eMeasurementType::MAX_AH:
+            return "Max Ah";
+        case eMeasurementType::SOC:
+            return "State of charge";
+        default:
+            return "Couldn't find BMS Data Type";
+    }
 }
--- rover_gui/src/QBmsData/QBmsData.hpp
+++ rover_gui/src/QBmsData/QBmsData.hpp
@@ -12,22 +12,57 @@
 #include <QtWidgets/QGridLayout>
 #include "UI_BmsData.h"
 #include "Global/QFlowLayout.hpp"
+#include <QLabel>
 
 
 class QBmsData : public QWidget
 {
     Q_OBJECT
 
+    static constexpr uint8_t ICON_DIMENSION = 55U;
+
+    enum class eMeasurementType
+    {
+      BATTERY_AMPS,
+      AH,
+      MAX_AH,
+      SOC,
+      CHARGE_AMPS,
+      LOAD_AMPS,
+      BATTERY_VOLT,
+      LOAD_VOLT,
+      CHARGE_VOLT,
+      CELL_1_VOLT,
+      CELL_2_VOLT,
+      CELL_3_VOLT,
+      CELL_4_VOLT,
+      CELL_5_VOLT,
+      CELL_6_VOLT
+    };
+
+    struct sBmsDataInfos
+    {
+        QWidget* bmsDataContainer;
+        QLabel* bmsInfoLabel;
+    };
+
     public:
         QBmsData(std::shared_ptr<rclcpp::Node> guiNode_, QWidget* parent_);
-        void callbackBmsData(const rover_msgs::msg::BmsData& msg_);
 
 
     private:
+        void callbackBmsData(const rover_msgs::msg::BmsData& msg_);
+        void initializeWidget(void);
+        void addMeasureWidget(eMeasurementType measurementType_);
+        std::string getBmsDataIcon(eMeasurementType measurementType_);
+        std::string getBmsDataName(eMeasurementType measurementType_);
+        void updateBmsData(eMeasurementType measurementType_, const rover_msgs::msg::BmsData& msg_);
+
         std::shared_ptr<rclcpp::Node> _node;
         Ui::DataLogger _ui;
         rclcpp::Subscription<rover_msgs::msg::BmsData>::SharedPtr _sub_bmsData;
         std::unique_ptr<QFlowLayout> _layout;
+        std::unordered_map<eMeasurementType, sBmsDataInfos> _bmsDataTypes;
 
 
 };
