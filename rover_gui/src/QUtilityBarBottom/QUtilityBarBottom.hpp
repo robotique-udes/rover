4f27cb38967e535c1529d347003488146bbc9440
Araa2876 <anibal.arango@usherbrooke.ca>
2026-05-09 18:37:59 -0400

    Gripper visual in gui

--- rover_gui/src/QUtilityBarBottom/QUtilityBarBottom.hpp
+++ rover_gui/src/QUtilityBarBottom/QUtilityBarBottom.hpp
@@ -2,19 +2,35 @@
 #define QUTILITY_BAR_BOTTOM_HPP
 
 #include "UI_UtilityBarBottom.h"
+#include <rclcpp/rclcpp.hpp>
+#include <rover_msgs/msg/arm_msg.hpp>
 
 class QUtilityBarBottom : public QWidget
 {
     Q_OBJECT
 
+    static constexpr const char* ARM_STATUS_TOPIC = "/rover/arm/joints_status";
+    static constexpr const float MAX_TORQUE_GRIPPER = 0.30F;
+
   public:
-    explicit QUtilityBarBottom(QWidget* parent_);
+    explicit QUtilityBarBottom(std::shared_ptr<rclcpp::Node> node_, QWidget* parent_);
 
   signals:
     void seeHistory();
+    void updateTorqueIndicator(float torque_);
+
+  private slots:
+    void updateIndicator(const float torque_);
 
   private:
+    void CB_armStatus(const rover_msgs::msg::ArmMsg& msg_);
+
     Ui::UtilityBarBottom _ui;
+    std::shared_ptr<rclcpp::Node> _node;
+
+    std::shared_ptr<rclcpp::Subscription<rover_msgs::msg::ArmMsg>> _sub_armJointStatus;
+
+    float _lastColor;
 };
 
 #endif  // QUTILITY_BAR_BOTTOM_HPP
--- rover_shared_libs/rover_can2/src/rover_can2/constant.hpp
+++ rover_shared_libs/rover_can2/src/rover_can2/constant.hpp
@@ -88,6 +88,7 @@ namespace RoverCan2::Constant
         
         ARM_JOINT_CMD,
         ARM_JOINT_STATUS,
+        ARM_JOINT_ADVANCED_STATUS,
         ARM_JOINT_CONFIG,
         
         FIX_POSITION,
@@ -109,12 +110,19 @@ namespace RoverCan2::Constant
      * @brief Array holding all valid and implemented msgs used on the network.
      *
      */
-    constexpr CompileTimeArray<eMsgId, 23UL> SUPPORTED_MSGS = {
-        eMsgId::TEST_MSG,          eMsgId::TEST_MSG_2,       eMsgId::ERROR_STATE, eMsgId::HEARTBEAT,    eMsgId::POWER_CMD,
-        eMsgId::POWER_STATUS,      eMsgId::PWM_CMD,          eMsgId::PWM_STATUS,  eMsgId::PWM_INFO,     eMsgId::PROP_SPEED_CMD,
-        eMsgId::PROP_SPEED_STATUS, eMsgId::DDB_CMD,          eMsgId::DDB_STATUS,  eMsgId::FIX_POSITION, eMsgId::FIX_HEADING,
-        eMsgId::FIX_INFO,          eMsgId::PTZ_CMD,          eMsgId::PTZ_STATUS,  eMsgId::PTZ_CONFIG,   eMsgId::ARM_JOINT_CMD,
-        eMsgId::ARM_JOINT_STATUS,  eMsgId::ARM_JOINT_CONFIG, eMsgId::SENSOR_BOX,
+    constexpr CompileTimeArray<eMsgId, 24UL> SUPPORTED_MSGS = {
+        eMsgId::TEST_MSG,          eMsgId::TEST_MSG_2,
+        eMsgId::ERROR_STATE,       eMsgId::HEARTBEAT,
+        eMsgId::POWER_CMD,         eMsgId::POWER_STATUS,
+        eMsgId::PWM_CMD,           eMsgId::PWM_STATUS,
+        eMsgId::PWM_INFO,          eMsgId::PROP_SPEED_CMD,
+        eMsgId::PROP_SPEED_STATUS, eMsgId::DDB_CMD,
+        eMsgId::DDB_STATUS,        eMsgId::FIX_POSITION,
+        eMsgId::FIX_HEADING,       eMsgId::FIX_INFO,
+        eMsgId::PTZ_CMD,           eMsgId::PTZ_STATUS,
+        eMsgId::PTZ_CONFIG,        eMsgId::ARM_JOINT_CMD,
+        eMsgId::ARM_JOINT_STATUS,  eMsgId::ARM_JOINT_ADVANCED_STATUS,
+        eMsgId::ARM_JOINT_CONFIG,  eMsgId::SENSOR_BOX,
     };
 
     /**
