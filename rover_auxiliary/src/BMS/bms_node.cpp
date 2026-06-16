bbcc2de7f3d703d878733aafaff6b71035b22974
JoeyBilodeuL <bilj1577@usherbrooke.ca>
2026-06-16 19:10:02 -0400

    updated branch with appropriate file changes

--- rover_auxiliary/src/BMS/bms_node.cpp
+++ rover_auxiliary/src/BMS/bms_node.cpp
@@ -3,22 +3,23 @@
 int main(int argc_, char** argv_)
 {
     rclcpp::init(argc_, argv_);
-    auto node = std::make_shared<BMSDataNode>();
+    std::shared_ptr<BMSDataNode> node = std::make_shared<BMSDataNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
 
 BMSDataNode::BMSDataNode():
-    Node("bms_info")
+    Node("bms_info"),
+    _terminal("/dev/ttyACM0")
 {
-    this->_publisher = create_publisher<rover_msgs::msg::BmsData>(TOPIC_BMS_DATA, QOS_DEFAULT);
+    _publisher = create_publisher<rover_msgs::msg::BmsData>(TOPIC_BMS_DATA, QOS_DEFAULT);
 
-    this->_timer_publisher = create_wall_timer(std::chrono::milliseconds(DELAY_PUBLISHER_MS),
-                                               [this](void)
-                                               {
-                                                   this->callbackBMSData();
-                                               });
+    _timer_publisher = create_wall_timer(std::chrono::milliseconds(DELAY_PUBLISHER_MS),
+                                         [this](void)
+                                         {
+                                             this->callbackBMSData();
+                                         });
 }
 
 void BMSDataNode::callbackBMSData(void)
@@ -27,79 +28,66 @@ void BMSDataNode::callbackBMSData(void)
 
     this->getData();
 
-    msg.battery_amps = this->_ampArray[AmpIndexType::BATTERY_AMPS];
+    msg.battery_amps = _ampArray[AmpIndexType::BATTERY_AMPS];
 
-    std::vector<uint16_t> cellVolts;
+    msg.cell_volt.resize(CELL_VOLT_END - CELL_VOLT_START);
 
     for (uint16_t i = VoltIndexType::CELL_VOLT_START; i < VoltIndexType::CELL_VOLT_END; i++)
     {
-        cellVolts.push_back(this->_voltArray[i]); 
+        msg.cell_volt[i - CELL_VOLT_START] = _voltArray[i];
     }
 
-    msg.cell_volt = cellVolts;
-
-    this->_publisher->publish(msg);
-
-    cellVolts.clear();
+    _publisher->publish(msg);
 }
 
 void BMSDataNode::getData(void)
 {
     std::string ampSerialOutput;
     std::string voltSerialOutput;
-    int fileDesc;
 
-    fileDesc = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_SYNC);
+    _terminal.serialWrite("?A\r");
+    ampSerialOutput = _terminal.serialRead();
+    _terminal.serialWrite("?V\r");
+    voltSerialOutput = _terminal.serialRead();
 
-    if (tcflush(fileDesc, TCIOFLUSH) == -1)
-    {
-        std::string errorMsg = "tcflush failed: " + std::string(strerror(errno));
-        RCLCPP_WARN(this->get_logger(), errorMsg.c_str());
-    }
-
-    SerialCom terminal(fileDesc);
-
-    terminal.serialWrite("?A\r");
-    ampSerialOutput = terminal.serialRead();
-    terminal.serialWrite("?V\r");
-    voltSerialOutput = terminal.serialRead();
-
-    this->parse(ampSerialOutput, this->_ampArray, AMP_DATA_TYPES);
-    this->parse(voltSerialOutput, this->_voltArray, VOLT_DATA_TYPES);
-
-    if (close(fileDesc) == -1)
-    {
-        std::string errorMsg = "unable to close the fileDesc:" + std::string(strerror(errno));
-        RCLCPP_WARN(this->get_logger(), errorMsg.c_str());
-    }
+    this->parse(ampSerialOutput, _ampArray, AMP_DATA_TYPES);
+    this->parse(voltSerialOutput, _voltArray, VOLT_DATA_TYPES);
 }
 
-void BMSDataNode::parse(std::string rawOutput, int16_t dataArray[], uint16_t arraySize)
+void BMSDataNode::parse(const std::string rawOutput_, uint16_t dataArray_[], uint16_t arraySize_)
 {
-    uint16_t indexGarb = 0;
+    std::string_view view = rawOutput_;
 
-    while(indexGarb < rawOutput.size() && rawOutput[indexGarb] != '=')
+    size_t startPos = view.find('=');
+    if (startPos == std::string_view::npos)
     {
-        indexGarb++;
+        RCLCPP_WARN(this->get_logger(), "Command to retrieve data from BMS failed");
+        return;
     }
 
-    rawOutput = rawOutput.substr(indexGarb + 1);
+    view.remove_prefix(startPos + 1);
 
-    for (uint16_t i = 0; i < arraySize; i++)
+    if (view.empty())
     {
-        uint16_t index = 0;
+        RCLCPP_WARN(this->get_logger(), "No data retreived from BMS command");
+        return;
+    }
 
-        while (index < rawOutput.size() && rawOutput[index] != ':')
-        {
-            index++;
-        }
-        dataArray[i] = std::stoi(rawOutput.substr(0, index));
+    for (uint16_t i = 0; i < arraySize_; i++)
+    {
+        size_t delimPos = view.find(':');
 
-        if (i < arraySize - 1)
+        dataArray_[i] = std::stoi(std::string(view.substr(0, delimPos)));
+
+        if (delimPos == std::string_view::npos)
         {
-            rawOutput = rawOutput.substr(index + 1);
+            if (i < arraySize_ - 1)
+            {
+                RCLCPP_WARN(this->get_logger(), "BMS's command output returned fewer data than expected");
+            }
+            return;
         }
+
+        view.remove_prefix(delimPos + 1);
     }
 }
-
-
--- rover_auxiliary/src/BMS/bms_node.hpp
+++ rover_auxiliary/src/BMS/bms_node.hpp
@@ -1,54 +1,57 @@
 #ifndef BMS_NODE_HPP
 #define BMS_NODE_HPP
 
-#include "rclcpp/rclcpp.hpp"
-#include "rover_lib2/helpers/constants.hpp"
 #include "rover_msgs/msg/bms_data.hpp"
+#include "rover_lib2/communication/Serial/serial_com.hpp"
+
+#include <rover_lib2/helpers/constants.hpp>
+#include <rclcpp/rclcpp.hpp>
 #include <utility>
 #include <fstream>
 #include <iostream>
 #include <vector>
 #include <fcntl.h>
 #include <termios.h>
-#include "rover_lib2/communication/Serial/serial_com.hpp"
 
 class BMSDataNode : public rclcpp::Node
 {
-    static constexpr const char* TOPIC_BMS_DATA = "rover/auxiliary/bms_data";
+    static constexpr const char* TOPIC_BMS_DATA = "/rover/auxiliary/bms_data";
     static constexpr uint64_t DELAY_PUBLISHER_MS = 1000UL;
     static constexpr uint16_t VOLT_DATA_TYPES = 9;
     static constexpr uint16_t AMP_DATA_TYPES = 6;
 
     enum AmpIndexType
     {
-      BATTERY_AMPS,
-      AH,
-      MAX_AH,
-      SOC,
-      CHARGE_AMPS,
-      LOAD_AMPS
+        BATTERY_AMPS,
+        AH,
+        MAX_AH,
+        SOC,
+        CHARGE_AMPS,
+        LOAD_AMPS
     };
 
     enum VoltIndexType
     {
-      BATTERY,
-      LOAD,
-      CHARGE,
-      CELL_VOLT_START,
-      CELL_VOLT_END = 9
+        BATTERY,
+        LOAD,
+        CHARGE,
+        CELL_VOLT_START,
+        CELL_VOLT_END = 9
     };
 
   public:
     BMSDataNode();
-    void callbackBMSData(void);
-    void getData(void);
-    void parse(std::string rawOutput, int16_t dataArray[], uint16_t arraySize);
 
   private:
-    int16_t _ampArray[AMP_DATA_TYPES];
-    int16_t _voltArray[VOLT_DATA_TYPES];
+    void callbackBMSData(void);
+    void getData(void);
+    void parse(std::string rawOutput_, uint16_t dataArray_[], uint16_t arraySize_);
+
+    uint16_t _ampArray[AMP_DATA_TYPES];
+    uint16_t _voltArray[VOLT_DATA_TYPES];
     rclcpp::Publisher<rover_msgs::msg::BmsData>::SharedPtr _publisher;
     rclcpp::TimerBase::SharedPtr _timer_publisher;
+    SerialCom _terminal;
 };
 
 #endif
