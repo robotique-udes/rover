5150e0454c9348fc79785c562d0ca9c25a7f9212
AraaRobot <araa2876@usherbrooke.ca>
2026-07-29 00:01:41 -0400

    Lib matching with micro

--- rover_shared_libs/rover_can2/src/rover_can2/constant.hpp
+++ rover_shared_libs/rover_can2/src/rover_can2/constant.hpp
@@ -89,6 +89,7 @@ namespace RoverCan2::Constant
         
         ARM_JOINT_CMD,
         ARM_JOINT_STATUS,
+        ARM_JOINT_ADVANCED_STATUS,
         ARM_JOINT_CONFIG,
         
         FIX_POSITION,
@@ -112,12 +113,20 @@ namespace RoverCan2::Constant
      * @brief Array holding all valid and implemented msgs used on the network.
      *
      */
-    constexpr CompileTimeArray<eMsgId, 24UL> SUPPORTED_MSGS = {
-        eMsgId::TEST_MSG,          eMsgId::TEST_MSG_2,       eMsgId::ERROR_STATE, eMsgId::HEARTBEAT,    eMsgId::POWER_CMD,
-        eMsgId::POWER_STATUS,      eMsgId::PWM_CMD,          eMsgId::PWM_STATUS,  eMsgId::PWM_INFO,     eMsgId::PROP_SPEED_CMD,
-        eMsgId::PROP_SPEED_STATUS, eMsgId::DDB_CMD,          eMsgId::DDB_STATUS,  eMsgId::FIX_POSITION, eMsgId::FIX_HEADING,
-        eMsgId::FIX_INFO,          eMsgId::PTZ_CMD,          eMsgId::PTZ_STATUS,  eMsgId::PTZ_CONFIG,   eMsgId::ARM_JOINT_CMD,
-        eMsgId::ARM_JOINT_STATUS,  eMsgId::ARM_JOINT_CONFIG, eMsgId::SENSOR_BOX,  eMsgId::MORSE_CODE,
+    constexpr CompileTimeArray<eMsgId, 25UL> SUPPORTED_MSGS = {
+        eMsgId::TEST_MSG,          eMsgId::TEST_MSG_2,
+        eMsgId::ERROR_STATE,       eMsgId::HEARTBEAT,
+        eMsgId::POWER_CMD,         eMsgId::POWER_STATUS,
+        eMsgId::PWM_CMD,           eMsgId::PWM_STATUS,
+        eMsgId::PWM_INFO,          eMsgId::PROP_SPEED_CMD,
+        eMsgId::PROP_SPEED_STATUS, eMsgId::DDB_CMD,
+        eMsgId::DDB_STATUS,        eMsgId::FIX_POSITION,
+        eMsgId::FIX_HEADING,       eMsgId::FIX_INFO,
+        eMsgId::PTZ_CMD,           eMsgId::PTZ_STATUS,
+        eMsgId::PTZ_CONFIG,        eMsgId::ARM_JOINT_CMD,
+        eMsgId::ARM_JOINT_STATUS,  eMsgId::ARM_JOINT_ADVANCED_STATUS,
+        eMsgId::ARM_JOINT_CONFIG,  eMsgId::SENSOR_BOX,
+        eMsgId::MORSE_CODE,
     };
 
     /**
