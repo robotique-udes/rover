6d7f9bf751279a703437cc22ece23c3ed5c22d2e
AraaRobot <araa2876@usherbrooke.ca>
2026-07-28 23:59:29 -0400

    Libs match

--- rover_shared_libs/rover_can2/src/rover_can2/msgs/arm_joint_advanced_status.hpp
+++ rover_shared_libs/rover_can2/src/rover_can2/msgs/arm_joint_advanced_status.hpp
@@ -1,5 +1,5 @@
-#ifndef ARM_JOINT_ADVANCED_STATUS_HPP
-#define ARM_JOINT_ADVANCED_STATUS_HPP
+#ifndef ROVER_CAN2_MSGS_ARM_JOINT_ADVANCED_STATUS_HPP
+#define ROVER_CAN2_MSGS_ARM_JOINT_ADVANCED_STATUS_HPP
 
 #include "rover_can2/msgs/msg.hpp"
 #include "rover_can2/helpers.hpp"
@@ -169,4 +169,4 @@ namespace RoverCan2::Msgs
 
 }  // namespace RoverCan2::Msgs
 
-#endif  // ARM_JOINT_ADVANCED_STATUS_HPP
+#endif  // ROVER_CAN2_MSGS_ARM_JOINT_ADVANCED_STATUS_HPP
