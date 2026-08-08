8c6811f754342151993eb947b7fd12a9a69566e5
JoeyBilodeuL <bilj1577@usherbrooke.ca>
2026-08-08 13:39:02 -0400

    fixed header in serial

--- rover_shared_libs/rover_lib2/src/rover_lib2/communication/Serial/serial_com.hpp
+++ rover_shared_libs/rover_lib2/src/rover_lib2/communication/Serial/serial_com.hpp
@@ -1,5 +1,5 @@
-#ifndef SERIAL_COM_H
-#define SERIAL_COM_H
+#ifndef ROVER_LIB2_COMMUNICATION_SERIAL_SERIAL_COM_HPP
+#define ROVER_LIB2_COMMUNICATION_SERIAL_SERIAL_COM_HPP
 
 #if defined(__linux__) && defined(ROS)
 
@@ -111,4 +111,4 @@ class SerialCom
 #else
 #error "SerialCom requires Linux (POSIX termios). Not available on this target."
 #endif  // __linux__
-#endif
+#endif  // ROVER_LIB2_COMMUNICATION_SERIAL_SERIAL_COM_HPP
