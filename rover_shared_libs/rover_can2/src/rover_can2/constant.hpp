073be63c938e4c4ea26850796425852b3b4efb45
Araa2876 <anibal.arango@usherbrooke.ca>
2026-08-08 20:18:08 -0400

    Lint

--- rover_shared_libs/rover_can2/src/rover_can2/constant.hpp
+++ rover_shared_libs/rover_can2/src/rover_can2/constant.hpp
@@ -111,18 +111,12 @@ namespace RoverCan2::Constant
      *
      */
     constexpr CompileTimeArray<eMsgId, 24UL> SUPPORTED_MSGS = {
-        eMsgId::TEST_MSG,          eMsgId::TEST_MSG_2,
-        eMsgId::ERROR_STATE,       eMsgId::HEARTBEAT,
-        eMsgId::POWER_CMD,         eMsgId::POWER_STATUS,
-        eMsgId::PWM_CMD,           eMsgId::PWM_STATUS,
-        eMsgId::PWM_INFO,          eMsgId::PROP_SPEED_CMD,
-        eMsgId::PROP_SPEED_STATUS, eMsgId::DDB_CMD,
-        eMsgId::DDB_STATUS,        eMsgId::FIX_POSITION,
-        eMsgId::FIX_HEADING,       eMsgId::FIX_INFO,
-        eMsgId::PTZ_CMD,           eMsgId::PTZ_STATUS,
-        eMsgId::PTZ_CONFIG,        eMsgId::ARM_JOINT_CMD,
-        eMsgId::ARM_JOINT_STATUS,  eMsgId::ARM_JOINT_CONFIG,
-        eMsgId::SENSOR_BOX,        eMsgId::ARM_JOINT_ADVANCED_STATUS,
+        eMsgId::TEST_MSG,         eMsgId::TEST_MSG_2,       eMsgId::ERROR_STATE,       eMsgId::HEARTBEAT,
+        eMsgId::POWER_CMD,        eMsgId::POWER_STATUS,     eMsgId::PWM_CMD,           eMsgId::PWM_STATUS,
+        eMsgId::PWM_INFO,         eMsgId::PROP_SPEED_CMD,   eMsgId::PROP_SPEED_STATUS, eMsgId::DDB_CMD,
+        eMsgId::DDB_STATUS,       eMsgId::FIX_POSITION,     eMsgId::FIX_HEADING,       eMsgId::FIX_INFO,
+        eMsgId::PTZ_CMD,          eMsgId::PTZ_STATUS,       eMsgId::PTZ_CONFIG,        eMsgId::ARM_JOINT_CMD,
+        eMsgId::ARM_JOINT_STATUS, eMsgId::ARM_JOINT_CONFIG, eMsgId::SENSOR_BOX,        eMsgId::ARM_JOINT_ADVANCED_STATUS,
     };
 
     /**
